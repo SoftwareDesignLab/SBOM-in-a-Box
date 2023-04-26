d1544684c582e6e21a8f121858d2926b49b022e1
Asa Horn <horntx3@gmail.com>
2023-04-26 15:50:28 -0400

    Fix web servlet not available error

--- api/src/main/java/org/svip/api/unifiedApplication.java
+++ api/src/main/java/org/svip/api/unifiedApplication.java
@@ -1,20 +1,19 @@
 package org.svip.api;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * This class contains the main function which runs both APIs as spring boot applications
  *
  * @author Asa Horn
  */
-public class unifiedApplication {
+@SpringBootApplication
+public class unifiedApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(plugFestApiController.class)
-                .web(WebApplicationType.SERVLET) //This is required to prevent "No valid webserver" error.
-                .run(args);
-        new SpringApplicationBuilder(SVIPApiController.class)
-                .web(WebApplicationType.SERVLET) //This is required to prevent "No valid webserver" error.
-                .run(args);
+        SpringApplication.run(unifiedApplication.class, args);
     }
 }
