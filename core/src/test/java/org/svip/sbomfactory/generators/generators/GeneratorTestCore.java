3c602e9411fd4f05ac334329b76d20b0a89fa7e5
Dylan Mulligan <60295839+dylan-mulligan@users.noreply.github.com>
2023-05-02 11:39:50 -0400

    Added abstract testing class for generators

---
+++ core/src/test/java/org/svip/sbomfactory/generators/generators/GeneratorTestCore.java
@@ -0,0 +1,28 @@
+package org.svip.sbomfactory.generators.generators;
+
+import org.svip.sbomfactory.generators.parsers.Parser;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public abstract class GeneratorTestCore {
+//    protected Parser PARSER;
+//
+//    // TODO: Docstring
+//    protected GeneratorTestCore(Parser parser, String src) {
+//        setDummyParser(parser, src);
+//    }
+//
+//    /**
+//     * Initializes given parser and assigns it to this.PARSER
+//     *
+//     * @param parser Parser to test
+//     * @param src Path to directory to be tested
+//     */
+//    private void setDummyParser(Parser parser, String src) {
+//        final Path path = Paths.get(src);
+//        parser.setPWD(path);
+//        parser.setSRC(path);
+//        this.PARSER = parser;
+//    }
+}
