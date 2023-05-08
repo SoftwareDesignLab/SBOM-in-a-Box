e864c4aa978166a4ca595402c0e0784d953e8fd0
pxlvse <pxlvse@rit.edu>
2023-05-08 09:15:23 -0400

    Init Requirements Parser Test

---
+++ core/src/test/java/org/svip/sbomfactory/generators/parsers/packagemanagers/RequirementsParserTest.java
@@ -0,0 +1,108 @@
+package org.svip.sbomfactory.generators.parsers.packagemanagers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.svip.sbomfactory.generators.utils.Debug;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.svip.sbomfactory.generators.utils.Debug.log;
+
+public class RequirementsParserTest extends ParseDepFileTestCore {
+    /**
+     * Constructor initializes the respective parser and assigns both the
+     * fileContents to test it against and the source directory to test on.
+     */
+    protected RequirementsParserTest() throws IOException {
+        super(new RequirementsParser(),
+                Files.readString(Paths.get("src/test/java/org/svip/sbomfactory/generators/TestData/Python/Absolute/requirements.txt")),
+                "src/test/java/org/svip/sbomfactory/generators/TestData/Python/Absolute");
+        }
+
+    @Test
+    @DisplayName("Test Properties")
+    void testProperties() {
+        // Get properties from PARSER
+        final HashMap<String, String> props = this.PARSER.properties;
+
+        int i = 5;
+        //"^[^#\\s]*(==){1}.* pliu ^(?:(?!<scr>).)*<scr>(?!.*<scr>).*$"
+        boolean b4 = Pattern.matches("^[^#](?:(?!==).)*==(?!.*==).+$", "#aa==bb".trim());
+
+        System.out.println(b4);
+
+        // Test correct count is found
+//        assertEquals(4, props.size());
+//
+//        // Get keySet
+//        final Set<String> keySet = props.keySet();
+//
+//        // Check for correct element insertion
+//        assertTrue(keySet.contains("arcgisVersion"));
+//        assertTrue(keySet.contains("sampleVersion1"));
+//        assertTrue(keySet.contains("rootPath"));
+//        assertTrue(keySet.contains("testPath"));
+//
+//        // Check values
+//        assertEquals("200.1.0", props.get("arcgisVersion"));
+//        assertEquals("this/is/a/path", props.get("rootPath"));
+//        assertEquals("$rootPath/test", props.get("testPath"));
+    }
+
+    @Test
+    @DisplayName("Test Dependencies")
+    void testDependencies() {
+        // Rebuild list of deps as map, with artifactId as key
+        final HashMap<String, LinkedHashMap<String, String>> deps =
+                (HashMap<String, LinkedHashMap<String, String>>) this.PARSER.dependencies
+                        .stream().collect(
+                                Collectors.toMap(
+                                        d -> d.get("artifactId"),
+                                        d -> d,
+                                        // Merge conflict function, currently overrides existing values
+                                        (d1, d2) -> {
+                                            log(Debug.LOG_TYPE.WARN, String.format("Duplicate key found: %s", d2.get("artifactId")));
+                                            return d2;
+                                        }
+                                ));
+
+        // Test correct count is found
+//        assertEquals(14, deps.size());
+//
+//
+//        // Get keySet
+//        final Set<String> keySet = deps.keySet();
+//
+//        // Check for correct element insertion
+//        assertTrue(keySet.contains("arcgis-java"));
+//        LinkedHashMap<String, String> depi = deps.get("arcgis-java");
+//        assertEquals("com.esri.arcgisruntime", depi.get("groupId"));
+//        assertEquals("$arcgisVersion", depi.get("version"));
+//        assertEquals("200.1.0", this.PARSER.properties.get("arcgisVersion"));
+//
+//        assertTrue(keySet.contains("spring-api"));
+//        depi = deps.get("spring-api");
+//        assertEquals("org.springframework", depi.get("groupId"));
+//        assertEquals("3.6", depi.get("version"));
+//
+//        assertTrue(keySet.contains("slf4j-nop"));
+//        depi = deps.get("slf4j-nop");
+//        assertEquals("org.slf4j", depi.get("groupId"));
+//        assertEquals("2.0.5", depi.get("version"));
+//
+//        assertTrue(keySet.contains("spring-core"));
+//        depi = deps.get("spring-core");
+//        assertEquals("org.springframework", depi.get("groupId"));
+//        assertEquals("2.5", depi.get("version"));
+
+    }
+}
