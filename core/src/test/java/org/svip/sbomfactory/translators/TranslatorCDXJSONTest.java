de72285424098550687f480ce37c064187656391
Dylan Mulligan <60295839+dylan-mulligan@users.noreply.github.com>
2023-05-09 11:35:27 -0400

    Initial refactor of translator tests

--- core/src/test/java/org/svip/sbomfactory/translators/TranslatorCDXJSONTest.java
+++ core/src/test/java/org/svip/sbomfactory/translators/TranslatorCDXJSONTest.java
@@ -4,8 +4,8 @@ import org.cyclonedx.exception.ParseException;
 import org.junit.jupiter.api.Test;
 
 import org.svip.sbom.model.SBOM;
-import org.svip.sbomfactory.translators.TranslatorCDXJSON;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  *
  * @author Tyler Drake
  */
-public class TranslatorCDXJSONTest {
+public class TranslatorCDXJSONTest extends TranslatorTestCore<TranslatorCDXJSON> {
 
     public static final String TEST_SMALL_CDX_JSON = "src/test/java/org/svip/sbomfactory/translators/sample_boms/cdx_json/sbom.alpine.json";
 
@@ -25,9 +25,14 @@ public class TranslatorCDXJSONTest {
 
     public static final String TEST_ANOTHER_SMALL_SYFT_CDX_JSON = "src/test/java/org/svip/sbomfactory/translators/sample_boms/cdx_json/cdx.json";
 
+    // Create instance of translator on test initialization
+    protected TranslatorCDXJSONTest() {
+        super(new TranslatorCDXJSON());
+    }
+
     @Test
-    public void build_SBOM_from_small_cdx_json_test() throws IOException, ParseException {
-        SBOM sbom = TranslatorCDXJSON.translatorCDXJSON(TEST_SMALL_CDX_JSON);
+    public void build_SBOM_from_small_cdx_json_test() throws IOException, ParseException, ParserConfigurationException {
+        SBOM sbom = this.TRANSLATOR.translatePath(TEST_SMALL_CDX_JSON);
         assertNotNull(sbom);
         assertEquals("1", sbom.getSbomVersion());
         assertEquals("1.4", sbom.getSpecVersion());
@@ -35,8 +40,8 @@ public class TranslatorCDXJSONTest {
     }
 
     @Test
-    public void build_SBOM_from_medium_cdx_json_test() throws IOException, ParseException {
-        SBOM sbom = TranslatorCDXJSON.translatorCDXJSON(TEST_MEDIUM_CDX_JSON);
+    public void build_SBOM_from_medium_cdx_json_test() throws IOException, ParseException, ParserConfigurationException {
+        SBOM sbom = this.TRANSLATOR.translatePath(TEST_MEDIUM_CDX_JSON);
         assertNotNull(sbom);
         assertEquals("1", sbom.getSbomVersion());
         assertEquals("1.4", sbom.getSpecVersion());
@@ -44,8 +49,8 @@ public class TranslatorCDXJSONTest {
     }
 
     @Test
-    public void build_SBOM_from_another_small_syft_json_test() throws IOException, ParseException {
-        SBOM sbom = TranslatorCDXJSON.translatorCDXJSON(TEST_ANOTHER_SMALL_SYFT_CDX_JSON);
+    public void build_SBOM_from_another_small_syft_json_test() throws IOException, ParseException, ParserConfigurationException {
+        SBOM sbom = this.TRANSLATOR.translatePath(TEST_ANOTHER_SMALL_SYFT_CDX_JSON);
         assertNotNull(sbom);
         assertEquals("1", sbom.getSbomVersion());
         assertEquals("1.4", sbom.getSpecVersion());
