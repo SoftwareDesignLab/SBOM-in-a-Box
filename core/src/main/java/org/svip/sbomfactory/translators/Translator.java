819511f4ddfd8ca2ed36539f57bb355a142e0f52
txdvse <txdvse@rit.edu>
2023-05-09 02:02:01 -0400

    Translator + Added basic constructor, contents conversion functionality, and abstract translation call

--- core/src/main/java/org/svip/sbomfactory/translators/Translator.java
+++ core/src/main/java/org/svip/sbomfactory/translators/Translator.java
@@ -1,4 +1,32 @@
 package org.svip.sbomfactory.translators;
 
-public class Translator {
+import org.svip.sbom.model.SBOM;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public abstract class Translator {
+
+    // File path of the target SBOM
+    String filePath;
+
+    public Translator(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public static String getContents(String path) {
+        // Read the contents at path into a string
+        String contents = null;
+        try {
+            contents = new String(Files.readAllBytes(Paths.get(path)));
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        return contents;
+    }
+
+    public abstract void translate(String fileContents, String filePath);
+
+
 }
