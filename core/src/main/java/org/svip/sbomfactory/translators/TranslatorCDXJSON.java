41b1d4b5c122cf2bceafb212027c6535bc7c4f57
txdvse <txdvse@rit.edu>
2023-05-09 20:06:28 -0400

    TranslatorCDXJSON.java + Changed formatting when collecting dependencies
    TranslatorCDXXML.java
    + dependencyBuilder call changed
    
    TranslatorSPDX.java
    + Changed certain data structure types relating to dependencies
    
    TranslatorCore.java
    + Now owns dependencyBuilder
    + dependencyBuilder has been reworked slightly to work for all other Translator (other translators now have their dependencies normalized)
    + dependency collection is no longer a MultiMap, but a HashMap with a nested ArrayList of dependencies.
    + Added defaultDependencies functionality
    + Added function called collectDependency to add dependency to the dependency collection
    + Added function called collectDependencies that replaces a specific key with a new list of dependencies

--- core/src/main/java/org/svip/sbomfactory/translators/TranslatorCDXJSON.java
+++ core/src/main/java/org/svip/sbomfactory/translators/TranslatorCDXJSON.java
@@ -31,7 +31,12 @@ public class TranslatorCDXJSON extends TranslatorCore {
      * @throws ParseException If file is not valid JSON
      */
     @Override
-    protected SBOM translateContents(String fileContents, String file_path) throws ParseException {
+    protected SBOM translateContents(String fileContents, String file_path) throws IOException, ParseException {
+        // Internal SBOM Object
+        SBOM sbom;
+
+        // Top level component for SBOM's dependencyTree
+        Component top_component = null;
 
         // Initialize JSON Parser
         JsonParser parser = new JsonParser();
@@ -39,22 +44,43 @@ public class TranslatorCDXJSON extends TranslatorCore {
         // Use JSON Parser to parse cdx.json file and store into cyclonedx Bom Object
         Bom json_sbom = parser.parse(fileContents.getBytes());
 
-        bom_data.put("originFormat", json_sbom.getBomFormat());
-        bom_data.put("specVersion", json_sbom.getSpecVersion());
-        bom_data.put("sbomVersion", String.valueOf(json_sbom.getVersion()));
-        bom_data.put("author", json_sbom.getMetadata().getAuthors() == null ?
-                        json_sbom.getMetadata().getTools().toString() : json_sbom.getMetadata().getAuthors().toString());
-        bom_data.put("serialNumber", json_sbom.getSerialNumber());
-        bom_data.put("timestamp" , json_sbom.getMetadata().getTimestamp().toString());
+        // Attempt to create the SBOM object. If information isn't found, cancel process and return a null object,
+        try {
+            // TODO: Add signature's later. CycloneDX BOM does a good job at dealing with them.
+            // Create new SBOM object with require data
+            sbom = new SBOM(
+                    json_sbom.getBomFormat(),
+                    json_sbom.getSpecVersion(),
+                    String.valueOf(json_sbom.getVersion()),
+                    json_sbom.getMetadata().getAuthors() == null
+                            ? json_sbom.getMetadata().getTools().toString()
+                            : json_sbom.getMetadata().getAuthors().toString(),
+                    json_sbom.getSerialNumber(),
+                    json_sbom.getMetadata().getTimestamp().toString(),
+                    null
+            );
+        } catch (Exception e) {
+            System.err.println("Error in creating internal SBOM for: " + file_path );
+            return null;
+        }
 
-        org.cyclonedx.model.Component top_component_meta = json_sbom.getMetadata().getComponent();
+        // Attempt to create the top component. If unable to create the component, print an error and continue.
+        try {
+            // Get top component information from MetaData
+            org.cyclonedx.model.Component top_component_meta = json_sbom.getMetadata().getComponent();
 
-        product_data.put("name", top_component_meta.getName());
-        product_data.put("publisher", top_component_meta.getPublisher());
-        product_data.put("version", top_component_meta.getVersion());
-        product_data.put("id", top_component_meta.getBomRef());
+            // Create top component and add it to SBOM object
+            top_component = new Component(
+                    top_component_meta.getName(),
+                    top_component_meta.getPublisher(),
+                    top_component_meta.getVersion(),
+                    top_component_meta.getBomRef()
+            );
+        } catch(Exception e) {
+            System.err.println("Could not create top-level component from MetaData.\n " +
+                    "If this is not expected please check SBOM file: " + file_path);
 
-        this.createSBOM();
+        }
 
         // Create new collection of components
         HashMap<String, Component> components = new HashMap<>();
@@ -97,14 +123,14 @@ public class TranslatorCDXJSON extends TranslatorCore {
                 components.put(new_component.getUniqueID(), new_component);
 
                 // If a top component doesn't exist, make this new component the top component
-                this.product = product == null ? new_component : product;
+                top_component = top_component == null ? new_component : top_component;
 
             }
 
         }
 
         // Add the top component to the sbom
-        if(this.sbom.getAllComponents().size() == 0) { this.sbom.addComponent(null, product); }
+        sbom.addComponent(null, top_component);
 
         // Create dependency collection
         //Map<String, List<String>> dependencies;
@@ -116,39 +142,79 @@ public class TranslatorCDXJSON extends TranslatorCore {
                             Collectors.toMap(
                                     Dependency::getRef,
                                     x -> {
-                                        // Returns dependencies as strings
                                         return x.getDependencies().stream().map(
                                                 y -> y.getRef()).collect(
                                                         Collectors.toCollection(ArrayList::new));
+                                        // Returns dependencies as strings
                                     },
                                     (x,y) -> y,
                                     HashMap::new
                             )
                     );
         } catch (NullPointerException nullPointerException) {
-            // If dependencies fail, default
+            // I failed, ourput error message and default dependencies to null
             System.err.println("Could not find dependencies from CycloneDX Object. " +
                     "Defaulting all components to point to head component. File: " + file_path);
-            dependencies.put(
-                    this.product.getUniqueID(),
-                    components.values().stream().map(x->x.getUniqueID()).collect(Collectors.toCollection(ArrayList::new))
-            );
+            dependencies = null;
         }
+
 
         // If the dependency list isn't empty, call dependencyBuilder to construct dependencyTree
         // Otherwise, default the dependencyTree by adding all subcomponents as children to the top component
         if( dependencies != null ) {
             try {
-                this.dependencyBuilder(components, this.product, null);
+                this.dependencyBuilder(components, top_component, sbom, null);
             } catch (Exception e) {
                 System.out.println("Error building dependency tree. Dependency tree may be incomplete for: " + file_path);
             }
+        } else {
+            try {
+                for (Map.Entry<String, Component> comp : components.entrySet()) {
+                    if (comp != null) { sbom.addComponent(top_component.getUUID(), comp.getValue()); }
+                }
+            } catch (Exception exception) {
+                System.out.println("Could not default default the dependency tree. Dependency tree may be empty.");
+                exception.printStackTrace();
+            }
         }
 
-        this.defaultDependencies(components, this.product);
 
-        return this.sbom;
+        this.defaultDependencies(components, top_component, sbom);
 
+
+        // Return SBOM object
+        return sbom;
     }
 
+    /**
+     * A simple recursive function to build a dependency tree out of the CDX JSON SBOM
+     *
+     * @param components    A map containing each Component with their bom-ref ID as a key
+     * @param parent        Parent component to have dependencies connected to
+     * @param sbom          The SBOM object
+     */
+    @Override
+    protected void dependencyBuilder(HashMap<String, Component> components, Component parent, SBOM sbom, Set<String> visited) {
+        // If top component is null, return. There is nothing to process.
+        if (parent == null) return;
+
+        // Get the parent's dependencies as a list
+        List<String> childRefs = dependencies.get(parent.getUniqueID());
+
+        if (visited != null) {
+            // Add this parent to the visited set
+            visited.add(parent.getUniqueID());
+        }
+
+        // Return if there are no children found
+        if(childRefs.size() == 0) return;
+
+        // Cycle through each dependency the parent component has
+        for (String childRef : childRefs) {
+            // Retrieve the component the parent has a dependency for
+            Component child = components.get(childRef);
+
+            addDependency(components, parent, child, sbom, visited);
+        }
+    }
 }
--- core/src/main/java/org/svip/sbomfactory/translators/TranslatorCore.java
+++ core/src/main/java/org/svip/sbomfactory/translators/TranslatorCore.java
@@ -1,57 +1,26 @@
 package org.svip.sbomfactory.translators;
 
 import org.cyclonedx.exception.ParseException;
+import org.cyclonedx.model.Dependency;
+import org.cyclonedx.model.Hash;
 import org.svip.sbom.model.Component;
 import org.svip.sbom.model.SBOM;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Stream;
 
-/**
- * File: TranslatorCore.java
- * A generic abstract translator that holds shared functionality
- * amongst the translators. Other translators having unique
- * parsing methods will be extended off of this translator.
- *
- * @author Tyler Drake
- * @author Dylan Mulligan
- */
 public abstract class TranslatorCore {
-
-    // Current file's extension
     protected final String FILE_EXTN;
 
-    // The internal SBOM object to be build
-    protected SBOM sbom;
-
-    // The top level component (what the SBOM is about)
-    protected Component product;
-
-    // Top level SBOM data
-    protected HashMap<String, String> bom_data;
-
-    // Top component data
-    protected HashMap<String, String> product_data;
-
-    // Map holding all components found
-    protected HashMap<String, String> components;
-
-    // Map of dependencies in SBOM between components
     protected HashMap<String, ArrayList<String>> dependencies;
 
-    /**
-     * Generic Translator core constructor.
-     *
-     * @param fileExtn The file extension
-     */
     protected TranslatorCore(String fileExtn) {
         FILE_EXTN = fileExtn;
-        bom_data = new HashMap<>();
-        product_data = new HashMap<>();
-        components = new HashMap<>();
         dependencies = new HashMap<>();
     }
 
@@ -64,53 +33,7 @@ public abstract class TranslatorCore {
      */
     protected abstract SBOM translateContents(String contents, String filePath) throws IOException, ParseException, ParserConfigurationException;
 
-    protected void createSBOM() {
-        try {
-            sbom = new SBOM(
-                    bom_data.get("format"),
-                    bom_data.get("specVersion"),
-                    bom_data.get("sbomVersion"),
-                    bom_data.get("author"),
-                    bom_data.get("serialNumber"),
-                    bom_data.get("timestamp"),
-                    null
-            );
-        } catch (Exception e) {
-            System.err.println(
-                    "Error: Internal SBOM could not be created. Cancelling translation for this SBOM. \n " +
-                    "File: " + this.FILE_EXTN + "\n"
-            );
-            e.printStackTrace();
-            sbom = null;
-            System.exit(0);
-        }
-
-        if (product == null) {
-            try {
-                product = new Component(
-                        product_data.get("name"),
-                        product_data.get("publisher"),
-                        product_data.get("version"),
-                        product_data.get("id")
-                );
-                sbom.addComponent(null, product);
-            } catch (Exception e) {
-                System.err.println("Error: Could not create top component from SBOM metadata. File: " + this.FILE_EXTN);
-            }
-        } else {
-            sbom.addComponent(null, product);
-        }
-
-    }
-
-    /**
-     * A simple recursive function to build a dependencyTree out of a list of dependencies.
-     *
-     * @param components All components from the translated SBOM file
-     * @param parent     The current parent to have the children components assigned to
-     * @param visited    Components that have been visited to prevent circular dependencies
-     */
-    protected void dependencyBuilder(HashMap<String, Component> components, Component parent, Set<String> visited) {
+    protected void dependencyBuilder(HashMap<String, Component> components, Component parent, SBOM sbom, Set<String> visited) {
 
         // If top component is null, return. There is nothing to process.
         if (parent == null) { return; }
@@ -145,40 +68,53 @@ public abstract class TranslatorCore {
                 // This means we are in the top level component
                 // Pass in a new hashset instead of the visited set
                 visited = new HashSet<>();
-                dependencyBuilder(components, child, new HashSet<>());
+                dependencyBuilder(components, child, sbom, new HashSet<>());
             }
             else {
                 // Only explore if we haven't already visited this component
                 if (!visited.contains(child.getUniqueID())) {
                     // Pass the child component as the new parent into dependencyBuilder
-                    dependencyBuilder(components, child, visited);
+                    dependencyBuilder(components, child, sbom, visited);
                 }
             }
         }
     }
 
-    /**
-     * Defaults all dependencies in the SBOM by adding them as children to the current parent component.
-     *
-     * @param components Components to be added as the children
-     * @param parent     Parent (product) component
-     */
-    protected void defaultDependencies(HashMap<String, Component> components, Component parent) {
+    protected void defaultDependencies(HashMap<String, Component> components, Component parent, SBOM sbom) {
         if (dependencies == null) { return; }
         for(ArrayList<String> defaults : dependencies.values()) {
             defaults.stream().forEach(x -> sbom.addComponent(parent.getUUID(), components.get(x)));
         }
     }
 
-    /**
-     * Adds a dependency to the dependency list. Key being the parent, value as the child.
-     *
-     * @param key   The parent component
-     * @param value The child component
-     */
-    protected void addDependency(String key, String value) {
+    protected void addDependency(HashMap<String, Component> components, Component parent, Component child, SBOM sbom, Set<String> visited) {
+        // If component is already in the dependency tree, add it as a child to the parent
+        // Else, add it to the dependency tree while setting the parent
+        if(sbom.hasComponent(child.getUUID())) {
+            parent.addChild(child.getUUID());
+        } else {
+            sbom.addComponent(parent.getUUID(), child);
+        }
+
+        if (visited == null) {
+            // This means we are in the top level component
+            // Pass in a new hashset instead of the visited set
+            visited = new HashSet<>();
+            dependencyBuilder(components, child, sbom, new HashSet<>());
+        }
+        else {
+            // Only explore if we haven't already visited this component
+            if (!visited.contains(child.getUniqueID())) {
+                // Pass the child component as the new parent into dependencyBuilder
+                dependencyBuilder(components, child, sbom, visited);
+            }
+        }
+    }
+
+    protected void collectDependency(String key, String value) {
         if (dependencies.get(key) == null || dependencies.get(key).isEmpty()) {
-            dependencies.put(key, new ArrayList<>(Arrays.asList(value)));
+            ArrayList<String> newDependencies = new ArrayList<>(Arrays.asList(value));
+            dependencies.put(key, newDependencies);
         } else {
             ArrayList<String> oldDependencies = dependencies.get(key);
             oldDependencies.add(value);
@@ -186,30 +122,12 @@ public abstract class TranslatorCore {
         }
     }
 
-    /**
-     * Defaults the dependency list. Asserts the given 'key' as the product or 'top component'
-     * and then sets all other 'values' as the children components.
-     *
-     * @param key    The component to be asserted as the product (top component)
-     * @param values The components to be assigned as children dependencies to the product
-     */
-    protected void defaultTopComponent(String key, ArrayList values) {
+    protected void collectDependencies(String key, ArrayList values) {
         values.remove(key);
         dependencies.put(key, values);
     }
 
-    /**
-     * Breaks the SBOM file into a String of contents. Then sends the contents to
-     * the respective trnslator for translation. If the file can't be broken down,
-     * return nothing.
-     *
-     * @param filePath path leading to the current SBOM file
-     * @return an SBOM object if translation is successful
-     * @throws IOException
-     * @throws ParseException
-     * @throws ParserConfigurationException
-     */
-    public SBOM translate(String filePath) throws IOException, ParseException, ParserConfigurationException {
+    public SBOM translatePath(String filePath) throws IOException, ParseException, ParserConfigurationException {
         // Read the file at filePath into a string
         String contents = null;
         try {
@@ -223,4 +141,47 @@ public abstract class TranslatorCore {
         return this.translateContents(contents, filePath);
     }
 
+    public ArrayList<SBOM> parseSBOMs(String sbomPath) throws IOException {
+        // Collection for potential SBOM files
+        final ArrayList<Path> sbom_files = new ArrayList<>();
+
+        // Collection for built SBOM Objects
+        final ArrayList<SBOM> sbom_objects = new ArrayList<>();
+
+        // Go through target folder and add files to sbom_file ArrayList
+        try (Stream<Path> paths = Files.walk(Paths.get(sbomPath))) {
+            paths.filter(Files::isRegularFile).forEach(sbom_files::add);
+        }
+
+        /*
+         * Iterate through every file found in SBOM folder. If a supported file is found, throw it into a translator.
+         * Supported formats:
+         *  - CYCLONE-DX XML
+         *  - SPDX TAG-VALUE
+         */
+        for (Path sbom_item : sbom_files) {
+            try {
+                if (sbom_item.toString().toLowerCase().endsWith(this.FILE_EXTN)) {
+                    sbom_objects.add(this.translatePath(sbom_item.toString()));
+                } else {
+                    System.err.println("\nInvalid SBOM format found in: " + sbom_item);
+                }
+                // todo deleting gitignore
+                try {
+                    Files.delete(sbom_item);
+                } catch (IOException e) {
+                    // This means it couldn't delete the file, which is fine
+                }
+            }
+            catch (Exception e){
+                System.err.println("Error translating SBOM: " + sbom_item);
+            }
+        }
+
+        // Remove all null sboms in sbom collection
+        sbom_objects.removeAll(Collections.singleton(null));
+
+        // Return ArrayList of Java SBOM Objects
+        return sbom_objects;
+    }
 }
