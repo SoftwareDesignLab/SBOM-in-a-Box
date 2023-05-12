eca6c449d949d0b756ec9f6d5459ecd81de0e144
txdvse <txdvse@rit.edu>
2023-05-12 09:10:00 -0400

    TranslatorCore.java + Now holds all main collection for managing gathered SBOM data + createSBOM now also created main component (product)
    TranslatorCDXJSON.java
    + Now collects data for SBOM and top component rather than creating them itself.
    + Uses TranslatorCore collections
    
    TranslatorCDXXML.java
    + Now collects data for SBOM and top component rather then creating them itself.
    + Uses TranslatorCore Collections
    
    TranslatorSPDX.java
    + Now collects data for SBOM and top component rather then creating them itself.
    + Uses TranslatorCore Collections
    + Reworked how translator gathers top level SBOM data
    + Now assigns top level component differently
    
    TranslatorSPDXTest.java
    + Changed a test

--- core/src/main/java/org/svip/sbomfactory/translators/TranslatorCDXJSON.java
+++ core/src/main/java/org/svip/sbomfactory/translators/TranslatorCDXJSON.java
@@ -31,12 +31,7 @@ public class TranslatorCDXJSON extends TranslatorCore {
      * @throws ParseException If file is not valid JSON
      */
     @Override
-    protected SBOM translateContents(String fileContents, String file_path) throws IOException, ParseException {
-        // Internal SBOM Object
-        SBOM sbom;
-
-        // Top level component for SBOM's dependencyTree
-        Component top_component = null;
+    protected SBOM translateContents(String fileContents, String file_path) throws ParseException {
 
         // Initialize JSON Parser
         JsonParser parser = new JsonParser();
@@ -44,43 +39,22 @@ public class TranslatorCDXJSON extends TranslatorCore {
         // Use JSON Parser to parse cdx.json file and store into cyclonedx Bom Object
         Bom json_sbom = parser.parse(fileContents.getBytes());
 
-        // Attempt to create the SBOM object. If information isn't found, cancel process and return a null object,
-        try {
-            // TODO: Add signature's later. CycloneDX BOM does a good job at dealing with them.
-            // Create new SBOM object with require data
-            sbom = new SBOM(
-                    json_sbom.getBomFormat(),
-                    json_sbom.getSpecVersion(),
-                    String.valueOf(json_sbom.getVersion()),
-                    json_sbom.getMetadata().getAuthors() == null
-                            ? json_sbom.getMetadata().getTools().toString()
-                            : json_sbom.getMetadata().getAuthors().toString(),
-                    json_sbom.getSerialNumber(),
-                    json_sbom.getMetadata().getTimestamp().toString(),
-                    null
-            );
-        } catch (Exception e) {
-            System.err.println("Error in creating internal SBOM for: " + file_path );
-            return null;
-        }
+        bom_data.put("originFormat", json_sbom.getBomFormat());
+        bom_data.put("specVersion", json_sbom.getSpecVersion());
+        bom_data.put("sbomVersion", String.valueOf(json_sbom.getVersion()));
+        bom_data.put("author", json_sbom.getMetadata().getAuthors() == null ?
+                        json_sbom.getMetadata().getTools().toString() : json_sbom.getMetadata().getAuthors().toString());
+        bom_data.put("serialNumber", json_sbom.getSerialNumber());
+        bom_data.put("timestamp" , json_sbom.getMetadata().getTimestamp().toString());
 
-        // Attempt to create the top component. If unable to create the component, print an error and continue.
-        try {
-            // Get top component information from MetaData
-            org.cyclonedx.model.Component top_component_meta = json_sbom.getMetadata().getComponent();
+        org.cyclonedx.model.Component top_component_meta = json_sbom.getMetadata().getComponent();
 
-            // Create top component and add it to SBOM object
-            top_component = new Component(
-                    top_component_meta.getName(),
-                    top_component_meta.getPublisher(),
-                    top_component_meta.getVersion(),
-                    top_component_meta.getBomRef()
-            );
-        } catch(Exception e) {
-            System.err.println("Could not create top-level component from MetaData.\n " +
-                    "If this is not expected please check SBOM file: " + file_path);
+        product_data.put("name", top_component_meta.getName());
+        product_data.put("publisher", top_component_meta.getPublisher());
+        product_data.put("version", top_component_meta.getVersion());
+        product_data.put("id", top_component_meta.getBomRef());
 
-        }
+        this.createSBOM();
 
         // Create new collection of components
         HashMap<String, Component> components = new HashMap<>();
@@ -123,14 +97,14 @@ public class TranslatorCDXJSON extends TranslatorCore {
                 components.put(new_component.getUniqueID(), new_component);
 
                 // If a top component doesn't exist, make this new component the top component
-                top_component = top_component == null ? new_component : top_component;
+                this.product = product == null ? new_component : product;
 
             }
 
         }
 
         // Add the top component to the sbom
-        sbom.addComponent(null, top_component);
+        if(this.sbom.getAllComponents().size() == 0) { this.sbom.addComponent(null, product); }
 
         // Create dependency collection
         //Map<String, List<String>> dependencies;
@@ -156,28 +130,25 @@ public class TranslatorCDXJSON extends TranslatorCore {
             System.err.println("Could not find dependencies from CycloneDX Object. " +
                     "Defaulting all components to point to head component. File: " + file_path);
             dependencies.put(
-                    top_component.getUniqueID(),
+                    this.product.getUniqueID(),
                     components.values().stream().map(x->x.getUniqueID()).collect(Collectors.toCollection(ArrayList::new))
             );
         }
-
 
         // If the dependency list isn't empty, call dependencyBuilder to construct dependencyTree
         // Otherwise, default the dependencyTree by adding all subcomponents as children to the top component
         if( dependencies != null ) {
             try {
-                this.dependencyBuilder(components, top_component, sbom, null);
+                this.dependencyBuilder(components, this.product, null);
             } catch (Exception e) {
                 System.out.println("Error building dependency tree. Dependency tree may be incomplete for: " + file_path);
             }
         }
 
+        this.defaultDependencies(components, this.product);
 
-        this.defaultDependencies(components, top_component, sbom);
+        return this.sbom;
 
-
-        // Return SBOM object
-        return sbom;
     }
 
 }
