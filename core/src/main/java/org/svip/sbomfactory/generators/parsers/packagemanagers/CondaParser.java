4a7892a235e30d9162f19210bfca56cd1b87730a
Asa Horn <horntx3@gmail.com>
2023-05-14 17:50:17 -0400

    start of dependency parser

--- core/src/main/java/org/svip/sbomfactory/generators/parsers/packagemanagers/CondaParser.java
+++ core/src/main/java/org/svip/sbomfactory/generators/parsers/packagemanagers/CondaParser.java
@@ -1,21 +1,43 @@
 package org.svip.sbomfactory.generators.parsers.packagemanagers;
 
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.svip.sbomfactory.generators.utils.ParserComponent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 
 public class CondaParser extends PackageManagerParser{
 
-    public CondaParser() { //todo
+    public CondaParser() {
         super(
-                null,
-                null,
-                null
+                "https://anaconda.org/",
+                new YAMLFactory(),
+                "\\$\\{([^\\n/\\\\]*)\\}" //regex101: https://regex101.com/r/Dy500U/1
         );
     }
+
     @Override
     protected void parseData(ArrayList<ParserComponent> components, HashMap<String, Object> data) {
-        //todo
+        // Init properties
+        this.properties = new HashMap<>();
+
+        // Init dependencies
+        this.dependencies = new HashMap<>();
+
+//        // get dependencies
+//        this.resolveProperties(
+//                this.dependencies,
+//                new HashMap(((LinkedHashMap<String, ArrayList<HashMap<String, String>>>) data.get("dependencies")))
+//                        .stream().collect(
+//                                Collectors.toMap(
+//                                        d -> d.split("=")[0].trim(),
+//                                        d -> d,
+//                                        (d1, d2) -> {
+//                                            return d2;
+//                                        }
+//                                )
+//        );
     }
 }
