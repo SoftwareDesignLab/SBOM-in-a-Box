8d222b85ef9a0c70c4e1011f5654656885111bcf
txdvse <txdvse@rit.edu>
2023-05-15 05:50:11 -0400

    TranslatorController.java + toSBOM function added that allows translation for a single SBOM with a direct file path. + docstring updates + import cleanup

--- core/src/main/java/org/svip/sbomfactory/translators/TranslatorController.java
+++ core/src/main/java/org/svip/sbomfactory/translators/TranslatorController.java
@@ -1,9 +1,7 @@
 package org.svip.sbomfactory.translators;
 
 import org.svip.sbom.model.*;
-import org.svip.sbomfactory.generators.parsers.Parser;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -23,6 +21,7 @@ import java.util.stream.Stream;
  */
 public class TranslatorController {
 
+    // Map containing all translators with their extensions
     private static final HashMap<String, TranslatorCore> EXTENSION_MAP = new HashMap<>() {{
 
         final TranslatorCDXXML translatorCDXXML = new TranslatorCDXXML();
@@ -36,50 +35,30 @@ public class TranslatorController {
 
     }};
 
-    public TranslatorController() {
-
-    }
-
 
     /**
-     * Parse an SBOM using the appropriate translator and return the object
+     * Parse an SBOM using the appropriate translator and
+     * return the object based on the contents of the file
      *
-     * @param path Path to the SBOM to translate
-     * @return SBOM object, null if failed
-     */
-
-    /**
-     * Parse an SBOM using the appropriate translator and return the object based on the contents of the file
-     *
-     * @param contents contents of the bom
      * @param filePath path to the bom
      * @return SBOM object, null if failed
      */
-    public SBOM translateContents(String contents, String filePath) {
+    public SBOM toSBOM(String filePath) {
 
         SBOM sbom = null;
 
-        // TODO check the contents of the file rather than trusting the file extension
-        // TODO address the parser exception rather than ignoring it
-
-        final String extension = filePath.substring(filePath.toLowerCase().lastIndexOf('.'));
-
         try {
+            // Get the respective translator based on the file's extension
+            final TranslatorCore translator = EXTENSION_MAP.get(
+                    filePath.substring(filePath.lastIndexOf('.') + 1)
+            );
 
-            switch (extension) {
-
-//                case ".xml"  -> sbom = TranslatorCDXXML.translatorCDXXMLContents(contents, filePath);
-
-                case ".json" -> {
-//                    if (new JSONObject(new String(Files.readAllBytes(Paths.get(filePath)))).toMap().get("bomFormat").equals("CycloneDX")) {
-//                        sbom = TranslatorCDXJSON.translateContents(contents, filePath);
-//                    }
-                }
-
-//                case ".spdx" -> sbom = TranslatorSPDX.translatorSPDXContents(contents, filePath);
-
-                default      -> System.err.println("\nInvalid SBOM format found at: " + filePath);
-
+            // If the translator exists and is an actual translator, translate and add the SBOM to the list
+            // Otherwise, print out an error stating that the SBOM is not a correct format
+            if(translator != null && translator instanceof TranslatorCore) {
+                sbom = translator.translate(filePath);
+            } else {
+                System.err.println("\nError: Invalid SBOM format found in: " + filePath);
             }
 
         }
@@ -132,7 +111,7 @@ public class TranslatorController {
                 if(translator != null && translator instanceof TranslatorCore) {
                     sbom_objects.add(translator.translate(sbom_item));
                 } else {
-                    System.err.println("\nInvalid SBOM format found in: " + sbom_item);
+                    System.err.println("\nError: Invalid SBOM format found in: " + sbom_item);
                 }
                 // todo deleting gitignore
                 try {
@@ -144,7 +123,7 @@ public class TranslatorController {
             }
             catch (Exception e){
                 // If there was any other issue translator the SBOM, output an error
-                System.err.println("Error translating SBOM: " + sbom_item);
+                System.err.println("Error: Issue with translating SBOM: " + sbom_item);
             }
         }
 
