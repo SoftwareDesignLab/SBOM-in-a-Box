29718ff1306bb59f0e7ef7514019b93c43d6bb98
Ian Dunn <itd3516@rit.edu>
2023-05-17 19:50:11 -0400

    Test all SPDX component addition cases

--- core/src/test/java/org/svip/sbomfactory/generators/generators/spdx/SPDXStoreTest.java
+++ core/src/test/java/org/svip/sbomfactory/generators/generators/spdx/SPDXStoreTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.svip.sbomfactory.generators.generators.BOMStoreTestCore;
 import org.svip.sbomfactory.generators.generators.utils.GeneratorException;
+import org.svip.sbomfactory.generators.generators.utils.License;
 import org.svip.sbomfactory.generators.utils.Debug;
 import org.svip.sbomfactory.generators.utils.ParserComponent;
 
@@ -16,8 +17,7 @@ public class SPDXStoreTest extends BOMStoreTestCore<SPDXStore> {
         super(new SPDXStore("testSerialNumber", 1, testComponents.get(0)));
 
         // Add test licenses and files analyzed to see how SPDXStore handles them
-        testComponents.get(0).addLicense("MIT");
-        testComponents.get(1).addLicense("Random External License");
+        testComponents.get(0).addLicense("Random External License");
         testComponents.get(0).addFile("Test/File/Path/source_file.xyz");
     }
 
@@ -40,35 +40,34 @@ public class SPDXStoreTest extends BOMStoreTestCore<SPDXStore> {
         Debug.log(Debug.LOG_TYPE.SUMMARY, "Correctly contains 2 components");
     }
 
-//    @Test
-//    @DisplayName("getComponents() from complete BOMStore")
-//    void getComponentsTest() throws GeneratorException {
-//        Debug.log(Debug.LOG_TYPE.INFO, "addComponent(testComponents.get(0))");
-//        bomStore.addComponent(testComponents.get(0)); // Add head component
-//        Debug.log(Debug.LOG_TYPE.INFO, "addComponent(testComponents.get(1))");
-//        bomStore.addComponent(testComponents.get(1)); // Add parent component
-//
-//        assertEquals(2, bomStore.getComponents().size());
-//        Debug.log(Debug.LOG_TYPE.SUMMARY, "Correctly contains 2 top-level components");
-//
-//        Debug.log(Debug.LOG_TYPE.INFO, "addChild(testComponents.get(0), testComponents.get(2))");
-//        bomStore.addChild(testComponents.get(0), testComponents.get(2)); // Add child component
-//
-//        assertEquals(2, bomStore.getComponents().size());
-//        Debug.log(Debug.LOG_TYPE.SUMMARY, "Correctly contains 2 top-level components");
-//    }
+    @Test
+    @DisplayName("addComponent()")
+    void addComponentTest() {
+        Debug.log(Debug.LOG_TYPE.INFO, "addComponent(testComponents.get(0))");
+        bomStore.addComponent(testComponents.get(0)); // Add head component
 
-//    @Test
-//    @DisplayName("addComponent()")
-//    void addComponentTest() {
-//        Debug.log(Debug.LOG_TYPE.INFO, "addComponent(testComponents.get(0))");
-//        bomStore.addComponent(testComponents.get(0)); // Add head component
-//
-//        assertTrue(bomStore.getAllComponents().contains(testComponents.get(0)));
-//        assertEquals(1, bomStore.getAllComponents().size());
-//        Debug.log(Debug.LOG_TYPE.SUMMARY, "Correctly contains testComponents.get(0)");
-//    }
-//
+        assertTrue(bomStore.getAllComponents().contains(testComponents.get(0)));
+        assertEquals(1, bomStore.getAllComponents().size());
+        Debug.log(Debug.LOG_TYPE.SUMMARY, "Correctly contains testComponents.get(0)");
+
+        ParserComponent component = bomStore.getAllComponents().stream().toList().get(0);
+
+        String spdxId = component.getSPDXID();
+        assertNotNull(spdxId);
+        Debug.log(Debug.LOG_TYPE.SUMMARY, "SPDX Package has ID: " + spdxId);
+
+        License external = bomStore.getExternalLicenses().stream().toList().get(0);
+        assertEquals(1, bomStore.getExternalLicenses().size());
+        Debug.log(Debug.LOG_TYPE.SUMMARY, "SPDX Package correctly contains resolved external license with ID: "
+                + external.getSpdxLicense());
+
+        assertTrue(bomStore.getDocumentDescribes().contains(spdxId));
+        Debug.log(Debug.LOG_TYPE.SUMMARY, "SPDX Document Description field contains ID: " + spdxId);
+
+        assertEquals(1, bomStore.getPackages().size());
+        Debug.log(Debug.LOG_TYPE.SUMMARY, "SPDX Packages contains added component");
+    }
+
 //    @Test
 //    @DisplayName("addChild() with valid parent")
 //    void addChildValidTest() throws GeneratorException {
@@ -95,41 +94,6 @@ public class SPDXStoreTest extends BOMStoreTestCore<SPDXStore> {
 //        });
 //
 //        Debug.log(Debug.LOG_TYPE.SUMMARY, "Correctly throws GeneratorException");
-//    }
-//
-//    @Test
-//    @DisplayName("getChildren() with children")
-//    void getChildrenTest() throws GeneratorException {
-//        Debug.log(Debug.LOG_TYPE.INFO, "addComponent(testComponents.get(0))");
-//        bomStore.addComponent(testComponents.get(0)); // Add head component
-//
-//        Debug.log(Debug.LOG_TYPE.INFO, "addChild(testComponents.get(0), testComponents.get(1))");
-//        bomStore.addChild(testComponents.get(0), testComponents.get(1)); // Add child component
-//
-//        Debug.log(Debug.LOG_TYPE.INFO, "addChild(testComponents.get(0), testComponents.get(2))");
-//        bomStore.addChild(testComponents.get(0), testComponents.get(2)); // Add child component
-//
-//        Debug.log(Debug.LOG_TYPE.INFO, "getChildren(testComponents.get(0).getUUID())");
-//        List<ParserComponent> children = bomStore.getChildren(testComponents.get(0).getUUID());
-//
-//        assertTrue(children.contains(testComponents.get(1)));
-//        Debug.log(Debug.LOG_TYPE.SUMMARY, "Child list correctly contains testComponents.get(1)");
-//
-//        assertTrue(children.contains(testComponents.get(2)));
-//        Debug.log(Debug.LOG_TYPE.SUMMARY, "Child list correctly contains testComponents.get(2)");
-//    }
-//
-//    @Test
-//    @DisplayName("getChildren() without children")
-//    void getChildrenWithoutChildrenTest() {
-//        Debug.log(Debug.LOG_TYPE.INFO, "addComponent(testComponents.get(0))");
-//        bomStore.addComponent(testComponents.get(0)); // Add head component
-//
-//        Debug.log(Debug.LOG_TYPE.INFO, "getChildren(testComponents.get(0).getUUID())");
-//        List<ParserComponent> children = bomStore.getChildren(testComponents.get(0).getUUID());
-//
-//        assertEquals(0, children.size());
-//        Debug.log(Debug.LOG_TYPE.SUMMARY, "Child list is empty");
 //    }
 }
 
