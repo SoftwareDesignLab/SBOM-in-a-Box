7680af5da2cdb4764c37197c1f3b3d13513b2a84
Ian Dunn <itd3516@rit.edu>
2023-05-25 14:28:22 -0400

    Merge all tests together

--- api/src/test/java/org/svip/api/GenerateFromAPITest.java
+++ api/src/test/java/org/svip/api/GenerateFromAPITest.java
@@ -3,12 +3,16 @@ package org.svip.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.svip.sbomfactory.generators.utils.Debug;
 import org.svip.sbomfactory.generators.utils.generators.GeneratorSchema;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
@@ -20,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * - generateTest: Test that the API can generate three SBOMs
  *
  * @author Juan Francisco Patino
+ * @author Ian Dunn
  */
 public class GenerateFromAPITest {
 
@@ -28,13 +33,73 @@ public class GenerateFromAPITest {
      */
     private SVIPApiController ctrl;
 
+    private final static String TESTCONTENTSARRAY_LENGTH1 = "[\"Example File Contents\"]";
+    private final static String TESTFILEARRAY_LENGTH1 = "[\"TestFileName1.java\"]";
+    private final static String TESTCONTENTSARRAY_LENGTH2 = "[\"Example File Contents 1\", \"Example File Contents 2\"]";
+    private final static String TESTFILEARRAY_LENGTH2 = "[\"src/java/SBOM/sbom2/TestFileName1.java\", \"src/java/SBOM/TestFileName2.java\"]";
+    private final static String CDX_SCHEMA = "CycloneDX";
+    private final static String INVALID_SCHEMA = "Invalid Test Schema";
+    private final static String JSON_FORMAT = "JSON";
+    private final static String INVALID_FORMAT = "GIF";
+
+    public GenerateFromAPITest() {
+        Debug.enableSummary();
+        ctrl = new SVIPApiController();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyContentsArrayTest(String fileContents) {
+        ResponseEntity<String> response = ctrl.generate(fileContents, TESTFILEARRAY_LENGTH1, CDX_SCHEMA, JSON_FORMAT);
+        logTestRequest(fileContents, TESTFILEARRAY_LENGTH1, CDX_SCHEMA, JSON_FORMAT);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyFileNamesArrayTest(String fileNames) {
+        ResponseEntity<String> response = ctrl.generate(TESTCONTENTSARRAY_LENGTH1, fileNames, CDX_SCHEMA, JSON_FORMAT);
+        logTestRequest(TESTCONTENTSARRAY_LENGTH1, fileNames, CDX_SCHEMA, JSON_FORMAT);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void mismatchedFileInfoTest() {
+        ResponseEntity<String> response = ctrl.generate(TESTCONTENTSARRAY_LENGTH2, TESTFILEARRAY_LENGTH1, CDX_SCHEMA, JSON_FORMAT);
+        logTestRequest(TESTCONTENTSARRAY_LENGTH2, TESTFILEARRAY_LENGTH1, CDX_SCHEMA, JSON_FORMAT);
+//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void invalidSchemaNameTest() {
+        ResponseEntity<String> response = ctrl.generate(TESTCONTENTSARRAY_LENGTH2, TESTFILEARRAY_LENGTH2, INVALID_SCHEMA, JSON_FORMAT);
+        logTestRequest(TESTCONTENTSARRAY_LENGTH2, TESTFILEARRAY_LENGTH2, INVALID_SCHEMA, JSON_FORMAT);
+//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode()); TODO test nulls, empty string, default to CDX
+    }
+
+    @Test
+    void invalidFormatNameTest() {
+        ResponseEntity<String> response = ctrl.generate(TESTCONTENTSARRAY_LENGTH2, TESTFILEARRAY_LENGTH2, CDX_SCHEMA, INVALID_FORMAT);
+        logTestRequest(TESTCONTENTSARRAY_LENGTH2, TESTFILEARRAY_LENGTH2, CDX_SCHEMA, INVALID_FORMAT);
+//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode()); TODO test nulls, empty string, test default to schema default, check if valid format/can be used in schema
+    }
+
+    @Test
+    void malformedContentsArrayTest() {
+        // TODO
+    }
+
+    @Test
+    void malformedFileArrayTest() {
+        // TODO
+    }
+
     /**
      * Test that the API can Merge three SBOMs
      * @throws IOException If the SBOM merging is broken
      */
     @Test
-    @Disabled // TODO merge this and generatesbom test
-    public void generateTest() throws IOException{
+    public void generateTest() throws IOException {
 
         String[] input = APITestInputInitializer.testInput();
 
@@ -59,20 +124,22 @@ public class GenerateFromAPITest {
                     ResponseEntity<String> report = ctrl.generate(contentsString, fileNamesString, schema.toString().toUpperCase(), format.toString().toUpperCase());
                     assertNotNull(report.getBody());
                     Debug.log(Debug.LOG_TYPE.SUMMARY, "PASSED " + schema + " " + format + "!\n-----------------\n");
+                    Debug.log(Debug.LOG_TYPE.SUMMARY, "Generated SBOM:\n" + report.getBody());
+                    // TODO assert translators can parse this back in
                 }
             }
         }
-
     }
 
-    /**
-     * SETUP: Start API before testing
-     */
-    @BeforeEach
-    public void setup(){
-
-        ctrl = new SVIPApiController();
-
+    private void logTestRequest(String fileContents, String fileNames, String schema, String format) {
+        String parameters = String.format("""
+                {
+                    fileContents: "%s",
+                    fileNames: "%s",
+                    schema: "%s",
+                    format: "%s",
+                }
+                """, fileContents, fileNames, schema, format);
+        Debug.log(Debug.LOG_TYPE.SUMMARY, "POST /SVIP/generateSBOM:\n" + parameters);
     }
-
 }
