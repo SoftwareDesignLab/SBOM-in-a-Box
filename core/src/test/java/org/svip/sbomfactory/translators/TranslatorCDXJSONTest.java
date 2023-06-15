ab7378bfe879793cd14b363ec5704efc9238b9e1
Ian Dunn <itd3516@rit.edu>
2023-06-15 16:14:35 -0400

    Migrate translator tests from plugfest

--- core/src/test/java/org/svip/sbomfactory/translators/TranslatorCDXJSONTest.java
+++ core/src/test/java/org/svip/sbomfactory/translators/TranslatorCDXJSONTest.java
@@ -1,14 +1,17 @@
 package org.svip.sbomfactory.translators;
 
-import org.cyclonedx.exception.ParseException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.svip.sbom.model.Component;
 import org.svip.sbom.model.SBOM;
+import org.svip.sbomfactory.generators.utils.Debug;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.svip.sbomfactory.translators.Utils.checkForAppTools;
+import static org.svip.sbomfactory.translators.Utils.noLicensesCheck;
 
 /**
  * File: TranslatorCDXJSONTest.java
@@ -18,42 +21,115 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  */
 public class TranslatorCDXJSONTest extends TranslatorTestCore<TranslatorCDXJSON> {
 
-    public static final String TEST_SMALL_CDX_JSON = "src/test/java/org/svip/sbomfactory/translators/sample_boms/cdx_json/sbom.alpine.json";
+    public static final String TEST_SMALL_CDX_JSON = "src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/sbom.alpine.json";
+    public static final String TEST_SMALL_CDX_JSON_NOMETADATA = "src/test/java/org/nvip/plugfest/tooling/sample_boms" +
+            "/cdx_json/sbom.alpine.nometadata.json";
 
-    public static final String TEST_MEDIUM_CDX_JSON ="src/test/java/org/svip/sbomfactory/translators/sample_boms/cdx_json/trivy-0.39.0_celery-3.1.cdx.json";
+    public static final String TEST_MEDIUM_CDX_JSON ="src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/trivy-0.39.0_celery-3.1.cdx.json";
 
-    public static final String TEST_ANOTHER_SMALL_SYFT_CDX_JSON = "src/test/java/org/svip/sbomfactory/translators/sample_boms/cdx_json/cdx.json";
+    public static final String TEST_ANOTHER_SMALL_SYFT_CDX_JSON = "src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/cdx.json";
 
-    // Create instance of translator on test initialization
+    public static final String TEST_SMALL_SYFT_CDX_JSON_HASHES = "src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/bom.json";
+
+    public static final String TEST_CDX_JSON = "src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/cdxgen-8.4.6-source.json";
+    public static final String TEST_CDX_JSON_NO_LICENSES = "src/test/java/org/nvip/plugfest/tooling/sample_boms/cdx_json/CDXMavenPlugin_build_cdx.json";
+
     protected TranslatorCDXJSONTest() {
         super(new TranslatorCDXJSON());
     }
 
     @Test
-    public void build_SBOM_from_small_cdx_json_test() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
-        SBOM sbom = this.TRANSLATOR.translate(TEST_SMALL_CDX_JSON);
+    public void noLicenseTest() throws TranslatorException {
+        SBOM sbom = this.TRANSLATOR.translate(TEST_CDX_JSON_NO_LICENSES);
+        assertNotNull(sbom);
+
+        assertTrue(noLicensesCheck(sbom));
+
+        assertEquals("1", sbom.getSbomVersion());
+        assertEquals("1.4", sbom.getSpecVersion());
+        assertEquals(17, sbom.getAllComponents().size());
+        assertEquals(1, sbom.getMetadata().size());
+        assertEquals(1, sbom.getAppTools().size());
+        assertFalse(checkForAppTools(sbom));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { TEST_SMALL_CDX_JSON, TEST_SMALL_CDX_JSON_NOMETADATA })
+    public void build_SBOM_from_small_cdx_json_test(String pathToSBOM) throws TranslatorException {
+        SBOM sbom = this.TRANSLATOR.translate(pathToSBOM);
         assertNotNull(sbom);
         assertEquals("1", sbom.getSbomVersion());
         assertEquals("1.4", sbom.getSpecVersion());
-        assertEquals(18, sbom.getAllComponents().size());
+        assertEquals(17, sbom.getAllComponents().size()); // TODO ensure no duplicates added?
+
+        if(pathToSBOM.equals(TEST_SMALL_CDX_JSON)) {
+            assertEquals(1, sbom.getMetadata().size());
+            assertEquals(2, sbom.getAppTools().size());
+        }
+        else{
+            assertEquals(0, sbom.getMetadata().size());
+            assertEquals(1, sbom.getAppTools().size());
+        }
+        assertFalse(checkForAppTools(sbom));
     }
 
     @Test
-    public void build_SBOM_from_medium_cdx_json_test() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
+    public void build_SBOM_from_medium_cdx_json_test() throws TranslatorException {
         SBOM sbom = this.TRANSLATOR.translate(TEST_MEDIUM_CDX_JSON);
         assertNotNull(sbom);
         assertEquals("1", sbom.getSbomVersion());
         assertEquals("1.4", sbom.getSpecVersion());
         assertEquals(124, sbom.getAllComponents().size());
+        assertEquals(1, sbom.getMetadata().size());
+        assertEquals(1, sbom.getAppTools().size());
+        assertFalse(checkForAppTools(sbom));
+
     }
 
     @Test
-    public void build_SBOM_from_another_small_syft_json_test() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
+    public void build_SBOM_from_another_small_syft_json_test() throws TranslatorException {
         SBOM sbom = this.TRANSLATOR.translate(TEST_ANOTHER_SMALL_SYFT_CDX_JSON);
         assertNotNull(sbom);
         assertEquals("1", sbom.getSbomVersion());
         assertEquals("1.4", sbom.getSpecVersion());
         assertEquals(48, sbom.getAllComponents().size());
+        assertEquals(1, sbom.getMetadata().size());
+        assertEquals(1, sbom.getAppTools().size());
+        assertFalse(checkForAppTools(sbom));
+    }
+
+    @Test
+    @DisplayName("Test for null UIDs")
+    void nullUIDTest() throws TranslatorException {
+        SBOM sbom = this.TRANSLATOR.translate(TEST_MEDIUM_CDX_JSON);
+
+        for (Component component : sbom.getAllComponents()) {
+            Debug.logBlockTitle(component.getName());
+
+            assertFalse(component.getCpes().contains(null));
+            Debug.log(Debug.LOG_TYPE.SUMMARY, "Component does not contain null CPEs");
+
+            assertFalse(component.getPurls().contains(null));
+            Debug.log(Debug.LOG_TYPE.SUMMARY, "Component does not contain null PURLs");
+
+            assertFalse(component.getSwids().contains(null));
+            Debug.log(Debug.LOG_TYPE.SUMMARY, "Component does not contain null SWIDs");
+
+            Debug.logBlock();
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {TEST_CDX_JSON})
+   // @DisplayName("Test on PlugFest Audit excel line 24") // todo
+    @DisplayName("FIX")
+    @Disabled
+    public void build_SBOM_cdx_json_test(String pathToSBOM) throws TranslatorException {
+        SBOM sbom = this.TRANSLATOR.translate(pathToSBOM);
+        assertNotNull(sbom);
+        assertEquals("1", sbom.getSbomVersion());
+        assertEquals("1.4", sbom.getSpecVersion());
+        assertEquals(5, sbom.getAllComponents().size());
     }
 
 }
--- core/src/test/java/org/svip/sbomfactory/translators/TranslatorSPDXTest.java
+++ core/src/test/java/org/svip/sbomfactory/translators/TranslatorSPDXTest.java
@@ -1,14 +1,19 @@
 package org.svip.sbomfactory.translators;
 
-import org.cyclonedx.exception.ParseException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.svip.sbom.model.Component;
 import org.svip.sbom.model.SBOM;
+import org.svip.sbomfactory.generators.utils.Debug;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.svip.sbomfactory.translators.Utils.noLicensesCheck;
 
 /**
  * File: SPDXParser.java
@@ -22,98 +27,159 @@ public class TranslatorSPDXTest extends TranslatorTestCore<TranslatorSPDX> {
      * Test Constants
      */
 
-    private static final String TEST_SPDX_v2_3_SBOM = "src/test/java/org/svip/sbomfactory/translators/sample_boms/sbom.alpine.2-3.spdx";
+    private static final String TEST_SPDX_v2_3_SBOM = "src/test/java/org/nvip/plugfest/tooling/sample_boms/sbom.alpine.2-3.spdx";
+    private static final String TEST_SPDX_v2_3_SBOM_NOMETADATA = "src/test/java/org/nvip/plugfest/tooling/sample_boms" +
+            "/sbom.alpine.2-3.nometadata.spdx";
 
-    private static final String TEST_SPDX_v2_2_SBOM = "src/test/java/org/svip/sbomfactory/translators/sample_boms/sbom.docker.2-2.spdx";
+    private static final String TEST_SPDX_v2_3_SBOM_LICENSINGINFO = "src/test/java/org/nvip/plugfest/tooling" +
+            "/sample_boms/sbom.alpine.2-3.licensinginfo.spdx";
 
-    private static final String TEST_SPDX_SMALL_v2_2_SBOM = "src/test/java/org/svip/sbomfactory/translators/sample_boms/sbom.docker.2-2_small.spdx";
+    private static final String TEST_SPDX_v2_2_SBOM = "src/test/java/org/nvip/plugfest/tooling/sample_boms/sbom.docker.2-2.spdx";
 
-    private static final String TEST_SPDX_LARGE_v2_3_SBOM = "src/test/java/org/svip/sbomfactory/translators/sample_boms/sbom.python.2-3.spdx";
+    private static final String TEST_SPDX_LARGE_v2_3_SBOM = "src/test/java/org/nvip/plugfest/tooling/sample_boms/sbom.python.2-3.spdx";
 
-    private static final String TEST_SBOM_DOESNT_EXIST = "src/test/java/org/svip/sbomfactory/translators/sample_boms/sbom.idontexist.spdx";
+    private static final String TEST_SBOM_DOESNT_EXIST = "src/test/java/org/nvip/plugfest/tooling/sample_boms/sbom.idontexist.spdx";
 
-    private static final String TEST_SBOM_SPDX_NO_COMPONENTS = "src/test/java/org/svip/sbomfactory/translators/sample_boms/sbom.nocomponents.2-3.spdx";
+    private static final String TEST_SBOM_SPDX_NO_COMPONENTS = "src/test/java/org/nvip/plugfest/tooling/sample_boms/sbom.nocomponents.2-3.spdx";
 
-    private static final String TEST_SBOM_SPDX_EMPTY = "src/test/java/org/svip/sbomfactory/translators/sample_boms/sbom.empty.2-3.spdx";
+    private static final String TEST_SBOM_SPDX_EMPTY = "src/test/java/org/nvip/plugfest/tooling/sample_boms/sbom.empty.2-3.spdx";
 
-    protected TranslatorSPDXTest() {
-        super(new TranslatorSPDX());
-    }
+    public TranslatorSPDXTest() { super(new TranslatorSPDX()); }
 
     /**
      * Tests
      */
 
-    @Test
-    public void builder_makes_SBOM_test() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
-        SBOM test = this.TRANSLATOR.translate(TEST_SPDX_v2_3_SBOM);
+    @ParameterizedTest
+    @ValueSource(strings = { TEST_SPDX_v2_3_SBOM, TEST_SPDX_v2_3_SBOM_NOMETADATA })
+    public void builder_makes_SBOM_test(String pathToSBOM) throws TranslatorException {
+        SBOM test = this.TRANSLATOR.translate(pathToSBOM);
         assertNotNull(test);
         Assertions.assertEquals(SBOM.Type.SPDX, test.getOriginFormat());
         assertEquals("1", test.getSbomVersion());
-        assertEquals("2.3", test.getSpecVersion());
+        assertEquals("SPDX-2.3", test.getSpecVersion());
         assertEquals(17, test.getAllComponents().size());
     }
 
     @Test
-    public void builder_makes_SBOM_from_SPDX_2_2_test() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
+    public void builder_makes_SBOM_from_SPDX_2_2_test() throws TranslatorException {
         SBOM test = this.TRANSLATOR.translate(TEST_SPDX_v2_2_SBOM);
         assertNotNull(test);
         assertEquals(SBOM.Type.SPDX, test.getOriginFormat());
         assertEquals("1", test.getSbomVersion());
-        assertEquals("2.2", test.getSpecVersion());
-        assertEquals(137, test.getAllComponents().size());
+        assertEquals("SPDX-2.2", test.getSpecVersion());
+        assertEquals(138, test.getAllComponents().size());
+        assertTrue(noLicensesCheck(test));
     }
-
-//    @Test
-//    public void builder_makes_SBOM_from_small_SPDX_2_2_test() throws IOException, ParseException, ParserConfigurationException {
-//        SBOM test = this.TRANSLATOR.translate(TEST_SPDX_SMALL_v2_2_SBOM); TODO this file doesn't exist??
-//        assertNotNull(test);
-//        assertEquals("1", test.getSbomVersion());
-//        assertEquals("2.2", test.getSpecVersion());
-//        assertEquals(28, test.getAllComponents().size());
-//    }
 
 
     @Test
-    public void builder_makes_large_SBOM_test() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
+    public void builder_makes_large_SBOM_test() throws TranslatorException {
         SBOM test = this.TRANSLATOR.translate(TEST_SPDX_LARGE_v2_3_SBOM);
         assertNotNull(test);
         assertEquals(SBOM.Type.SPDX, test.getOriginFormat());
         assertEquals("1", test.getSbomVersion());
-        assertEquals("2.3", test.getSpecVersion());
+        assertEquals("SPDX-2.3", test.getSpecVersion());
         assertEquals(433, test.getAllComponents().size());
     }
 
 
     @Test
-    public void builder_does_not_make_SBOM_from_blank_path() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
+    public void builder_does_not_make_SBOM_from_blank_path() throws TranslatorException {
         SBOM test = this.TRANSLATOR.translate("");
         assertNull(test);
     }
 
     @Test
-    public void builder_does_not_make_SBOM_from_non_existing_file() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
+    public void builder_does_not_make_SBOM_from_non_existing_file() throws TranslatorException {
         SBOM test = this.TRANSLATOR.translate(TEST_SBOM_DOESNT_EXIST);
         assertNull(test);
     }
 
     @Test
-    public void builder_parses_SBOM_with_no_components() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
+    public void builder_parses_SBOM_with_no_components() throws TranslatorException {
         SBOM test = this.TRANSLATOR.translate(TEST_SBOM_SPDX_NO_COMPONENTS);
         assertNotNull(test);
         assertEquals(SBOM.Type.SPDX, test.getOriginFormat());
         assertEquals("1", test.getSbomVersion());
-        assertEquals("2.3", test.getSpecVersion());
+        assertEquals("SPDX-2.3", test.getSpecVersion());
         assertEquals(1, test.getAllComponents().size());
     }
 
     @Test
-    public void builder_parses_SBOM_that_is_empty() throws IOException, ParseException, ParserConfigurationException, TranslatorException {
+    public void builder_parses_SBOM_that_is_empty() throws TranslatorException {
         SBOM test = this.TRANSLATOR.translate(TEST_SBOM_SPDX_EMPTY);
         assertNotNull(test);
         assertEquals(SBOM.Type.SPDX, test.getOriginFormat());
         assertEquals("1", test.getSbomVersion());
         assertEquals(null, test.getSpecVersion());
         assertEquals(1, test.getAllComponents().size());
+    }
+
+    @Test
+    @DisplayName("Single-Line Licensing Information")
+    void extractedLicenseOneLineTest() throws TranslatorException {
+        SBOM sbom = this.TRANSLATOR.translate(TEST_SPDX_v2_3_SBOM_LICENSINGINFO);
+        for(Component component : sbom.getAllComponents()) {
+            if(!component.getName().equals("ssl_client")) continue;
+
+            Set<String> licenses = component.getLicenses();
+            assertEquals(1, licenses.size());
+            Debug.log(Debug.LOG_TYPE.SUMMARY, "Found licenses in component ssl_client: " + licenses);
+
+            Map<String, Map<String, String>> extractedLicenses = component.getExtractedLicenses();
+            assertEquals(1, extractedLicenses.size());
+            Debug.log(Debug.LOG_TYPE.SUMMARY,
+                    "Found extracted licenses in component ssl_client: " + extractedLicenses);
+
+            Map<String, String> attributes = extractedLicenses.get("LicenseRef-TestLicense-0");
+            assertEquals("Test License 0", attributes.get("name"));
+            assertEquals("Test license text", attributes.get("text"));
+            assertEquals("https://google.com", attributes.get("crossRef"));
+        }
+    }
+
+    @Test
+    @DisplayName("Multi-Line Licensing Information")
+    void extractedLicensingMultiLineTest() throws TranslatorException {
+        SBOM sbom = this.TRANSLATOR.translate(TEST_SPDX_v2_3_SBOM_LICENSINGINFO);
+        for(Component component : sbom.getAllComponents()) {
+            if(!component.getName().equals("musl")) continue;
+
+            Set<String> licenses = component.getLicenses();
+            assertEquals(1, licenses.size());
+            Debug.log(Debug.LOG_TYPE.SUMMARY, "Found licenses in component musl: " + licenses);
+
+            Map<String, Map<String, String>> extractedLicenses = component.getExtractedLicenses();
+            assertEquals(1, extractedLicenses.size());
+            Debug.log(Debug.LOG_TYPE.SUMMARY,
+                    "Found extracted licenses in component musl: " + extractedLicenses);
+
+            Map<String, String> attributes = extractedLicenses.get("LicenseRef-TestLicense-1");
+            assertEquals("Test License 1", attributes.get("name"));
+            assertTrue(attributes.get("text").contains("\n"));
+            assertEquals("https://bing.com", attributes.get("crossRef"));
+        }
+    }
+
+    @Test
+    @DisplayName("Test for null UIDs")
+    void nullUIDTest() throws TranslatorException {
+        SBOM sbom = this.TRANSLATOR.translate(TEST_SPDX_LARGE_v2_3_SBOM);
+
+        for (Component component : sbom.getAllComponents()) {
+            Debug.logBlockTitle(component.getName());
+
+            assertFalse(component.getCpes().contains(null));
+            Debug.log(Debug.LOG_TYPE.SUMMARY, "Component does not contain null CPEs");
+
+            assertFalse(component.getPurls().contains(null));
+            Debug.log(Debug.LOG_TYPE.SUMMARY, "Component does not contain null PURLs");
+
+            assertFalse(component.getSwids().contains(null));
+            Debug.log(Debug.LOG_TYPE.SUMMARY, "Component does not contain null SWIDs");
+
+            Debug.logBlock();
+        }
     }
 }
