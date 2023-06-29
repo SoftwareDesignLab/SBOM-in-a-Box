464427e605c25fe9f9deb151d509d7d7bde86119
dlg1206 <dlg1206@g.rit.edu>
2023-06-29 14:59:10 -0400

    added schema specific interfaces and updated descriptions

--- core/src/main/java/org/svip/sbom/model/interfaces/schemas/CycloneDX14/CDX14Schema.java
+++ core/src/main/java/org/svip/sbom/model/interfaces/schemas/CycloneDX14/CDX14Schema.java
@@ -1,10 +1,13 @@
-package org.svip.sbom.model.interfaces.generics.schemas;
+package org.svip.sbom.model.interfaces.schemas.CycloneDX14;
 
 import org.svip.sbom.model.interfaces.generics.SBOM;
 
 /**
  * File: CDX14Schema.java
+ *  <p>
  * CycloneDX 1.4 specific fields
+ * <p>
+ * Source: <a href="https://cyclonedx.org/docs/1.4/json/">https://cyclonedx.org/docs/1.4/json/</a>
  *
  * @author Derek Garcia
  */
--- core/src/main/java/org/svip/sbom/model/interfaces/schemas/SPDX23/SPDX23Schema.java
+++ core/src/main/java/org/svip/sbom/model/interfaces/schemas/SPDX23/SPDX23Schema.java
@@ -1,10 +1,11 @@
-package org.svip.sbom.model.interfaces.generics.schemas;
-
+package org.svip.sbom.model.interfaces.schemas.SPDX23;
 import org.svip.sbom.model.interfaces.generics.SBOM;
 
 /**
  * File: SPDX23Schema.java
  * SPDX 2.3 specific fields
+ * <p>
+ * Source: <a href="https://spdx.github.io/spdx-spec/v2.3/">https://spdx.github.io/spdx-spec/v2.3/"</a>
  *
  * @author Derek Garcia
  */
@@ -14,6 +15,10 @@ public interface SPDX23Schema extends SBOM {
      + getSnippets(): Set<Snippet>
      + getAdditionalLicenseInformation(): Set<LicenseInfo>
      + getAnnotaionInformation(): Set<Annotation>
+     */
+
+    /**
+     * @return Version of the SPDX License List used when the SPDX document was created
      */
     String getSPDXLicenseListVersion();
 }
