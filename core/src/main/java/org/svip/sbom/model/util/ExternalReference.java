0e037ad767f05bff861303aec3a9b0d57afc11fe
dlg1206 <dlg1206@g.rit.edu>
2023-06-29 12:44:05 -0400

    added external reference

---
+++ core/src/main/java/org/svip/sbom/model/util/ExternalReference.java
@@ -0,0 +1,83 @@
+package org.svip.sbom.model.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * File: ExternalReference.java
+ * External Reference model for references outside the SBOM
+ *
+ * @author Derek Garcia
+ */
+public class ExternalReference {
+
+    private final String url;
+    private final String type;
+    private String category;
+    private final Map<String, String> hashes = new HashMap<>();
+
+    /**
+     * CDX Style External Reference constructor
+     *
+     * @param url URL to reference
+     * @param type Type of External Reference
+     */
+    public ExternalReference(String url, String type){
+        this.url = url;
+        this.type = type;
+    }
+
+    /**
+     * SPDX Style External Reference constructor
+     *
+     * @param category Category of External Reference
+     * @param url URL to reference
+     * @param type Type of External Reference
+     */
+    public ExternalReference(String category, String url, String type){
+        this.category = category;
+        this.url = url;
+        this.type = url;
+    }
+
+
+    /**
+     * @param algorithm Hash Algorithm
+     * @param hash Hash value
+     */
+    public void addHash(String algorithm, String hash){
+        this.hashes.put(algorithm, hash);
+    }
+
+    ///
+    /// getters
+    ///
+
+    /**
+     * @return url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @return type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @return category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @return Hashes
+     */
+    public Map<String, String> getHashes() {
+        return hashes;
+    }
+}
