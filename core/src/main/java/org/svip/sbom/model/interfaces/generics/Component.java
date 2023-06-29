c3f4ad7f5f270d5bb766d4d5f48bac13e20e1e1a
dlg1206 <dlg1206@g.rit.edu>
2023-06-29 13:18:53 -0400

    added component generic

---
+++ core/src/main/java/org/svip/sbom/model/interfaces/generics/Component.java
@@ -0,0 +1,50 @@
+package org.svip.sbom.model.interfaces.generics;
+
+import java.util.Map;
+
+/**
+ * file: Component.java
+ * Generic component details that many componentss share
+ *
+ * @author Derek Garcia
+ */
+public interface Component {
+    /**
+     * @return Type of component
+     */
+    String getType();
+
+    /**
+     * @return Unique identifier of the component
+     */
+    String getUID();
+
+    /**
+     * @return Author of the component
+     */
+    String getAuthor();
+
+
+    /**
+     * @return Name of the component
+     */
+    String getName();
+
+    /**
+     * todo
+     * @return component Licenses
+     */
+//    LicenseCollection getLicenses();
+
+
+    /**
+     * @return Get component copyright
+     */
+    String getCopyright();
+
+    /**
+     * @return Get hashes
+     */
+    Map<String, String> getHashes();
+
+}
