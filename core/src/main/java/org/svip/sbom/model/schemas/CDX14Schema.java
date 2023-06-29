e6b078bdd0c6f14ff5b7e608c10ccd096b25ed13
dlg1206 <dlg1206@g.rit.edu>
2023-06-29 12:54:57 -0400

    added CDX14 interface

---
+++ core/src/main/java/org/svip/sbom/model/schemas/CDX14Schema.java
@@ -0,0 +1,18 @@
+package org.svip.sbom.model.schemas;
+
+import org.svip.sbom.model.generics.SBOM;
+
+/**
+ * File: CDX14Schema.java
+ * CycloneDX 1.4 specific fields
+ *
+ * @author Derek Garcia
+ */
+public interface CDX14Schema extends SBOM {
+
+    /* todo
+    + getServices(): Set<Service>
+    + getCompositions(): Set<Composition>
+    + getSignature(): Signature
+     */
+}
