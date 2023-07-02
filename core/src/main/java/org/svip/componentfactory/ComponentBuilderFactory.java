b018aa7e162b9a7fc29c984f201f379ca2ade15d
Matthew Morrison <msm8275@rit.edu>
2023-07-02 08:16:16 -0400

    Implementation of ComponentBuilderFactory interface

---
+++ core/src/main/java/org/svip/componentfactory/ComponentBuilderFactory.java
@@ -0,0 +1,18 @@
+package org.svip.componentfactory;
+
+import org.svip.builders.component.ComponentBuilder;
+
+/**
+ * file: ComponentBuilderFactory.java
+ * Interface that aids in building components/packages in an SBOM
+ *
+ * @author Matthew Morrison
+ */
+public interface ComponentBuilderFactory extends ComponentBuilder {
+
+    /**
+     * Create a new ComponentBuilder object
+     * @return a new ComponentBuilder
+     */
+    ComponentBuilder createBuilder();
+}
