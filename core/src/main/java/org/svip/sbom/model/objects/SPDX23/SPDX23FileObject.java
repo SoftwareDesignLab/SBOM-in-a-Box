f073b132385ac592d88ea17ee5ee589e2269ecad
Matthew Morrison <msm8275@rit.edu>
2023-07-02 14:33:02 -0400

    Implementation of SPDX23FileObject

--- core/src/main/java/org/svip/sbom/model/objects/SPDX23/SPDX23FileObject.java
+++ core/src/main/java/org/svip/sbom/model/objects/SPDX23/SPDX23FileObject.java
@@ -3,9 +3,35 @@ package org.svip.sbom.model.objects.SPDX23;
 import org.svip.sbom.model.interfaces.schemas.SPDX23.SPDX23File;
 import org.svip.sbom.model.shared.util.LicenseCollection;
 
+import java.util.HashMap;
 import java.util.Map;
+
+/**
+ * file: SPDX23FileObject.java
+ * Holds information for a single SPDX 2.3 File object
+ *
+ * @author Derek Garcia
+ * @author Matthew Morrison
+ */
 // todo
 public class SPDX23FileObject implements SPDX23File {
+
+    private String type;
+
+    private String uid;
+
+    private String author;
+
+    private String name;
+
+    private LicenseCollection licenses;
+
+    private String copyright;
+
+    private HashMap<String, String> hashes;
+
+    private String fileNotice;
+
     @Override
     public String getComment() {
         return null;
@@ -23,36 +49,52 @@ public class SPDX23FileObject implements SPDX23File {
 
     @Override
     public String getType() {
-        return null;
+        return this.type;
     }
 
     @Override
     public String getUID() {
-        return null;
+        return this.uid;
     }
 
     @Override
     public String getAuthor() {
-        return null;
+        return this.author;
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public LicenseCollection getLicenses() {
-        return null;
+        return this.licenses;
     }
 
     @Override
     public String getCopyright() {
-        return null;
+        return this.copyright;
     }
 
     @Override
     public Map<String, String> getHashes() {
-        return null;
+        return this.hashes;
+    }
+
+    public SPDX23FileObject(String type, String uid, String author, String name,
+                      LicenseCollection licenses, String copyright,
+                      HashMap<String, String> hashes, String fileNotice){
+        this.type = type;
+        this.uid = uid;
+        this.author = author;
+        this.name = name;
+        this.licenses = licenses;
+        this.copyright = copyright;
+        this.hashes = hashes;
+        this.fileNotice = fileNotice;
+
+
+
     }
 }
