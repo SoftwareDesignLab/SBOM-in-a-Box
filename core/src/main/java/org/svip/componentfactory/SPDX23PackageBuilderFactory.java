4233bde70becb197ba81fbd20c2fff6ab6b8497f
Matthew Morrison <msm8275@rit.edu>
2023-07-02 12:04:48 -0400

    Addition of SPDX23PackageBuilderFactory

---
+++ core/src/main/java/org/svip/componentfactory/SPDX23PackageBuilderFactory.java
@@ -0,0 +1,21 @@
+package org.svip.componentfactory;
+
+import org.svip.builders.component.ComponentBuilder;
+
+/**
+ * file: SPDX23PackageBuilderFactory.java
+ * Class to build SPDX 2.3 package specifications
+ *
+ * @author Matthew Morrison
+ */
+public class SPDX23PackageBuilderFactory implements ComponentBuilderFactory{
+
+    /**
+     * Create a new Builder
+     * @return a new SPDX23PackageBuilder
+     */
+    @Override
+    public ComponentBuilder createBuilder() {
+        return null;
+    }
+}
