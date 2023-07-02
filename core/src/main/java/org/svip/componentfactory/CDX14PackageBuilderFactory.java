6e9473ec2592854617d35fb0309786580730c618
Matthew Morrison <msm8275@rit.edu>
2023-07-02 12:04:48 -0400

    Addition of SPDX23PackageBuilderFactory

--- core/src/main/java/org/svip/componentfactory/CDX14PackageBuilderFactory.java
+++ core/src/main/java/org/svip/componentfactory/CDX14PackageBuilderFactory.java
@@ -6,7 +6,7 @@ import org.svip.builders.component.ComponentBuilder;
 
 /**
  * file: CDX14PackageBuilderFactory.java
- * Class to build CycloneDX specific packages
+ * Class to build CycloneDX 1.4 specific packages
  *
  * @author Matthew Morrison
  */
@@ -15,7 +15,7 @@ public class CDX14PackageBuilderFactory implements ComponentBuilderFactory {
     //TODO implement with CDX14PackageBuilder.java
     /**
      * Create a new Builder
-     * @return a ComponentBuilder
+     * @return a new CDX14PackageBuilder
      */
     @Override
     public ComponentBuilder createBuilder() {
