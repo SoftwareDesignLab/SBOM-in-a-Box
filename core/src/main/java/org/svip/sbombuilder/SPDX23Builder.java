4f9641e1315600c17271e5908858659f521db56a
Tom-Roman <tommy101800@gmail.com>
2023-07-03 12:38:45 -0400

    fixed errors

--- core/src/main/java/org/svip/sbombuilder/SPDX23Builder.java
+++ core/src/main/java/org/svip/sbombuilder/SPDX23Builder.java
@@ -9,6 +9,7 @@ import org.svip.sbom.model.objects.SPDX23.SPDX23SBOM;
 import org.svip.sbom.model.shared.Relationship;
 import org.svip.sbom.model.shared.metadata.CreationData;
 import org.svip.sbom.model.shared.util.ExternalReference;
+import org.svip.sbombuilder.interfaces.SPDX23SBOMBuilder;
 import org.svip.sbomvex.VEXFactory;
 
 import java.util.HashMap;
@@ -41,77 +42,77 @@ public abstract class SPDX23Builder implements SPDX23SBOMBuilder {
     private String SPDXLicenseListVersion;
     
     @Override
-    public SBOMBuilder setFormat(String format)
+    public SPDX23Builder setFormat(String format)
     {
         this.format = format;
         return this;
     }
 
     @Override
-    public SBOMBuilder setName(String name)
+    public SPDX23Builder setName(String name)
     {
         this.name = name;
         return this;
     }
 
     @Override
-    public SBOMBuilder setUID(String uid)
+    public SPDX23Builder setUID(String uid)
     {
         this.uid = uid;
         return this;
     }
 
     @Override
-    public SBOMBuilder setVersion(String version)
+    public SPDX23Builder setVersion(String version)
     {
         this.version = version;
         return this;
     }
 
     @Override
-    public SBOMBuilder setSpecVersion(String specVersion)
+    public SPDX23Builder setSpecVersion(String specVersion)
     {
         this.specVersion = specVersion;
         return this;
     }
 
     @Override
-    public SBOMBuilder addLicense(String license)
+    public SPDX23Builder addLicense(String license)
     {
         this.licenses.add(license);
         return this;
     }
 
     @Override
-    public SBOMBuilder setCreationData(CreationData creationData)
+    public SPDX23Builder setCreationData(CreationData creationData)
     {
         this.creationData = creationData;
         return this;
     }
 
     @Override
-    public SBOMBuilder setDocumentComment(String documentComment)
+    public SPDX23Builder setDocumentComment(String documentComment)
     {
         this.documentComment = documentComment;
         return this;
     }
 
     @Override
-    public SBOMBuilder setRootComponent(Component rootComponent)
+    public SPDX23Builder setRootComponent(Component rootComponent)
     {
         this.rootComponent = rootComponent;
         return this;
     }
 
     @Override
-    public SBOMBuilder addComponent(Component component)
+    public SPDX23Builder addComponent(Component component)
     {
         this.components.add(component);
         return this;
     }
 
     @Override
-    public SBOMBuilder addRelationship(String componentName, Relationship relationship)
+    public SPDX23Builder addRelationship(String componentName, Relationship relationship)
     {
         /** create a temporary list to maintain all the previously established relationships*/
         Set<Relationship> relationshipList = new HashSet<Relationship>();
@@ -124,28 +125,28 @@ public abstract class SPDX23Builder implements SPDX23SBOMBuilder {
     }
 
     @Override
-    public SBOMBuilder addExternalReference(ExternalReference externalReference)
+    public SPDX23Builder addExternalReference(ExternalReference externalReference)
     {
         this.externalReferences.add(externalReference);
         return this;
     }
 
     @Override
-    public SPDX23SBOMBuilder setSPDXLicenseListVersion(String licenseListVersion) {
+    public SPDX23Builder setSPDXLicenseListVersion(String licenseListVersion) {
         this.version = version;
         return this;
     }
 
     /** adds an SPDX 2.3 package to the components list */
     @Override
-    public SPDX23SBOMBuilder addSPDX23Package(SPDX23Package spdx23Package) {
+    public SPDX23Builder addSPDX23Package(SPDX23Package spdx23Package) {
         this.components.add(spdx23Package);
         return this;
     }
 
     /** adds an SPDX 2.3 file to the components list */
     @Override
-    public SPDX23SBOMBuilder addSPDX23File(SPDX23File spdx23File) {
+    public SPDX23Builder addSPDX23File(SPDX23File spdx23File) {
         this.components.add(spdx23File);
         return this;
     }
