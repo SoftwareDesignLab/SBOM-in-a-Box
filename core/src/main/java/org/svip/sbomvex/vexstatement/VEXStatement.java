971dc788f36788d31fa57b130fb6309e0a86cd4a
Matthew Morrison <msm8275@rit.edu>
2023-07-05 10:59:37 -0400

    Implement static Builder class for VEXStatement

--- core/src/main/java/org/svip/sbomvex/vexstatement/VEXStatement.java
+++ core/src/main/java/org/svip/sbomvex/vexstatement/VEXStatement.java
@@ -12,25 +12,25 @@ import java.util.Set;
  */
 public class VEXStatement {
     /**The statement ID*/
-    private String statementID;
+    private final String statementID;
 
     /**The statement version*/
-    private String statementVersion;
+    private final String statementVersion;
 
     /**The statement's first issued date*/
-    private String statementFirstIssued;
+    private final String statementFirstIssued;
 
     /**The statement's last updated date*/
-    private String statementLastUpdated;
+    private final String statementLastUpdated;
 
     /**The statement's status*/
-    private Status status;
+    private final Status status;
 
     /**The statement's set of products*/
-    private Set<Product> products;
+    private final Set<Product> products;
 
     /**The statement's vulnerability statement*/
-    private Vulnerability vulnerability;
+    private final Vulnerability vulnerability;
 
     /**
      * Get the VEX Statement ID
@@ -88,26 +88,121 @@ public class VEXStatement {
         return this.vulnerability;
     }
 
+
+    public static class Builder{
+        // Required fields
+        /**The statement ID*/
+        private String statementID;
+
+        /**The statement version*/
+        private String statementVersion;
+
+        /**The statement's first issued date*/
+        private String statementFirstIssued;
+
+        /**The statement's last updated date*/
+        private String statementLastUpdated;
+
+        /**The statement's status*/
+        private Status status;
+
+        /**The statement's set of products*/
+        private Set<Product> products;
+
+        /**The statement's vulnerability statement*/
+        private Vulnerability vulnerability;
+
+        /**
+         * Set the statement ID
+         * @param statementID the statement ID
+         * @return a Builder
+         */
+        public Builder setStatementID(String statementID){
+            this.statementID = statementID;
+            return this;
+        }
+
+        /**
+         * Set the statement version
+         * @param statementVersion the statement version
+         * @return a Builder
+         */
+        public Builder setStatementVersion(String statementVersion){
+            this.statementVersion = statementVersion;
+            return this;
+        }
+
+        /**
+         * Set the statement first issued date
+         * @param statementFirstIssued the statement first issued date
+         * @return a Builder
+         */
+        public Builder setStatementFirstIssued(String statementFirstIssued){
+            this.statementFirstIssued = statementFirstIssued;
+            return this;
+        }
+
+        /**
+         * Set the statement last updated date
+         * @param statementLastUpdated the statement last updated date
+         * @return a Builder
+         */
+        public Builder setStatementLastUpdated(String statementLastUpdated){
+            this.statementLastUpdated = statementLastUpdated;
+             return this;
+        }
+
+        /**
+         * Set the statement status
+         * @param status the status
+         * @return a Builder
+         */
+        public Builder setStatus(Status status){
+            this.status = status;
+            return this;
+        }
+
+        /**
+         * Add a product to the statement
+         * @param product the product
+         * @return a Builder
+         */
+        public Builder addProduct(Product product){
+            this.products.add(product);
+            return this;
+        }
+
+        /**
+         * Set the statement's vulnerability
+         * @param vulnerability the vulnerability
+         * @return a Builder
+         */
+        public Builder setVulnerability(Vulnerability vulnerability){
+            this.vulnerability = vulnerability;
+            return this;
+        }
+
+        /**
+         * Build a new VEX statement using the builder
+         * @return a new VEX Statement
+         */
+        public VEXStatement build(){
+            return new VEXStatement(this);
+        }
+    }
+
     /**
-     * Constructor to buiild a new VEX Statement
-     * @param statementID the statement ID
-     * @param statementVersion the statement version
-     * @param statementFirstIssued when the statement was first issued
-     * @param statementLastUpdated when the statement was last updated
-     * @param status the statement status
-     * @param products the statement products
-     * @param vulnerability the statement vulnerability
+     * Constructor to build a new VEX Statement
+     * @param builder the Builder static class object for VEX Statement
      */
-    private VEXStatement(String statementID, String statementVersion,
-                         String statementFirstIssued, String statementLastUpdated,
-                         Status status, Set<Product> products, Vulnerability vulnerability){
-        this.statementID = statementID;
-        this.statementVersion = statementVersion;
-        this.statementFirstIssued = statementFirstIssued;
-        this.statementLastUpdated = statementLastUpdated;
-        this.status = status;
-        this.products = products;
-        this.vulnerability = vulnerability;
+    private VEXStatement(Builder builder){
+        this.statementID = builder.statementID;
+        this.statementVersion = builder.statementVersion;
+        this.statementFirstIssued = builder.statementFirstIssued;
+        this.statementLastUpdated = builder.statementLastUpdated;
+        this.status = builder.status;
+        this.products = builder.products;
+        this.vulnerability = builder.vulnerability;
     }
 
 }
