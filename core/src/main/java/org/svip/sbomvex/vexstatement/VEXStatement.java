a38350b551d9e0802060272d2c5035e61055950b
Matthew Morrison <msm8275@rit.edu>
2023-07-05 09:41:18 -0400

    Start of VEXStatement and Status. Added enums VulnStatus and Justification

---
+++ core/src/main/java/org/svip/sbomvex/vexstatement/VEXStatement.java
@@ -0,0 +1,23 @@
+package org.svip.sbomvex.vexstatement;
+
+/**
+ * file: VEXStatement.java
+ * Class that holds a single statement for a VEX Document
+ *
+ * @author Matthew Morrison
+ */
+public class VEXStatement {
+    /**The statement ID*/
+    private String statementID;
+
+    /**The statement version*/
+    private String version;
+
+    /**The statement's first issued date*/
+    private String statementFirstIssued;
+
+    /**The statement's last updated date*/
+    private String statementLastUpdated;
+
+    /**The statement's status*/
+}
---
+++ core/src/main/java/org/svip/sbomvex/vexstatement/status/Status.java
@@ -0,0 +1,10 @@
+package org.svip.sbomvex.vexstatement.status;
+
+/**
+ * file: Status.java
+ * Class that defines a status for a VEX document
+ *
+ * @author Matthew Morrison
+ */
+public class Status {
+}
