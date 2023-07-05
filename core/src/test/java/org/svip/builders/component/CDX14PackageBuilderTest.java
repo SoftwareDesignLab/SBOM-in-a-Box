172884a2c249845b31a1b0596313c9c2b0c95efc
Matthew Morrison <msm8275@rit.edu>
2023-07-05 18:04:46 -0400

    Start of CDX14PackageBuilderTest

---
+++ core/src/test/java/org/svip/builders/component/CDX14PackageBuilderTest.java
@@ -0,0 +1,141 @@
+package org.svip.builders.component;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * file: CDX14PackageBuilderTest.java
+ * File to test CDX14PackageBuilder
+ */
+class CDX14PackageBuilderTest {
+    CDX14PackageBuilder test_package;
+
+    String test_mimeType = "image/jpeg";
+
+    String test_publisher = "The Python Foundation";
+
+    String test_scope = "required";
+
+    String test_group = "org.python";
+
+    String test_extRef_url = "https://www.python.org";
+
+    String test_extTef_type = "website";
+
+    String test_property_name = "local";
+
+    String test_property_value = "team_responsible";
+
+    String test_type = "library";
+
+    String test_uid = "749d4a17-1074-4b78-a968-fafc67378f75";
+
+    String test_author = "Guido van Rossum";
+
+    String test_name = "Python";
+
+    String test_license1 = "PSFL";
+
+    String test_license2 = "GPL";
+
+    String test_copyright = "Test Copyright";
+
+    String test_hash_algo = "SHA1";
+
+    String test_hash_value = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
+
+    String test_supplier = "Python Foundation";
+
+    String test_version = "1.23.1";
+
+    String test_description = "This is a test description";
+
+    String test_random_cpe = "cpe:2.3:a:random_test_cpe:random:3.11.2:*:*:*:*:*:*:*";
+
+    String test_random_purl = "pkg:random/test@2.0.0";
+
+    @Test
+    void setMimeType() {
+    }
+
+    @Test
+    void setPublisher() {
+    }
+
+    @Test
+    void setScope() {
+    }
+
+    @Test
+    void setGroup() {
+    }
+
+    @Test
+    void addExternalReferences() {
+    }
+
+    @Test
+    void addProperty() {
+    }
+
+    @Test
+    void setType() {
+    }
+
+    @Test
+    void setUID() {
+    }
+
+    @Test
+    void setAuthor() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setLicenses() {
+    }
+
+    @Test
+    void setCopyright() {
+    }
+
+    @Test
+    void addHash() {
+    }
+
+    @Test
+    void setSupplier() {
+    }
+
+    @Test
+    void setVersion() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void addCPE() {
+    }
+
+    @Test
+    void addPURL() {
+    }
+
+    @Test
+    void addExternalReference() {
+    }
+
+    @Test
+    void build() {
+    }
+
+    @Test
+    void buildAndFlush() {
+    }
+}
