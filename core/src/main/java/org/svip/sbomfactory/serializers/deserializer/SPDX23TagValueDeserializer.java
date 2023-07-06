26870cbf552c9d56ce6ca2cd8759c67b74fb70fd
Ian Dunn <itd3516@rit.edu>
2023-07-06 12:46:47 -0400

    Deserializer docstrings

--- core/src/main/java/org/svip/sbomfactory/serializers/deserializer/SPDX23TagValueDeserializer.java
+++ core/src/main/java/org/svip/sbomfactory/serializers/deserializer/SPDX23TagValueDeserializer.java
@@ -3,12 +3,31 @@ package org.svip.sbomfactory.serializers.deserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.svip.sbom.model.interfaces.generics.SBOM;
 
+/**
+ * File: SPDX23TagValueDeserializer.java
+ * This class implements the Deserializer interface and the Jackson StdDeserializer to provide all functionality to
+ * read an SPDX 2.3 SBOM object from an SPDX 2.3 tag-value file string.
+ *
+ * @author Ian Dunn
+ */
 public class SPDX23TagValueDeserializer implements Deserializer {
+
+    /**
+     * Deserializes an SPDX 2.3 tag-value SBOM from a string.
+     *
+     * @param fileContents The file contents of the SPDX 2.3 tag-value SBOM to deserialize.
+     * @return The deserialized SPDX 2.3 SBOM object.
+     */
     @Override
     public SBOM readFromString(String fileContents) {
         return null;
     }
 
+    /**
+     * Gets the ObjectMapper of the serializer to expose configuration.
+     *
+     * @return A reference to the ObjectMapper of the serializer.
+     */
     @Override
     public ObjectMapper getObjectMapper() {
         return null;
