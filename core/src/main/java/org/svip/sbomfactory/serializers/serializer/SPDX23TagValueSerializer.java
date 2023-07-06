63c4f83e2b09cfe23afe074211106f77e40ec403
Ian Dunn <itd3516@rit.edu>
2023-07-06 12:41:18 -0400

    Serializer docstrings

--- core/src/main/java/org/svip/sbomfactory/serializers/serializer/SPDX23TagValueSerializer.java
+++ core/src/main/java/org/svip/sbomfactory/serializers/serializer/SPDX23TagValueSerializer.java
@@ -3,12 +3,31 @@ package org.svip.sbomfactory.serializers.serializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.svip.sbom.model.objects.SVIPSBOM;
 
+/**
+ * File: SPDX23TagValueSerializer.java
+ * This class implements the Serializer interface to provide all functionality to write an SBOM object to an SPDX 2.3
+ * tag-value file string.
+ *
+ * @author Ian Dunn
+ */
 public class SPDX23TagValueSerializer implements Serializer {
+
+    /**
+     * Serializes an SBOM to an SPDX 2.3 tag-value file.
+     *
+     * @param sbom The SBOM to serialize.
+     * @return A string containing the final SBOM file.
+     */
     @Override
     public String writeToString(SVIPSBOM sbom) {
         return null;
     }
 
+    /**
+     * Gets the ObjectMapper of the serializer to expose configuration.
+     *
+     * @return A reference to the ObjectMapper of the serializer.
+     */
     @Override
     public ObjectMapper getObjectMapper() {
         return null;
