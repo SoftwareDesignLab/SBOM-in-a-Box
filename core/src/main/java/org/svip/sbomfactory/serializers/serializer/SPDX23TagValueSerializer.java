ef9dd156d5ba37a43d3ef3548a6e8f7730ca740f
Ian Dunn <itd3516@rit.edu>
2023-07-07 17:54:16 -0400

    Setup document info

--- core/src/main/java/org/svip/sbomfactory/serializers/serializer/SPDX23TagValueSerializer.java
+++ core/src/main/java/org/svip/sbomfactory/serializers/serializer/SPDX23TagValueSerializer.java
@@ -2,6 +2,12 @@ package org.svip.sbomfactory.serializers.serializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.svip.sbom.model.objects.SVIPSBOM;
+import org.svip.sbom.model.shared.metadata.Contact;
+import org.svip.sbomfactory.serializers.Metadata;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * File: SPDX23TagValueSerializer.java
@@ -20,8 +26,62 @@ public class SPDX23TagValueSerializer implements Serializer {
      */
     @Override
     public String writeToString(SVIPSBOM sbom) {
-        // TODO add tag-value generator code here
-        return "";
+        StringBuilder out = new StringBuilder();
+
+        out.append(getCreationInfo(sbom));
+
+        return out.toString();
+    }
+
+    private String buildTagValue(String tag, String value) {
+        return tag + ": " + value + "\n";
+    }
+
+    private String getCreatorString(String type, String primaryId, String secondaryId) {
+        if (type.equalsIgnoreCase("tool"))
+            return String.format("Tool: %s-%s", primaryId, secondaryId);
+
+        return String.format("%s: %s (%s)", type, primaryId, secondaryId);
+    }
+
+    private String getCreationInfo(SVIPSBOM sbom) {
+        StringBuilder out = new StringBuilder();
+
+        Set<String> creators = new HashSet<>(sbom.getCreationData().getAuthors().stream()
+                .map(a -> getCreatorString("Person", a.getName(), a.getEmail())).toList());
+        creators.addAll(sbom.getCreationData().getCreationTools().stream()
+                .map(t -> getCreatorString("Tool", t.getName(), t.getVersion())).toList());
+
+        Optional<Contact> supplierContact = sbom.getCreationData().getSupplier().getContacts().stream().findFirst();
+        String supplierEmail = "";
+        if (supplierContact.isPresent())
+            supplierEmail = supplierContact.get().getEmail();
+        creators.add(getCreatorString("Organization", sbom.getCreationData().getSupplier().getName(), supplierEmail));
+
+        out.append(buildTagValue("SPDXVersion", "SPDX-" + sbom.getSpecVersion()));
+        out.append(buildTagValue("DataLicense", "CC0-1.0"));
+        out.append(buildTagValue("SPDXID", "SPDXRef-DOCUMENT"));
+        out.append(buildTagValue("DocumentName", sbom.getName()));
+        out.append(buildTagValue("DocumentNamespace", sbom.getUID()));
+        out.append(buildTagValue("LicenseListVersion", sbom.getSPDXLicenseListVersion()));
+
+        for (String creator : creators) {
+            out.append(buildTagValue("Creator", creator));
+        }
+
+        out.append(buildTagValue("Created", sbom.getCreationData().getCreationTime()));
+
+        String creatorComment = sbom.getCreationData().getCreatorComment();
+        String documentComment = sbom.getDocumentComment();
+        if (creatorComment == null || creatorComment.isEmpty())
+            creatorComment = Metadata.SERIALIZED_COMMENT;
+        else if (documentComment == null || documentComment.isEmpty())
+            documentComment = Metadata.SERIALIZED_COMMENT;
+
+        out.append(buildTagValue("CreatorComment", creatorComment));
+        out.append(buildTagValue("DocumentComment", documentComment));
+
+        return out.toString();
     }
 
     /**
--- core/src/test/java/org/svip/sbomfactory/serializers/serializer/SPDX23TagValueSerializerTest.java
+++ core/src/test/java/org/svip/sbomfactory/serializers/serializer/SPDX23TagValueSerializerTest.java
@@ -1,7 +1,16 @@
 package org.svip.sbomfactory.serializers.serializer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.svip.sbomfactory.generators.utils.Debug;
+
 public class SPDX23TagValueSerializerTest extends SerializerTest {
     public SPDX23TagValueSerializerTest() {
         super(new SPDX23TagValueSerializer());
+    }
+
+    @Test
+    public void writeToStringTest() throws JsonProcessingException {
+        Debug.log(Debug.LOG_TYPE.SUMMARY, getSerializer().writeToString(getTestSBOM()));
     }
 }
