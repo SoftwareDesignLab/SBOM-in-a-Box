18cce308efac506b5f39d6a4ee23f55f00af2146
txdvse <txdvse@rit.edu>
2023-07-08 14:34:11 -0400

    MergerController.java + Moved to new merger package + Now the controller class for merging
    MergerCDX.java
    + Base file for CycloneDX Merging
    
    MergerSPDX.java
    + Base file for SPDX Merging
    
    MergerSVIP.java
    + Base file for SVIP Merging
    
    NewMergerTest.java
    + Updated tests

--- core/src/main/java/org/svip/sbomanalysis/comparison/merger/MergerCDX.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/merger/MergerCDX.java
@@ -1,42 +1,4 @@
 package org.svip.sbomanalysis.comparison.merger;
 
-import org.svip.sbom.model.interfaces.generics.Component;
-import org.svip.sbom.model.interfaces.generics.SBOM;
-import org.svip.sbom.model.objects.CycloneDX14.CDX14ComponentObject;
-import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
-import org.svip.sbom.model.objects.SVIPComponentObject;
-import org.svip.sbom.model.shared.Relationship;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
-public class MergerCDX extends Merger {
-
-    public MergerCDX(CDX14SBOM A, CDX14SBOM B) {
-        super(A, B);
-    }
-
-    @Override
-    protected SBOM mergeSBOM(SBOM A, SBOM B) {
-
-        Set<CDX14ComponentObject> componentsA = Collections.singleton((CDX14ComponentObject) A.getComponents());
-        Set<CDX14ComponentObject> componentsB = Collections.singleton((CDX14ComponentObject) A.getComponents());
-
-        CDX14SBOM mainSBOM = (CDX14SBOM) A;
-
-        return mainSBOM;
-
-    }
-
-    @Override
-    protected Set<Component> mergeComponents(Set<Component> A, Set<Component> B) {
-        return null;
-    }
-
-    @Override
-    protected Component mergeComponent(Component A, Component B) {
-        return null;
-    }
+public class MergerCDX {
 }
