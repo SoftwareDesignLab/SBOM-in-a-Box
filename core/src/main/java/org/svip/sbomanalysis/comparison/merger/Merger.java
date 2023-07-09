b7da648502f718131b3424419e0ab8d22470a1a7
txdvse <txdvse@rit.edu>
2023-07-09 09:51:47 -0400

    Merger.java + All methods for merger interface implemented

--- core/src/main/java/org/svip/sbomanalysis/comparison/merger/Merger.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/merger/Merger.java
@@ -3,6 +3,8 @@ package org.svip.sbomanalysis.comparison.merger;
 import org.svip.sbom.model.interfaces.generics.Component;
 import org.svip.sbom.model.interfaces.generics.SBOM;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 public interface Merger {
@@ -12,6 +14,8 @@ public interface Merger {
     Set<Component> mergeComponents();
 
     Component mergeComponent();
+
+    HashMap<String, HashSet<String>> mergeRelationships();
 
 }
 
