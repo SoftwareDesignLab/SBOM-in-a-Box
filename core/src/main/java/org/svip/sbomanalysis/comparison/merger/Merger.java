cdd8bdbd92a86b5e7d32d1266618e6a810e7b58c
txdvse <txdvse@rit.edu>
2023-07-09 09:50:00 -0400

    Merger.java + Base file for generic merger interface

--- core/src/main/java/org/svip/sbomanalysis/comparison/merger/Merger.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/merger/Merger.java
@@ -1,6 +1,18 @@
 package org.svip.sbomanalysis.comparison.merger;
 
+import org.svip.sbom.model.interfaces.generics.Component;
+import org.svip.sbom.model.interfaces.generics.SBOM;
+
+import java.util.Set;
+
 public interface Merger {
+
+    SBOM merge();
+
+    Set<Component> mergeComponents();
+
+    Component mergeComponent();
+
 }
 
 
