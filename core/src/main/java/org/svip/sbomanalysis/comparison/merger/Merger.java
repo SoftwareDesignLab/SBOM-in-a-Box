ce04281cc0743769457fd8de3fda7594add13875
txdvse <txdvse@rit.edu>
2023-07-10 05:34:38 -0400

    Merger.java + Implemented mergeComponents + Started mergeComponent

--- core/src/main/java/org/svip/sbomanalysis/comparison/merger/Merger.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/merger/Merger.java
@@ -1,22 +1,43 @@
 package org.svip.sbomanalysis.comparison.merger;
 
+import org.checkerframework.checker.units.qual.C;
 import org.svip.sbom.model.interfaces.generics.Component;
 import org.svip.sbom.model.interfaces.generics.SBOM;
+import org.svip.sbom.model.objects.CycloneDX14.CDX14ComponentObject;
+import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
+import org.svip.sbom.model.objects.SVIPComponentObject;
+import org.svip.sbom.model.shared.util.LicenseCollection;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-public interface Merger {
+public abstract class Merger {
 
-    SBOM merge();
+    public Merger(SBOM A, SBOM B) {
 
-    Set<Component> mergeComponents();
+    }
 
-    Component mergeComponent();
+    protected abstract SBOM mergeSBOM(SBOM A, SBOM B);
 
-    HashMap<String, HashSet<String>> mergeRelationships();
+    protected Set<Component> mergeComponents(Set<Component> A, Set<Component> B) {
+        Set<Component> merged_components = new HashSet<>();
+
+        for(Component current_A : A) {
+            for(Component current_B : B) {
+                if(current_A.getUID().equals(current_B.getUID()) && current_A.getName().equals(current_B.getName())) {
+                    /** Merge all relevant data into A component **/
+                    merged_components.add(mergeComponent(current_A, current_B));
+                    B.remove(current_B);
+                }
+            }
+        }
+        merged_components.addAll(B);
+        return merged_components;
+    }
+
+    protected Component mergeComponent(Component A, Component B) {
+        Component C = null;
+        return C;
+    }
 
 }
-
-
