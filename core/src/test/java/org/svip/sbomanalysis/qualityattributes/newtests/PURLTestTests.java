ab578489c6b97dd91bf48dc3db3a34d8c24f6d04
Matthew Morrison <msm8275@rit.edu>
2023-07-11 15:39:31 -0400

    Start of PURLTestTests

---
+++ core/src/test/java/org/svip/sbomanalysis/qualityattributes/newtests/PURLTestTests.java
@@ -0,0 +1,67 @@
+package org.svip.sbomanalysis.qualityattributes.newtests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.svip.sbom.model.interfaces.generics.Component;
+import org.svip.sbom.model.objects.CycloneDX14.CDX14ComponentObject;
+import org.svip.sbom.model.objects.SPDX23.SPDX23PackageObject;
+import org.svip.sbom.model.shared.Relationship;
+import org.svip.sbom.model.shared.metadata.CreationData;
+import org.svip.sbomanalysis.qualityattributes.newtests.enumerations.ATTRIBUTE;
+import org.svip.sbomanalysis.qualityattributes.resultfactory.Result;
+import org.svip.sbomanalysis.qualityattributes.resultfactory.enumerations.STATUS;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PURLTestTests {
+
+    String nullPURL = null;
+
+    String testRandomPURL = "pkg:random/test@2.0.0";
+
+    String testActualPURL = "pkg:golang/rsc.io/sampler@v1.3.0";
+
+    PURLTest purlTest;
+
+    String test_name = "Python";
+
+
+    @BeforeEach
+    public void create_purlTest(){
+        SPDX23PackageObject test_component = new SPDX23PackageObject(
+                null, null, null, test_name, null, null,
+                null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null,
+                null, null, null);
+        List<ATTRIBUTE> attributes = new ArrayList<>(List.of(
+                ATTRIBUTE.SPDX23, ATTRIBUTE.UNIQUENESS
+        ));
+
+        purlTest = new PURLTest(test_component, attributes);
+    }
+
+    @Test
+    public void isValidPURL_assert_Error_status_test(){
+       Set<Result> result =  purlTest.test("purl", nullPURL);
+
+       List<Result> resultList = new ArrayList<Result>(result);
+       Result r = resultList.get(0);
+
+       List<ATTRIBUTE> test_attributes = new ArrayList<>(List.of(
+               ATTRIBUTE.SPDX23, ATTRIBUTE.UNIQUENESS
+       ));
+       //TODO change details once implemented differently
+       Result test_result = new Result(test_attributes, "PURLTest",
+               "null is a missing purl", "TODO", STATUS.ERROR);
+
+       assertEquals(test_result, r);
+
+
+
+
+
+    }
+
+}
