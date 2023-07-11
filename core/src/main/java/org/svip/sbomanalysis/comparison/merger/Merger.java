85b4db21c08b0a360d8a7683d74d071165698976
txdvse <txdvse@rit.edu>
2023-07-11 09:36:14 -0400

    Rebuilt interface again.

--- core/src/main/java/org/svip/sbomanalysis/comparison/merger/Merger.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/merger/Merger.java
@@ -15,45 +15,19 @@ import java.util.Set;
 
 public abstract class Merger {
 
-    public Merger(SBOM A, SBOM B) {
+    SBOM A;
 
+    SBOM B;
+
+    public Merger(SBOM A, SBOM B) {
+        this.A = A;
+        this.B = B;
     }
 
     protected abstract SBOM mergeSBOM(SBOM A, SBOM B);
 
-    protected Set<Component> mergeComponents(Set<Component> A, Set<Component> B) {
+    protected abstract Set<Component> mergeComponents(Set<Component> A, Set<Component> B);
 
-        Set<Component> merged_components = new HashSet<>();
+    protected abstract Component mergeComponent(Component A, Component B);
 
-        for(Component current_A : A) {
-            for(Component current_B : B) {
-                if(current_A.getUID().equals(current_B.getUID()) && current_A.getName().equals(current_B.getName())) {
-                    /** Merge all relevant data into A component **/
-                    merged_components.add(mergeComponent(current_A, current_B));
-                    B.remove(current_B);
-                }
-            }
-        }
-        merged_components.addAll(B);
-        return merged_components;
-    }
-
-    protected Component mergeComponent(Component A, Component B) {
-
-        for(String concluded : B.getLicenses().getConcluded().stream().toList()) {
-            A.getLicenses().addConcludedLicenseString(concluded);
-        }
-        for(String declared : B.getLicenses().getDeclared().stream().toList()) {
-            A.getLicenses().addDeclaredLicense(declared);
-        }
-        for(String info : B.getLicenses().getInfoFromFiles().stream().toList()) {
-            A.getLicenses().addLicenseInfoFromFile(info);
-        }
-
-        for(Map.Entry hash : B.getHashes().entrySet()) {
-            //A.getHashes().putAll();
-        }
-
-        return A;
-    }
 }
