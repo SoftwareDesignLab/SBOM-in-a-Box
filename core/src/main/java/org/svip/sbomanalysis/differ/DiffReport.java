c1cee7caf7f1ae51f3c3c2e706a4f44b05c5df97
Tom-Roman <tfr8811@rit.edu>
2023-07-12 17:24:44 -0400

    implemented APIController, Diff Report, and Comparison

--- core/src/main/java/org/svip/sbomanalysis/differ/DiffReport.java
+++ core/src/main/java/org/svip/sbomanalysis/differ/DiffReport.java
@@ -2,22 +2,16 @@ package org.svip.sbomanalysis.differ;
 
 import org.svip.sbomanalysis.comparison.Comparison;
 
-import java.util.HashMap;
 import java.util.Map;
-/**
- * file: DiffReport.java
- * DiffReport for SBOM comparison
- *
- * @author Thomas Roman
- */
+
 public class DiffReport {
     private String targetUID;
-    private Map<String, Comparison> comparisons = new HashMap<>();
+    private Map<String, Comparison> comparisons;
 
     DiffReport(String targetUID) {
         this.targetUID = targetUID;
     }
     public void addComparison(Comparison comparison) {
-        this.comparisons.put(String.valueOf(comparisons.size()), comparison);
+        this.comparisons.put(this.targetUID, comparison);
     }
 }
