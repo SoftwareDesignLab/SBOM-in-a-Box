75bbc3c0a72c9c3a9a67b86fa744d46b22c03a5f
txdvse <txdvse@rit.edu>
2023-07-12 11:55:44 -0400

    + Added merging CreationData functionality to MergerCDX

--- core/src/main/java/org/svip/sbomanalysis/comparison/merger/MergerCDX.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/merger/MergerCDX.java
@@ -1,16 +1,20 @@
 package org.svip.sbomanalysis.comparison.merger;
 
+import org.svip.sbom.builder.objects.schemas.CDX14.CDX14Builder;
+
 import org.svip.sbom.model.interfaces.generics.Component;
 import org.svip.sbom.model.interfaces.generics.SBOM;
+
 import org.svip.sbom.model.objects.CycloneDX14.CDX14ComponentObject;
 import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
 import org.svip.sbom.model.objects.SVIPComponentObject;
 import org.svip.sbom.model.shared.Relationship;
+import org.svip.sbom.model.shared.metadata.Contact;
+import org.svip.sbom.model.shared.metadata.CreationData;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class MergerCDX extends Merger {
 
@@ -24,9 +28,49 @@ public class MergerCDX extends Merger {
         Set<CDX14ComponentObject> componentsA = Collections.singleton((CDX14ComponentObject) A.getComponents());
         Set<CDX14ComponentObject> componentsB = Collections.singleton((CDX14ComponentObject) A.getComponents());
 
+        // declare SBOM A as the main SBOM, cast it back to CDX14SBOM
         CDX14SBOM mainSBOM = (CDX14SBOM) A;
 
-        return mainSBOM;
+        // Create a new builder for the new SBOM
+        CDX14Builder builder = new CDX14Builder();
+
+        /* Assign all top level data for the new SBOM */
+
+        // Format
+        builder.setFormat(mainSBOM.getFormat());
+
+        // Name
+        builder.setName(mainSBOM.getName());
+
+        // UID (In this case, bom-ref)
+        builder.setUID(mainSBOM.getUID());
+
+        // SBOM Version
+        builder.setVersion(mainSBOM.getVersion());
+
+        // Spec Version (1.4)
+        builder.setSpecVersion(mainSBOM.getSpecVersion());
+
+        // Licenses
+        for(String license : mainSBOM.getLicenses()) { builder.addLicense(license); }
+
+        // Creation Data
+        if(!A.getCreationData().equals(null) && !B.getCreationData().equals(null)) {
+            builder.setCreationData(mergeCreationData(A.getCreationData(), B.getCreationData())); /** A new object should be created for this. Will do later.**/
+        } else if (!A.getCreationData().equals(null)) {
+            builder.setCreationData(A.getCreationData());
+        } else if (!B.getCreationData().equals(null)) {
+            builder.setCreationData(B.getCreationData());
+        } else { builder.setCreationData(null); }
+
+        // Document Comment
+        builder.setDocumentComment(mainSBOM.getDocumentComment());
+
+        // Root Component
+        builder.setRootComponent(mainSBOM.getRootComponent());
+
+
+        return builder.Build();
 
     }
 
@@ -39,4 +83,45 @@ public class MergerCDX extends Merger {
     protected Component mergeComponent(Component A, Component B) {
         return null;
     }
+
+    @Override
+    protected CreationData mergeCreationData(CreationData A, CreationData B) {
+
+        CreationData mergedCreationData = new CreationData();
+
+        // Create a new timestamp and add it to the new SBOM, rather than using one of previous timestamps
+        SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        mergedCreationData.setCreationTime(timeFormat.format(timestamp));
+
+        // Authors
+        Set<Contact> authorsA = A.getAuthors();
+        Set<Contact> authorsB = B.getAuthors();
+        Set<Contact> authorsNew = new HashSet<>();
+
+        // Cycle through each primary SBOM author
+        for(Contact authorA : authorsA) {
+            // Cycle through all authors from sbom B and compare them against current primary SBOM author
+            for(Contact authorB : authorsB) {
+                if(
+                        authorA.getName().contains(authorB.getName()) &&
+                        authorA.getEmail().contains(authorB.getEmail()) &&
+                        authorA.getPhone().contains(authorB.getPhone())
+                ) {
+                    // If a duplicate is found remove it from second SBOM authors
+                    authorsB.remove(authorB);
+                }
+            }
+            // After duplicates have been removed, add the author from the primary SBOM in
+            authorsNew.add(authorA);
+        }
+        // Add all remaining authors from second SBOM into new authors
+        for(Contact authorB : authorsB) {
+            authorsNew.add(authorB);
+        }
+
+
+        return mergedCreationData;
+    }
+
 }
