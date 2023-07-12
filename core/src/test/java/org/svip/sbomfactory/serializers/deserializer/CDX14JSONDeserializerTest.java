1d6ec07ed7c7af876634d930f57aec0ec157ebd9
Ian Dunn <techwizguy7@gmail.com>
2023-07-12 19:51:06 -0400

    Implement all cdx 1.4 json deserializer tests

--- core/src/test/java/org/svip/sbomfactory/serializers/deserializer/CDX14JSONDeserializerTest.java
+++ core/src/test/java/org/svip/sbomfactory/serializers/deserializer/CDX14JSONDeserializerTest.java
@@ -6,8 +6,11 @@ import org.svip.sbom.model.interfaces.generics.SBOM;
 import org.svip.sbom.model.interfaces.schemas.CycloneDX14.CDX14Package;
 import org.svip.sbom.model.objects.CycloneDX14.CDX14ComponentObject;
 import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
+import org.svip.sbom.model.shared.Relationship;
 import org.svip.sbom.model.shared.metadata.Contact;
 import org.svip.sbom.model.shared.metadata.CreationTool;
+import org.svip.sbom.model.shared.metadata.Organization;
+import org.svip.sbom.model.shared.util.ExternalReference;
 import org.svip.sbom.model.shared.util.LicenseCollection;
 
 import java.io.IOException;
@@ -19,6 +22,8 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CDX14JSONDeserializerTest extends DeserializerTest {
+    // TODO no way to test root component
+
     private final CDX14SBOM cdx14json;
     public CDX14JSONDeserializerTest() throws IOException {
         super(new CDX14JSONDeserializer());
@@ -49,38 +54,115 @@ public class CDX14JSONDeserializerTest extends DeserializerTest {
     public void metadataTest() {
         assertEquals("TEST TIMESTAMP", cdx14json.getCreationData().getCreationTime());
 
-        CreationTool tool = cdx14json.getCreationData().getCreationTools().stream().findFirst().get();
-        assertEquals("SVIP", tool.getName());
-        assertEquals("SVIP", tool.getVendor());
-        assertEquals("SVIP", tool.getVersion());
-        assertEquals("hash", tool.getHashes().get("SHA256"));
+        assertEquals("This SBOM was generated using the SVIP serializer tooling.",
+                cdx14json.getCreationData().getCreatorComment());
+        assertTrue(cdx14json.getCreationData().getProperties().get("testProperty").contains("testValue"));
 
+        assertTrue(cdx14json.getLicenses().contains("MIT"));
+    }
+
+    @Test
+    public void metadataToolTest() {
+        CreationTool tool = cdx14json.getCreationData().getCreationTools().stream().findFirst().get();
+        assertEquals("SVIP Serializer", tool.getName());
+        assertEquals("SVIP", tool.getVendor());
+        assertEquals("1.0.0", tool.getVersion());
+        assertEquals("hash", tool.getHashes().get("SHA256"));
+    }
+
+    @Test
+    public void metadataAuthorTest() {
         Contact author = cdx14json.getCreationData().getAuthors().stream().findFirst().get();
         assertEquals("Test Author", author.getName());
         assertEquals("author@publisher.xyz", author.getEmail());
         assertEquals("123-456-7890", author.getPhone());
-
-        // TODO
     }
 
     @Test
-    private void testComponent(Component component) {
-        assertEquals("1.4", cdx14json.getSpecVersion());
+    public void metadataSupplierTest() {
+        Organization supplier = cdx14json.getCreationData().getManufacture();
+        assertEquals("Supplier", supplier.getName());
+        assertEquals("svip.xyz", supplier.getUrl());
+        assertEquals("Supplier", supplier.getContacts().stream().findFirst().get().getName());
+        assertEquals("supplier@svip.xyz", supplier.getContacts().stream().findFirst().get().getEmail());
+        assertEquals("123-456-7890", supplier.getContacts().stream().findFirst().get().getPhone());
+    }
 
-        // TODO
+    @Test
+    public void metadataManufactureTest() {
+        Organization manufacture = cdx14json.getCreationData().getManufacture();
+        assertEquals("Manufacturer", manufacture.getName());
+        assertEquals("svip.xyz", manufacture.getUrl());
+        assertEquals("SVIP", manufacture.getContacts().stream().findFirst().get().getName());
+        assertEquals("manufacturer@svip.xyz", manufacture.getContacts().stream().findFirst().get().getEmail());
+        assertEquals("123-456-7890", manufacture.getContacts().stream().findFirst().get().getPhone());
+
+    }
+
+    private void testComponent(CDX14ComponentObject component, int num) {
+        assertEquals("COMPONENT " + num, component.getName());
+        assertEquals("uid" + num, component.getUID());
+        assertEquals("mimeType" + num, component.getMimeType());
+        assertEquals("type" + num, component.getType());
+        assertEquals("group" + num, component.getGroup());
+        assertEquals("version" + num, component.getVersion());
+        assertEquals("scope" + num, component.getScope());
+        assertEquals("copyright" + num, component.getCopyright());
+        assertTrue(component.getCPEs().contains("cpe" + num));
+        assertTrue(component.getPURLs().contains("purl" + num));
+
+        // Supplier
+        Organization supplier = component.getSupplier();
+        assertEquals("SVIP" + num, supplier.getName());
+        assertEquals(num + ".svip.xyz", supplier.getUrl());
+        assertEquals("SVIP", supplier.getContacts().stream().findFirst().get().getName());
+        assertEquals("svip@svip.xyz", supplier.getContacts().stream().findFirst().get().getEmail());
+        assertEquals("123-456-7890", supplier.getContacts().stream().findFirst().get().getPhone());
+
+        assertEquals("author" + num, component.getAuthor());
+        assertEquals("publisher" + num, component.getPublisher());
+        assertEquals("Summary: summary" + num + " | Details: extendedDescription" + num,
+                component.getDescription().getSummary());
+        assertEquals("hash" + num, component.getHashes().get("SHA256"));
+
+        // TODO how do we find the difference?
+        assertTrue(component.getLicenses().getConcluded().containsAll(List.of("licenseFileText" + num,
+                "declared" + num,
+                "concluded" + num)));
+
+        ExternalReference ref = component.getExternalReferences().stream().findFirst().get();
+        assertEquals(num + ".svip.xyz", ref.getUrl());
+        // Comment is irrelevant here
+        assertEquals("testRef", ref.getType());
+        assertEquals("hash" + num, ref.getHashes().get("SHA256"));
+        assertTrue(component.getProperties().get("property" + num).contains("value" + num));
+    }
+
+    @Test
+    public void rootComponentTest() {
+        testComponent(cdx14json.getRootComponent(), 0);
+    }
+
+    @Test
+    public void componentTest() {
+        for (Component component : cdx14json.getComponents()) {
+            testComponent((CDX14ComponentObject) component,
+                    component.getName().charAt(component.getName().length() - 1));
+        }
     }
 
     @Test
     public void externalReferencesTest() {
-        assertEquals("12345678", cdx14json.getUID());
-
-        // TODO
+        ExternalReference ref = cdx14json.getExternalReferences().stream().findFirst().get();
+        assertEquals("svip.xyz", ref.getUrl());
+        // Comment is irrelevant here
+        assertEquals("testRef", ref.getType());
+        assertEquals("hash", ref.getHashes().get("SHA256"));
     }
 
     @Test
     public void dependenciesTest() {
-        assertEquals("1.0.0", cdx14json.getVersion());
-
-        // TODO
+        Relationship rel = cdx14json.getRelationships().get("uid1").stream().findFirst().get();
+        assertEquals("uid3", rel.getOtherUID());
     }
 }
