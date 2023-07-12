2dff92cdb6daaf17f8a60e0876687b7f0b0f0035
Tom-Roman <tfr8811@rit.edu>
2023-07-12 17:58:10 -0400

    conflict implementation

--- core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/MissingConflict.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/MissingConflict.java
@@ -3,5 +3,7 @@ package org.svip.sbomanalysis.comparison.conflicts;
 public class MissingConflict extends Conflict {
     MissingConflict(String field, String targetValue, String otherValue) {
         super(field, targetValue, otherValue);
+        this.type = type;
+        this.message = field + " is missing";
     }
 }
