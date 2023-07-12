1d501aa4ef5d035966929496ce2f811e6f5758ce
Ian Dunn <techwizguy7@gmail.com>
2023-07-12 18:49:23 -0400

    Setup cdx 1.4 json deserializer tests

--- core/src/test/java/org/svip/sbomfactory/serializers/deserializer/CDX14JSONDeserializerTest.java
+++ core/src/test/java/org/svip/sbomfactory/serializers/deserializer/CDX14JSONDeserializerTest.java
@@ -6,6 +6,7 @@ import org.svip.sbom.model.interfaces.generics.SBOM;
 import org.svip.sbom.model.interfaces.schemas.CycloneDX14.CDX14Package;
 import org.svip.sbom.model.objects.CycloneDX14.CDX14ComponentObject;
 import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
+import org.svip.sbom.model.shared.metadata.Contact;
 import org.svip.sbom.model.shared.metadata.CreationTool;
 import org.svip.sbom.model.shared.util.LicenseCollection;
 
@@ -18,60 +19,68 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CDX14JSONDeserializerTest extends DeserializerTest {
-    public CDX14JSONDeserializerTest() {
+    private final CDX14SBOM cdx14json;
+    public CDX14JSONDeserializerTest() throws IOException {
         super(new CDX14JSONDeserializer());
+        cdx14json = (CDX14SBOM) getDeserializer().readFromString(Files.readString(Path.of(CDX_14_JSON_SBOM)));
     }
 
     @Test
-    public void readFromStringTest() throws IOException {
-        CDX14JSONDeserializer cdx14Deserializer = new CDX14JSONDeserializer();
-        CDX14SBOM sbom = cdx14Deserializer.readFromString(Files.readString(Path.of(CDX_14_JSON_SBOM)));
+    public void formatTest() {
+        assertEquals("CycloneDX", cdx14json.getFormat());
     }
 
     @Test
-    public void metadataTest() throws IOException {
-        CDX14JSONDeserializer cdx14Deserializer = new CDX14JSONDeserializer();
-        CDX14SBOM sbom = cdx14Deserializer.readFromString(Files.readString(Path.of(CDX_14_JSON_SBOM)));
-
-        // TODO more assertions
-        assertNotNull(sbom);
-        assertEquals("1.4", sbom.getSpecVersion());
-        assertEquals("urn:uuid:139f2560-6fb3-4b85-b5fc-fe48be29b72b", sbom.getUID());
-        assertEquals("1", sbom.getVersion());
-        assertEquals("2023-04-05T07:56:11-04:00", sbom.getCreationData().getCreationTime());
-        assertEquals(1, sbom.getCreationData().getCreationTools().size());
-        List<CreationTool> creationTools = sbom.getCreationData().getCreationTools().stream().toList();
-        assertEquals("anchore", creationTools.get(0).getVendor());
-        assertEquals("syft", creationTools.get(0).getName());
-        assertEquals("0.69.1", creationTools.get(0).getVersion());
-        assertEquals("c4a58a4c9826996e", sbom.getRootComponent().getUID());
-        assertEquals("container", sbom.getRootComponent().getType());
-        assertEquals("alpine:latest", sbom.getRootComponent().getName());
-        assertEquals("sha256:b6ca290b6b4cdcca5b3db3ffa338ee0285c11744b4a6abaa9627746ee3291d8d", sbom.getRootComponent().getVersion());
+    public void specVersionTest() {
+        assertEquals("1.4", cdx14json.getSpecVersion());
     }
 
     @Test
-    public void componentsTest() throws IOException {
-        CDX14JSONDeserializer cdx14Deserializer = new CDX14JSONDeserializer();
-        CDX14SBOM sbom = cdx14Deserializer.readFromString(Files.readString(Path.of(CDX_14_JSON_SBOM)));
-        assertEquals(17, sbom.getComponents().size());
-        List<Component> components = sbom.getComponents().stream().toList();
-        // checking for duplicates:
-        int count = 1;
-        for (int i = 1; i < components.size(); i++) {
-            if (components.get(i).getUID() == "pkg:apk/alpine/musl@1.2.3-r4?arch=x86_64&upstream=musl&distro=alpine-3.17.3&package-id=d9700f02cf26e8b8") {
-                // TODO implement author
-                assertEquals("library", components.get(i).getType());
-                assertEquals("musl", components.get(i).getName());
-                // TODO CDX specific data is lost as the CDX SBOM stores incomplete component interfaces
-                // assertEquals("1.2.3-r4", components.get(i).getVersion());
-                // assertEquals("the musl c library (libc) implementation", components.get(i).getDescription());
-                // TODO licenses not working
-                // LicenseCollection licenses = components.get(i).getLicenses();
-                // assertEquals("MIT", licenses.getInfoFromFiles().stream().toList().get(i));
-                // TODO cpe, purl, external ref, and properties are not available in generic component
-                count += 1;
-            }
-        }
+    public void uidTest() {
+        assertEquals("12345678", cdx14json.getUID());
+    }
+
+    @Test
+    public void versionTest() {
+        assertEquals("1.0.0", cdx14json.getVersion());
+    }
+
+    @Test
+    public void metadataTest() {
+        assertEquals("TEST TIMESTAMP", cdx14json.getCreationData().getCreationTime());
+
+        CreationTool tool = cdx14json.getCreationData().getCreationTools().stream().findFirst().get();
+        assertEquals("SVIP", tool.getName());
+        assertEquals("SVIP", tool.getVendor());
+        assertEquals("SVIP", tool.getVersion());
+        assertEquals("hash", tool.getHashes().get("SHA256"));
+
+        Contact author = cdx14json.getCreationData().getAuthors().stream().findFirst().get();
+        assertEquals("Test Author", author.getName());
+        assertEquals("author@publisher.xyz", author.getEmail());
+        assertEquals("123-456-7890", author.getPhone());
+
+        // TODO
+    }
+
+    @Test
+    private void testComponent(Component component) {
+        assertEquals("1.4", cdx14json.getSpecVersion());
+
+        // TODO
+    }
+
+    @Test
+    public void externalReferencesTest() {
+        assertEquals("12345678", cdx14json.getUID());
+
+        // TODO
+    }
+
+    @Test
+    public void dependenciesTest() {
+        assertEquals("1.0.0", cdx14json.getVersion());
+
+        // TODO
     }
 }
