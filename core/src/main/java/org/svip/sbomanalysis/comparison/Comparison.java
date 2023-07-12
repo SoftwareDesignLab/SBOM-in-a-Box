27415edfc58f6a8a8ea577b8e7b023b8f8028cdb
Tom-Roman <tfr8811@rit.edu>
2023-07-12 17:24:44 -0400

    implemented APIController, Diff Report, and Comparison

--- core/src/main/java/org/svip/sbomanalysis/comparison/Comparison.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/Comparison.java
@@ -12,15 +12,33 @@ public class Comparison {
     private List<String> missingComponents;
 
     public Comparison(SBOM targetSBOM, SBOM otherSBOM) {
-
+        // METADATA
+        CompareMetadata(targetSBOM, otherSBOM);
+        // COMPONENTS
+        List<Component> targetComponents = targetSBOM.getComponents().stream().toList();
+        List<Component> otherComponents = otherSBOM.getComponents().stream().toList();
+        int count;
+        for (int i = 0; i < targetComponents.size(); i++) {
+            count = 0;
+            for (int j = 0; j < otherComponents.size(); j++) {
+                if (targetComponents.get(i).getUID() == otherComponents.get(j).getUID()) {
+                    CompareComponent(targetComponents.get(i), otherComponents.get(j));
+                    count += 1;
+                }
+            }
+            // component not found in otherComponents
+            if (count == 0) {
+                missingComponents.add(targetComponents.get(i).getUID());
+            }
+        }
     }
 
     private void CompareMetadata(SBOM targetSBOM, SBOM otherSBOM) {
-
+        componentConflicts.put(targetSBOM.getUID(), targetSBOM.compare(otherSBOM));
     }
 
     private void CompareComponent(Component targetComponent, Component otherComponent) {
-
+        componentConflicts.put(targetComponent.getUID(), targetComponent.compare(otherComponent));
     }
 
     // TODO compareOtherReleventField()
