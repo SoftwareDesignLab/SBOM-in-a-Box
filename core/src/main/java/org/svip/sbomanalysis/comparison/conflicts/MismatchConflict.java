eb45df998a732292fcedffd7cc92e1663a3ccbb9
Tom-Roman <tfr8811@rit.edu>
2023-07-12 17:58:10 -0400

    conflict implementation

--- core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/MismatchConflict.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/MismatchConflict.java
@@ -1,7 +1,9 @@
 package org.svip.sbomanalysis.comparison.conflicts;
 
 public class MismatchConflict extends Conflict {
-    MismatchConflict(String field, String targetValue, String otherValue) {
+    MismatchConflict(String field, String targetValue, String otherValue, ConflictType type) {
         super(field, targetValue, otherValue);
+        this.type = ConflictType.MISSING;
+        this.message = field + " doesn't match";
     }
 }
