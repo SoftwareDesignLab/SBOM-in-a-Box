f87cb8a2242f43185370dfde364334f1e56f6049
Tom-Roman <tfr8811@rit.edu>
2023-07-12 12:15:08 -0400

    swapped test sbom for the spdx tag value deserializer

--- core/src/test/java/org/svip/sbomfactory/serializers/deserializer/SPDX23TagValueDeserializerTest.java
+++ core/src/test/java/org/svip/sbomfactory/serializers/deserializer/SPDX23TagValueDeserializerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.svip.sbom.model.interfaces.generics.Component;
 import org.svip.sbom.model.objects.SPDX23.SPDX23SBOM;
 import org.svip.sbom.model.old.SBOM;
+import org.svip.sbom.model.shared.metadata.Contact;
 import org.svip.sbom.model.shared.metadata.CreationTool;
 import org.svip.sbomfactory.translators.TranslatorException;
 
@@ -37,38 +38,51 @@ public class SPDX23TagValueDeserializerTest extends DeserializerTest {
         // TODO more assertions
         assertNotNull(sbom);
         // SPDXVersion
-        assertEquals("2.3", sbom.getSpecVersion());
+        assertEquals("1.4", sbom.getSpecVersion());
         // DataLicense
         assertEquals("CC0-1.0", sbom.getLicenses().stream().toList().get(0));
         // DocumentName
-        assertEquals("alpine:latest", sbom.getName());
+        assertEquals("Test SBOM", sbom.getName());
         // DocumentNamespace
-        assertEquals("https://anchore.com/syft/image/alpine-latest-b9fc484b-41c4-4589-b3ef-c57bba20078c", sbom.getUID());
+        assertEquals("12345678", sbom.getUID());
         // LicenseListVersion
-        assertEquals("3.19", sbom.getSPDXLicenseListVersion());
+        assertEquals("0.0", sbom.getSPDXLicenseListVersion());
         // created
-        assertEquals("2023-03-10T14:43:10Z", sbom.getCreationData().getCreationTime());
+        assertEquals("TEST TIMESTAMP", sbom.getCreationData().getCreationTime());
 
         // creators
         assertEquals(1, sbom.getCreationData().getCreationTools().size());
         List<CreationTool> creationTools = sbom.getCreationData().getCreationTools().stream().toList();
-        assertEquals("syft", creationTools.get(0).getName());
-        assertEquals("0.69.1", creationTools.get(0).getVersion());
+        assertEquals("SVIP Serializer", creationTools.get(0).getName());
+        assertEquals("1.0.0", creationTools.get(0).getVersion());
+        List<Contact> authorsList = sbom.getCreationData().getAuthors().stream().toList();
+        assertEquals(2, authorsList.size());
+        for( int i = 0; i < authorsList.size(); i++) {
+            if (authorsList.get(i).getName() == "Test Author") {
+                assertEquals("author@publisher.xyz", authorsList.get(i).getEmail());
+            }
+            else if (authorsList.get(i).getName() == "Supplier"){
+                assertEquals("supplier@svip.xyz", authorsList.get(i).getEmail());
+            }
+        }
+        //assertEquals("This SBOM was created for serializer testing",
+        //        sbom.getCreationData().getCreatorComment());
+        //assertEquals("Test Document Comment", sbom.getDocumentComment());
     }
     @Test
     public void componentTest() throws IOException {
         SPDX23TagValueDeserializer spdx23Deserializer = new SPDX23TagValueDeserializer();
         SPDX23SBOM sbom = spdx23Deserializer.readFromString(Files.readString(Path.of(SPDX23_TAGVALUE_SBOM)));
         assertNotNull(sbom);
-
-        assertEquals(94, sbom.getComponents().size());
+        assertEquals(3, sbom.getComponents().size());
         List<Component> components = sbom.getComponents().stream().toList();
-        //assertEquals("SPDXRef-1071b0c6b4d98bb1", components.get(0).getUID());
-        // TODO get a better sbom
         // check for duplicates
         int count = 0;
         for (int i = 0; i < components.size(); i++) {
-            if (Objects.equals(components.get(i).getUID(), "SPDXRef-35ab393f27e0bc39")) {
+            Component componentObject = components.get(i);
+            if (Objects.equals(componentObject.getUID(), "uid1")) {
+                assertEquals("COMPONENT 1", componentObject.getName());
+                // TODO access the spdx23 specific data
                 count += 1;
             }
         }
