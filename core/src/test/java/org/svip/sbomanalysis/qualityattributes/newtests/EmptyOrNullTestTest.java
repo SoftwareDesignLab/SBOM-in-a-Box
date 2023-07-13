09a1afb91abbd01e9e2470f8330069b97e4267ed
Matthew Morrison <msm8275@rit.edu>
2023-07-13 10:32:33 -0400

    Fixed bug when value was null, implemented tests for EmptyOrNullTest

---
+++ core/src/test/java/org/svip/sbomanalysis/qualityattributes/newtests/EmptyOrNullTestTest.java
@@ -0,0 +1,63 @@
+package org.svip.sbomanalysis.qualityattributes.newtests;
+
+import org.junit.jupiter.api.*;
+import org.svip.sbomanalysis.qualityattributes.newtests.enumerations.ATTRIBUTE;
+import org.svip.sbomanalysis.qualityattributes.resultfactory.Result;
+import org.svip.sbomanalysis.qualityattributes.resultfactory.enumerations.STATUS;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmptyOrNullTestTest {
+
+    Collection<String> testEmptyCol = new ArrayList<>();
+    Collection<String> testActualCol = new ArrayList<>(List.of(
+            "String1", "String2"
+    ));
+
+    EmptyOrNullTest emptyOrNullTest;
+
+    @BeforeEach
+    public void create_EmptyOrNullTest(){
+        emptyOrNullTest = new EmptyOrNullTest(ATTRIBUTE.COMPLETENESS);
+    }
+
+    @Test
+    public void test_null_string_fail_test(){
+        Result result = emptyOrNullTest.test("String", null);
+        assertEquals(STATUS.FAIL, result.getStatus());
+    }
+
+    @Test
+    public void test_empty_string_fail_test(){
+        Result result = emptyOrNullTest.test("String", "");
+        assertEquals(STATUS.FAIL, result.getStatus());
+    }
+
+    @Test
+    public void test_string_pass_test(){
+        Result result = emptyOrNullTest.test("String", "AnActualString");
+        assertEquals(STATUS.PASS, result.getStatus());
+    }
+
+    @Test
+    public void test_null_collection_fail_test(){
+        Result result = emptyOrNullTest.test("collection", null);
+        assertEquals(STATUS.FAIL, result.getStatus());
+    }
+
+    @Test
+    public void test_empty_collection_fail_test(){
+        Result result = emptyOrNullTest.test("collection", testEmptyCol);
+        assertEquals(STATUS.FAIL, result.getStatus());
+    }
+
+    @Test
+    public void test_collection_pass_test(){
+        Result result = emptyOrNullTest.test("collection", testActualCol);
+        assertEquals(STATUS.PASS, result.getStatus());
+    }
+}
