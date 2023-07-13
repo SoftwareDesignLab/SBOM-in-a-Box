ce50c62be5f8a0f3e69e192a5006af0f2af495e9
Tom-Roman <tfr8811@rit.edu>
2023-07-13 15:39:05 -0400

    added comments

--- core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/MissingConflict.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/MissingConflict.java
@@ -1,5 +1,10 @@
 package org.svip.sbomanalysis.comparison.conflicts;
-
+/**
+ * file: MissingConflict.java
+ * Class for missing conflict
+ *
+ * @author Thomas Roman
+ */
 public class MissingConflict extends Conflict {
     public MissingConflict(String field, String targetValue, String otherValue) {
         super(field, targetValue, otherValue);
--- core/src/main/java/org/svip/sbomanalysis/differ/APIController.java
+++ core/src/main/java/org/svip/sbomanalysis/differ/APIController.java
@@ -2,7 +2,12 @@ package org.svip.sbomanalysis.differ;
 
 import org.svip.sbom.model.interfaces.generics.SBOM;
 import org.svip.sbomanalysis.comparison.Comparison;
-
+/**
+ * file: APIController.java
+ * APIController for the diff report
+ *
+ * @author Thomas Roman
+ */
 public class APIController {
     public DiffReport compare(int targetIndex, SBOM[] sboms) {
         DiffReport diffReport = new DiffReport(sboms[targetIndex].getUID());
