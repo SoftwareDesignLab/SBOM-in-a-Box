512e069b9ae8d1d89be76d3fd3d84735f6b9807d
Ian Dunn <itd3516@rit.edu>
2023-07-13 15:44:08 -0400

    Restructure parsers test directory

---
+++ core/src/test/java/org/svip/sbomfactory/parsers/TestData/Java/lib/Foo.java
@@ -0,0 +1,23 @@
+package org.svip.sbomfactory.parsers.TestData.Java.lib;
+
+/**
+ * Long Comment
+ */
+
+// Live Import
+
+import java.util.ArrayList;
+
+public class Foo {
+
+    private ArrayList<String> test;
+    public Foo() {
+        this.test = new ArrayList<String>;
+    }
+
+    public void callProcess() {
+        //Process Call Comment
+        Process test = new ProcessBuilder("C:\\PathToExe\\MyExe.exe","param1","param2").start();
+    }
+
+}
