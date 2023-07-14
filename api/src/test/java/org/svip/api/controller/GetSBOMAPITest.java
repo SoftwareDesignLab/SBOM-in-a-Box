5de836aa98286b7a54450328f5445721c526720c
Derek Garcia <dlg1206@g.rit.edu>
2023-07-14 09:24:02 -0400

    stubbed out unit tests

---
+++ api/src/test/java/org/svip/api/controller/GetSBOMAPITest.java
@@ -0,0 +1,52 @@
+package org.svip.api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Derek Garcia
+ **/
+
+public class GetSBOMAPITest extends APITest{
+
+
+
+    @Test
+    @DisplayName("Parse Valid CDX14 JSON")
+    public void get_valid_CDX_14_SBOM_JSON(){
+
+    }
+
+    @Test
+    @DisplayName("Parse Valid SPDX23 Tag Value")
+    public void get_valid_SPDX_23_SBOM_TAGVALUE(){
+
+    }
+
+    @Test
+    @DisplayName("Parse Valid SPDX23 JSON")
+    public void get_valid_SPDX23_SBOM_JSON(){
+
+    }
+
+    @Test
+    @DisplayName("Parse Unsupported SBOM")
+    public void get_unsupported_SBOM_format(){
+
+    }
+
+    @Test
+    @DisplayName("Request SBOM with invalid ID value")
+    public void get_invalid_ID_SBOM(){
+
+    }
+
+    @Test
+    @DisplayName("Request SBOM with valid, but missing ID")
+    public void get_missing_ID_SBOM(){
+
+    }
+
+}
