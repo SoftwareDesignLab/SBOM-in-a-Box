0f862ef374a1f8bdc55c5b8b6c73a08cee81395d
txdvse <txdvse@rit.edu>
2023-07-14 14:40:22 -0400

    MergerSPDX.java + Added imports for spdx merging + added implementation for top level SBOM merging

--- core/src/main/java/org/svip/sbomanalysis/comparison/merger/MergerSPDX.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/merger/MergerSPDX.java
@@ -1,4 +1,104 @@
 package org.svip.sbomanalysis.comparison.merger;
 
-public class MergerSPDX {
+import org.svip.sbom.builder.objects.schemas.SPDX23.SPDX23Builder;
+
+import org.svip.sbom.model.interfaces.generics.Component;
+import org.svip.sbom.model.interfaces.generics.SBOM;
+
+import org.svip.sbom.model.objects.SPDX23.SPDX23SBOM;
+import org.svip.sbom.model.shared.metadata.Contact;
+import org.svip.sbom.model.shared.metadata.CreationData;
+import org.svip.sbom.model.shared.metadata.CreationTool;
+import org.svip.sbom.model.shared.metadata.Organization;
+import org.svip.sbom.model.shared.util.ExternalReference;
+import org.svip.sbom.model.shared.util.LicenseCollection;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
+public class MergerSPDX extends Merger {
+
+    public MergerSPDX(SPDX23SBOM A, SPDX23SBOM B) {
+        super(A, B);
+    }
+
+    @Override
+    protected SBOM mergeSBOM() {
+
+        Set<Component> componentsA = A.getComponents();
+        Set<Component> componentsB = B.getComponents();
+
+        // declare SBOM A as the main SBOM, cast it back to CDX14SBOM
+        SPDX23SBOM mainSBOM = (SPDX23SBOM) A;
+
+        // Create a new builder for the new SBOM
+        SPDX23Builder builder = new SPDX23Builder();
+
+        /** Assign all top level data for the new SBOM **/
+
+        // Format
+        builder.setFormat(mainSBOM.getFormat());
+
+        // Name
+        builder.setName(mainSBOM.getName());
+
+        // UID (In this case, bom-ref)
+        builder.setUID(mainSBOM.getUID());
+
+        // SBOM Version
+        builder.setVersion(mainSBOM.getVersion());
+
+        // Spec Version (1.4)
+        builder.setSpecVersion(mainSBOM.getSpecVersion());
+
+        // Licenses
+        for(String license : mainSBOM.getLicenses()) { builder.addLicense(license); }
+
+        // Creation Data
+        if(A.getCreationData() != null && B.getCreationData() != null) {
+            builder.setCreationData(mergeCreationData(A.getCreationData(), B.getCreationData()));
+        } else if (A.getCreationData() != null) {
+            builder.setCreationData(A.getCreationData());
+        } else if (B.getCreationData() != null) {
+            builder.setCreationData(B.getCreationData());
+        } else { builder.setCreationData(null); }
+
+        // Document Comment
+        builder.setDocumentComment(mainSBOM.getDocumentComment());
+
+        // Root Component
+        builder.setRootComponent(mainSBOM.getRootComponent());
+
+        // Components
+        Set<Component> mergedComponents = mergeComponents(componentsA, componentsB);
+        for(Component mergedComponent : mergedComponents) {
+            builder.addComponent(mergedComponent);
+        }
+
+        // Relationships TODO: Add merging of relationships in future sprint
+
+        // External References
+        //mergeExternalReferences(
+        //        A.getExternalReferences(), B.getExternalReferences()
+        //).forEach(x -> builder.addExternalReference(x));
+
+        // Return the newly built merged SBOM
+        return builder.Build();
+
+    }
+
+    @Override
+    protected Set<Component> mergeComponents(Set<Component> A, Set<Component> B) {
+        return null;
+    }
+
+    @Override
+    protected Component mergeComponent(Component A, Component B) {
+        return null;
+    }
+
+    @Override
+    protected CreationData mergeCreationData(CreationData A, CreationData B) {
+        return null;
+    }
 }
