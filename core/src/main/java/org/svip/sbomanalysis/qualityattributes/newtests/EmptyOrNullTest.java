9d0928220ff31d5301708861d4130f2b3b517b97
Matthew Morrison <msm8275@rit.edu>
2023-07-14 10:15:28 -0400

    Clean up EmptyOrNullTest

--- core/src/main/java/org/svip/sbomanalysis/qualityattributes/newtests/EmptyOrNullTest.java
+++ core/src/main/java/org/svip/sbomanalysis/qualityattributes/newtests/EmptyOrNullTest.java
@@ -6,7 +6,6 @@ import org.svip.sbomanalysis.qualityattributes.resultfactory.ResultFactory;
 import org.svip.sbomanalysis.qualityattributes.resultfactory.enumerations.INFO;
 
 import java.util.Collection;
-import java.util.List;
 
 /**
  * file: EmptyOrNullTest.java
@@ -17,61 +16,60 @@ import java.util.List;
  */
 public class EmptyOrNullTest {
 
-    private final String TEST_NAME = "EmptyOrNullTest";
-    private ResultFactory resultFactory;
+    private final ResultFactory resultFactory;
 
-    private final List<ATTRIBUTE> attributes;
 
     /**
      * Constructor class to create a new EmptyOrNullTest
+     *
      * @param attributes a list of attributes associates with the test
      */
-    public EmptyOrNullTest(ATTRIBUTE... attributes){
-        this.attributes = List.of(attributes);
+    public EmptyOrNullTest(ATTRIBUTE... attributes) {
+        String TEST_NAME = "EmptyOrNullTest";
         resultFactory = new ResultFactory(TEST_NAME, attributes);
     }
 
 
     /**
      * Utility function that checks if an object is null, a string is blank or a collection is empty
+     *
      * @param field Object to test
      * @return a result if the field is empty/null or has a value
      */
-    public Result test(String field, Object value, String context){
+    public Result test(String field, Object value, String context) {
         Result r;
-        boolean isEmptyorNull = false;
 
         // Check if value is null first
-        if(value == null){
+        if (value == null) {
             r = resultFactory.fail(field, INFO.NULL, (String) null, context);
             return r;
         }
+
+        // value is a string
         // Check for empty string if value is instance of a string
-        if(value instanceof String)
-            isEmptyorNull = ((String) value).isEmpty();
+        else if (value instanceof String) {
+            if (((String) value).isEmpty()) {
+                r = resultFactory.fail(field, INFO.MISSING, (String) value, context);
+            } else {
+                r = resultFactory.pass(field, INFO.HAS, (String) value, context);
+            }
+            return r;
+        }
 
+        // value is a collection of value
         // Check for empty collection is value is instance of a collection
-        if(value instanceof Collection<?>)
-            isEmptyorNull = ((Collection<?>) value).isEmpty();
-
-        // if value was a single string and was empty
-        if(value instanceof String && isEmptyorNull)
-            r = resultFactory.fail(field, INFO.MISSING, (String) value, context);
-
-        // if value was a collection of Strings and was empty
-        else if(value instanceof Collection<?> && isEmptyorNull){
-            r = resultFactory.fail(field, INFO.MISSING, (Collection<String>) value, context);
-        }
-        // if value was a single string and had a value
-        else if(!isEmptyorNull && value instanceof String ){
-            r = resultFactory.pass(field, INFO.HAS, (String) value, context);
-        }
-        // if value was a collection of Strings and was not empty
         else{
-            r = resultFactory.pass(field, INFO.HAS, (Collection<String>) value, context);
+            // if value is an empty collection, test fails
+            if (((Collection<?>) value).isEmpty()) {
+                r = resultFactory.fail(field, INFO.MISSING,
+                        (Collection<String>) value, context);
+            }
+            // collection is not empty, test passes
+            else {
+                r = resultFactory.pass(field, INFO.HAS,
+                        (Collection<String>) value, context);
+            }
+            return r;
         }
-
-        return r;
-
     }
 }
