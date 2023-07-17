fe6978a3d360020f1160ee4bcad4dd188587fcb7
dlg1206 <dlg1206@g.rit.edu>
2023-07-17 09:29:07 -0400

    Created new Conflict object and commented out build errors

--- core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/Conflict.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/Conflict.java
@@ -1,18 +1,102 @@
 package org.svip.sbomanalysis.comparison.conflicts;
 /**
  * file: Conflict.java
- * Generic class for Conflicts
+ * Representation for conflicts
  *
  * @author Thomas Roman
+ * @author Derek Garcia
  */
-public abstract class Conflict {
-    protected ConflictType type;
-    protected String message;
-    protected String target;
-    protected String other;
+public class Conflict {
 
-    Conflict(String field, String targetValue, String otherValue) {
-        target = targetValue;
-        other = otherValue;
+    private MismatchType type;
+    private String message;
+    private String target;
+    private String other;
+
+    /**
+     * Mismatch Conflict Constructor
+     *
+     * @param field Name of the conflict field
+     * @param mismatchType Type of mismatch
+     * @param target Target Value
+     * @param other Other Value
+     */
+    private Conflict(String field, MismatchType mismatchType, String target, String other){
+        this.type = mismatchType;
+        this.message = getMismatchMessage(field);
+        this.target = target;
+        this.other = other;
     }
+
+    /**
+     * Missing Conflict Constructor
+     *
+     * @param field Name of the conflict field
+     * @param target Target Value
+     * @param other Other Value
+     */
+    private Conflict(String field, String target, String other){
+        this.type = MismatchType.MISSING;
+        this.message = getMissingMessage(field);
+        this.target = target;
+        this.other = other;
+    }
+
+    ///
+    /// Helper Methods
+    ///
+
+    /**
+     * Get the present Missing message
+     *
+     * @param field Name of the field that is missing
+     * @return Missing message
+     */
+    private String getMissingMessage(String field){
+        return field + " is missing";
+    }
+
+    /**
+     * Get the preset Mismatch message
+     *
+     * @param field Name of the field that is mismatched
+     * @return Mismatch message
+     */
+    private String getMismatchMessage(String field){
+        return field + " doesn't match";
+    }
+
+    /**
+     * Creates a new conflict if one exists, otherwise returns null
+     *
+     * @param field Name of the conflict field
+     * @param mismatchType Type of mismatch
+     * @param target Target Value
+     * @param other Other Value
+     * @return New conflict object
+     */
+    public static Conflict buildConflict(String field, MismatchType mismatchType, String target, String other){
+
+        // Both are missing, no conflict
+        if(target == null && other == null)
+            return null;
+
+        // Target is missing
+        if(target == null || target.isEmpty())
+            return new Conflict(field, target, other);
+
+        // Other is missing
+        if(other == null || other.isEmpty())
+            return new Conflict(field, target, other);
+
+        // Mismatch
+        if(!target.equals(other))
+            return new Conflict(field, mismatchType, target, other);
+
+        // Default to no conflict
+        return null;
+    }
+
+
+
 }
