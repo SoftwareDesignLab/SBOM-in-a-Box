7e878fdcbae2a4c1b2b6431cbe77c382f436cc3d
dlg1206 <dlg1206@g.rit.edu>
2023-07-17 08:59:23 -0400

    stubbed out ConflictFactory

---
+++ core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/ConflictFactory.java
@@ -0,0 +1,41 @@
+package org.svip.sbomanalysis.comparison.conflicts;
+
+import org.checkerframework.checker.units.qual.C;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * File: ConflictFactory.java
+ * Handles creating new Conflicts
+ *
+ * @author Derek Garcia
+ */
+public class ConflictFactory {
+    private final List<Conflict> conflicts = new ArrayList<>();
+
+    /**
+     * Attempt to add a new Conflict. If there is no conflict, nothing is added
+     *
+     * @param field Name of the Comparison Field
+     * @param conflictType Type of conflict
+     * @param target Target value
+     * @param other Other value
+     */
+    public void addConflict(String field, ConflictType conflictType, String target, String other){
+//        // Attempt to make a conflict
+//        Conflict c = Conflict.buildConflict(field, conflictType, target, other);
+//
+//        // Add conflict if there is one
+//        if(c != null)
+//            this.conflicts.add(c);
+    }
+
+    /**
+     * @return List of stored conflicts
+     */
+    public List<Conflict> getConflicts(){
+        return this.conflicts;
+    }
+
+}
