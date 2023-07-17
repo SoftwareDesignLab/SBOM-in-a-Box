8575c9be7d6be5255cfd6d4b13cdfae87476918e
txdvse <txdvse@rit.edu>
2023-07-17 02:24:43 -0400

    MergerCDX.java + Import cleanup

--- core/src/main/java/org/svip/sbomanalysis/comparison/merger/MergerCDX.java
+++ core/src/main/java/org/svip/sbomanalysis/comparison/merger/MergerCDX.java
@@ -9,15 +9,8 @@ import org.svip.sbom.model.interfaces.generics.SBOM;
 import org.svip.sbom.model.objects.CycloneDX14.CDX14ComponentObject;
 import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
 
-import org.svip.sbom.model.shared.metadata.Contact;
-import org.svip.sbom.model.shared.metadata.CreationData;
-import org.svip.sbom.model.shared.metadata.CreationTool;
-import org.svip.sbom.model.shared.metadata.Organization;
-import org.svip.sbom.model.shared.util.ExternalReference;
 import org.svip.sbom.model.shared.util.LicenseCollection;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class MergerCDX extends Merger {
