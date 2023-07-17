95338e0015f9cde399ddc9f66ee3236212d0037d
Derek Garcia <dlg1206@g.rit.edu>
2023-07-17 10:48:19 -0400

    Added Contact equals method

--- core/src/main/java/org/svip/sbom/model/shared/metadata/Contact.java
+++ core/src/main/java/org/svip/sbom/model/shared/metadata/Contact.java
@@ -1,5 +1,10 @@
 package org.svip.sbom.model.shared.metadata;
 
+import org.svip.sbomanalysis.comparison.conflicts.Conflict;
+
+import java.util.List;
+import java.util.Objects;
+
 /**
  * File: Contact.java
  *
@@ -49,5 +54,38 @@ public class Contact {
      */
     public String getPhone() {
         return phone;
+    }
+
+    ///
+    /// Util
+    ///
+    public List<Conflict> compare(Contact other){
+        return null;
+    }
+
+    /**
+     * Compare based if any fields match since contact can be the same just incomplete
+     *
+     * @param o Other object
+     * @return True if they share any fields
+     */
+    @Override
+    public boolean equals(Object o) {
+        // Test if correct class
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact other = (Contact) o;
+
+        // Check if name equivalent
+        if(this.name.equals(other.getName()))
+            return true;
+
+        // Check if email equivalent
+        if(this.email.equals(other.getEmail()))
+            return true;
+
+        // no fields match if phone doesn't match
+        return this.phone.equals(other.getPhone());
+
+
     }
 }
