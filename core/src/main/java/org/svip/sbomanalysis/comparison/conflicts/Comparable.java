0fc00f591f63748bbfbc3ac708eda0e8921fb881
dlg1206 <dlg1206@g.rit.edu>
2023-07-17 23:11:41 -0400

    Conflict solution

---
+++ core/src/main/java/org/svip/sbomanalysis/comparison/conflicts/Comparable.java
@@ -0,0 +1,13 @@
+package org.svip.sbomanalysis.comparison.conflicts;
+
+import java.util.List;
+
+/**
+ * Comparable interface for comparing SBOM fields
+ *
+ * @author Derek Garcia
+ **/
+
+public interface Comparable {
+    List<Conflict> compare(Comparable o);
+}
