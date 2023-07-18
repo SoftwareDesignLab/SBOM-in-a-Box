8e88603e978b093f573ff25c7064382faa133f58
Matthew Morrison <msm8275@rit.edu>
2023-07-18 12:31:10 -0400

    Reworking of methods, updated using java.net.http and not using curl commands. Stubbed/TODO remaining classes

--- core/src/main/java/org/svip/sbomvex/database/OSVClient.java
+++ core/src/main/java/org/svip/sbomvex/database/OSVClient.java
@@ -1,9 +1,20 @@
 package org.svip.sbomvex.database;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.concurrent.CompletableFuture;
+
+import org.svip.sbom.model.interfaces.generics.SBOMPackage;
+import org.svip.sbomvex.database.interfaces.VulnerabilityDBClient;
+import org.svip.sbomvex.model.VEX;
+import org.svip.sbomvex.vexstatement.Product;
+import org.svip.sbomvex.vexstatement.VEXStatement;
+import org.svip.sbomvex.vexstatement.status.Justification;
+import org.svip.sbomvex.vexstatement.status.Status;
+import org.svip.sbomvex.vexstatement.Vulnerability;
+import org.svip.sbomvex.vexstatement.status.VulnStatus;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /**
  * file: OSVClient.java
@@ -11,67 +22,160 @@ import java.util.concurrent.CompletableFuture;
  *
  * @author Matthew Morrison
  */
-public class OSVClient {
+public class OSVClient implements VulnerabilityDBClient {
 
-    /**url endpoint to access OSV database*/
-    private final String ENDPOINT = "\"https://api.osv.dev/v1/query\"";
+    /**url endpoint to access OSV database POST methods*/
+    private final String POST_ENDPOINT = "https://api.osv.dev/v1/query";
 
+    /**url endpoint to access OSV database GET method*/
+    private final String GET_ENDPOINT = "https://api.osv.dev/v1/vulns/";
 
-    private String buildCommandWithParams(String... params) {
-        String CURL_COMMAND = "curl -d ";
+    HttpClient httpClient = HttpClient.newHttpClient();
 
-
-        return CURL_COMMAND + Arrays.toString(params) + ENDPOINT;
-    }
-
-    private CompletableFuture<String> getRequestAsync(String... params) {
-        String command = buildCommandWithParams(params);
-
-        try{
-            // TODO how to implement?
-            Runtime rt = Runtime.getRuntime();
-            Process pr = rt.exec(command);
-
-            BufferedReader input = new BufferedReader(new InputStreamReader(
-                    pr.getInputStream()));
-            return null;
-        } catch(Exception e){
-            return null;
+    /**
+     * Build the API Request, then get the OSV database's response
+     * @param jsonBody the string body of the request or OSV ID for GET
+     * @param requestMethod the type of request method to differentiate
+     * the builder
+     * @return OSV APIs response to the HttpRequest
+     */
+    private String getOSVResponse(String jsonBody, String requestMethod) {
+        try {
+            HttpRequest request;
+            // build the post method, most requests will follow this
+            if(requestMethod.equals("post")){
+                request = HttpRequest.newBuilder()
+                        .uri(URI.create(POST_ENDPOINT))
+                        .POST(HttpRequest.BodyPublishers.ofString(jsonBody)).build();
+            }
+            // only one request uses get, specific to OSV ID
+            else{
+                String getVulnsByID = GET_ENDPOINT + jsonBody;
+                request = HttpRequest.newBuilder()
+                        .uri(URI.create(getVulnsByID)).GET().build();
+            }
+            // send the response and get the APIs response
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
         }
-
+        // error with the HttpRequest occurs
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public CompletableFuture<String> getOSVByNameVersion(String componentName, String componentVersion) {
-        String commandString = "\"{\\\"package\\\": {\\\"name\\\": \\\"" +
-                componentName + "\\\"}, \\\"version\\\": " +
-                componentVersion + "\\\"}\" ";
-        return getRequestAsync(commandString);
+
+    /**
+     * Generate the json body for the API request using the component's
+     * name and version
+     * @param componentName the component's name
+     * @param componentVersion the component's version
+     * @return the response from the OSV API request
+     */
+    public String getOSVByNameVersionPost(String componentName, String componentVersion) {
+        String jsonBody = "{\"package\": {\"name\": \"" +
+                componentName + "\"}, \"version\": " +
+                componentVersion + "\"}";
+        return getOSVResponse(jsonBody, "post");
     }
 
-    public CompletableFuture<String> getOSVByNameVersionEcosystem(
+    /**
+     * Generate the json body for the API request using the component's
+     * name, version, and ecosystem
+     * @param componentName the component's name
+     * @param componentVersion the component's version
+     * @param ecosystem the component's ecosystem
+     * @return the response from the OSV API request
+     */
+    public String getOSVByNameVersionEcosystemPost(
             String componentName, String componentVersion, String ecosystem) {
-        String commandString = "\"{\\\"package\\\": {\\\"name\\\": \\\"" +
-                componentName + "\\\", \\\"ecosystem\\\": " +   "\\\"" +
-                ecosystem + "\\\"}, \\\"version\\\": " +
-                componentVersion + "\\\"}\" ";
-        return getRequestAsync(commandString);
+        String jsonBody = "{\"package\": {\"name\": \"" +
+                componentName + "\", \"ecosystem\": " +   "\"" +
+                ecosystem + "\"}, \"version\": " +
+                componentVersion + "\"} ";
+        return getOSVResponse(jsonBody, "post");
     }
 
-    public CompletableFuture<String> getOSVByPURL(String purlString) {
-        String commandString = "\"{\\\"package\\\": {\\\"purl\\\": \\\"" +
-                purlString + "\\\"}}\" ";
-        return getRequestAsync(commandString);
+    /**
+     * Generate the json body for the API request with the component's PURL
+     * @param purlString the component's purl
+     * @return the response from the OSV API request
+     */
+    public String getOSVByPURLPost(String purlString) {
+        String jsonBody = "{\"package\": {\"purl\": \"" +
+                purlString + "\"}} ";
+        return getOSVResponse(jsonBody, "post");
     }
 
-    public CompletableFuture<String> getOSVByCommitAsync(String commitString) {
-        String commandString = "\"{\\\"commit\\\": " +
-                commitString + "\\\"}\" ";
-        return getRequestAsync(commandString);
+    /**
+     * Generate the json body for the API request with the most
+     * recent commit of the component
+     * @param commitString the commit string
+     * @return the response from the OSV API request
+     */
+    public String getOSVByCommitAsyncPost(String commitString) {
+        String jsonBody = "{\"commit\": " +
+                commitString + "\"} ";
+        return getOSVResponse(jsonBody, "post");
+    }
+
+    /**
+     * Find a vulnerability through its OSV ID
+     * @param osvID the OSV ID to search for
+     * @return the response from the OSV API request
+     */
+    public String getVulnByIdGet(String osvID){
+        return getOSVResponse(osvID, "get");
     }
 
 
-
+    /**
+     * Generate a new VEX document
+     * @param sbomPackage the SBOMPackage to create the VEX Document
+     * @return a new VEX object
+     */
     //TODO implement
-    private void run(){
+    @Override
+    public VEX generateVEX(SBOMPackage sbomPackage) {
+        // create vex builder to create VEX document
+        VEX.Builder vexBuilder = new VEX.Builder();
+
+        // initially get the necessary fields for the VEX Document builder
+        String creationTime = String.valueOf(java.time.LocalDateTime.now());
+        // TODO how to determine these fields?
+//        vexBuilder.setVEXIdentifier();
+//        vexBuilder.setOriginType();
+//        vexBuilder.setSpecVersion();
+        vexBuilder.setDocVersion("1.0");
+        vexBuilder.setTimeFirstIssued(creationTime);
+        vexBuilder.setTimeLastUpdated(creationTime);
+
+        //TODO loop through response and get content to build VEXStatements
+
+
+        return null;
+    }
+
+
+    public VEXStatement generateVEXStatement(){
+        return null;
+    }
+
+    public Status createAffectedStatus(String actionStatement){
+        return new Status(VulnStatus.AFFECTED, null, actionStatement,
+                null);
+    }
+
+    public Status createNotAffectedStatus(Justification justification, String impactStatement){
+        return new Status(VulnStatus.NOT_AFFECTED, justification, null,
+                impactStatement);
+    }
+
+    public Product createProduct(String productID, String supplier){
+        return new Product(productID, supplier);
+    }
+
+    public Vulnerability createVulnerability(String id, String description){
+        return new Vulnerability(id, description);
     }
 }
