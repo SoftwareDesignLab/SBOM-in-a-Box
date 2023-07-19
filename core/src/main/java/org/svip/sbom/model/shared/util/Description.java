3605b6e1a512fc852ea5cce914de74030ac7f49e
dlg1206 <dlg1206@g.rit.edu>
2023-07-19 16:03:07 -0400

    added isequals and compare method for description

--- core/src/main/java/org/svip/sbom/model/shared/util/Description.java
+++ core/src/main/java/org/svip/sbom/model/shared/util/Description.java
@@ -1,9 +1,16 @@
 package org.svip.sbom.model.shared.util;
 
+import org.checkerframework.checker.units.qual.C;
+import org.svip.sbom.model.shared.metadata.CreationData;
+import org.svip.sbom.model.shared.metadata.CreationTool;
 import org.svip.sbomanalysis.comparison.conflicts.Comparable;
 import org.svip.sbomanalysis.comparison.conflicts.Conflict;
+import org.svip.sbomanalysis.comparison.conflicts.ConflictFactory;
+import org.svip.sbomanalysis.comparison.conflicts.MismatchType;
 
 import java.util.List;
+
+import static org.svip.sbomanalysis.comparison.conflicts.MismatchType.MISC_MISMATCH;
 
 /**
  * File: Description.java
@@ -52,7 +59,28 @@ public class Description implements Comparable {
 
     @Override
     public List<Conflict> compare(Comparable o) {
-        // todo
-        return null;
+        // Don't compare if not instance of same object
+        if(!(o instanceof Description other))
+            return null;
+
+        ConflictFactory cf = new ConflictFactory();
+
+        cf.addConflict("Summary", MISC_MISMATCH, this.summary, other.getSummary());
+        cf.addConflict("Description", MISC_MISMATCH, this.description, other.getDescription());
+
+        return cf.getConflicts();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Description other = (Description) o;
+
+        // compare summary
+        if(!this.summary.equals(other.getSummary()))
+            return false;
+
+        // compare descriptions
+        return this.description.equals(other.getDescription());
     }
 }
