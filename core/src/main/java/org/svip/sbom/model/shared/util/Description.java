115d071d2abfcc81b84bf26173c2dc4f3e12763b
dlg1206 <dlg1206@g.rit.edu>
2023-07-19 15:57:55 -0400

    refactor CDX14ComponentObject

--- core/src/main/java/org/svip/sbom/model/shared/util/Description.java
+++ core/src/main/java/org/svip/sbom/model/shared/util/Description.java
@@ -1,12 +1,17 @@
 package org.svip.sbom.model.shared.util;
 
+import org.svip.sbomanalysis.comparison.conflicts.Comparable;
+import org.svip.sbomanalysis.comparison.conflicts.Conflict;
+
+import java.util.List;
+
 /**
  * File: Description.java
  * Utility object to hold description information
  *
  * @author Derek Garcia
  */
-public class Description {
+public class Description implements Comparable {
     private final String summary;
     private String description;
 
@@ -43,5 +48,11 @@ public class Description {
      */
     public String getDescription() {
         return description;
+    }
+
+    @Override
+    public List<Conflict> compare(Comparable o) {
+        // todo
+        return null;
     }
 }
