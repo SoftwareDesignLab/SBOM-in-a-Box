6d064faa11b55936f05a0d1bfde74c42ea495a8b
Matthew Morrison <msm8275@rit.edu>
2023-07-27 11:38:12 -0400

    Added methods to allow for an api key to be entered and used

--- core/src/main/java/org/svip/sbomvex/database/NVDClient.java
+++ core/src/main/java/org/svip/sbomvex/database/NVDClient.java
@@ -29,25 +29,37 @@ import java.util.concurrent.TimeUnit;
  * @author Henry Lu
  */
 public class NVDClient implements VulnerabilityDBClient {
-    private static final String ENDPOINT = "https://services.nvd.nist.gov/rest/json/cves/2.0";
+    private static final String DEFAULT_ENDPOINT = "https://services.nvd.nist.gov/rest/json/cves/2.0";
     private final HttpClient httpClient = HttpClient.newHttpClient();
 
     /**
-     * build the url for the api request using the component's cpe
+     * build the url for the api request using the component's cpe and the
+     * default endpoint
      * @param cpe the cpe string
      * @return a String of the concatenated URL string
      */
     private String buildURLWithCPE(String cpe){
-        return ENDPOINT.concat("?cpeName=").concat(cpe);
+        return DEFAULT_ENDPOINT.concat("?cpeName=").concat(cpe);
     }
 
     /**
-     * run the api request and returns the response
+     * build the url for the api request using the component's cpe and the
+     * user's API key
+     * @param cpe the cpe string
+     * @param key the user's NVD API key to use
+     * @return a String of the concatenated URL string
+     */
+    private String buildURLWithUserKey(String cpe, String key){
+        return key.concat("?cpeName=").concat(cpe);
+    }
+
+    /**
+     * run the api request and returns the response with the default key
      * @param cpe the cpe string
      * @return the response from the NVD API
      * @throws Exception if an error occurs with getting NVD's response
      */
-    private String vexFieldsWithCPE(String cpe) throws Exception{
+    private String accessNVD(String cpe) throws Exception{
         var request = HttpRequest.newBuilder()
                 .uri(URI.create(buildURLWithCPE(cpe)))
                 .GET()
@@ -59,15 +71,34 @@ public class NVDClient implements VulnerabilityDBClient {
     }
 
     /**
-     * Get all vulnerabilities of a component using the NVD API
+     * run the api request and returns the response with the user's key
+     * @param cpe the cpe string
+     * @param key the user's NVD API Key to use
+     * @return the response from the NVD API
+     * @throws Exception if an error occurs with getting NVD's response
+     */
+    private String accessNVD(String cpe, String key) throws Exception{
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(buildURLWithUserKey(cpe, key)))
+                .GET()
+                .build();
+
+        CompletableFuture<HttpResponse<String>> apiResponse = httpClient
+                .sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        return apiResponse.get().body();
+    }
+
+    /**
+     * Get all vulnerabilities of a component using the NVD API default key
      * @param s the SBOM package to create the VEX Statement for
      * @return a List of VEX Statements if there are vulnerabilities or
      * throw a general Exception if none are found
-     * @throws Exception if no vulnerabilities are found or an error occurs
+     * @throws Exception an error occurs when getting vulnerabilities
      */
     @Override
     public List<VEXStatement> getVEXStatements(SBOMPackage s) throws Exception{
         List<VEXStatement> vexStatements = new ArrayList<>();
+        int waitTime = 6;
         String response;
         String componentID;
         Set<String> cpes = s.getCPEs();
@@ -81,8 +112,8 @@ public class NVDClient implements VulnerabilityDBClient {
         ArrayList<String> cpesList = new ArrayList<>(cpes);
         String cpeString = cpesList.get(0);
         componentID = cpeString;
-        response = vexFieldsWithCPE(cpeString);
-        TimeUnit.SECONDS.sleep(6);
+        response = accessNVD(cpeString);
+        TimeUnit.SECONDS.sleep(waitTime);
 
 
         // check that the response was not null to find vulnerabilities
@@ -103,6 +134,54 @@ public class NVDClient implements VulnerabilityDBClient {
         }
         return vexStatements;
     }
+
+    /**
+     * Get all vulnerabilities of a component using the user's API key
+     * @param s the SBOM package to create the VEX Statement for
+     * @param key the user's NVD API key to use
+     * @return a List of VEX Statements if there are vulnerabilities or
+     * throw a general Exception if none are found
+     * @throws Exception an error occurs when getting vulnerabilities
+     */
+    public List<VEXStatement> getVEXStatements(SBOMPackage s, String key) throws Exception {
+        List<VEXStatement> vexStatements = new ArrayList<>();
+        int waitTime = 2;
+        String response;
+        String componentID;
+        Set<String> cpes = s.getCPEs();
+
+        // if component has no cpes continue as we can only search if there are cpes
+        if(cpes==null || cpes.isEmpty())
+            throw new Exception("Component does not have CPEs " +
+                    "to test with NVD API");
+
+        // use the cpes to search for vulnerabilities
+        ArrayList<String> cpesList = new ArrayList<>(cpes);
+        String cpeString = cpesList.get(0);
+        componentID = cpeString;
+        response = accessNVD(cpeString, key);
+        TimeUnit.SECONDS.sleep(waitTime);
+
+
+        // check that the response was not null to find vulnerabilities
+        if(response != null){
+            JSONObject jsonResponse = new JSONObject(response);
+            JSONArray vulns = new JSONArray(
+                    jsonResponse.getJSONArray("vulnerabilities"));
+            for (int i = 0; i < vulns.length(); i++) {
+                // get the singular vulnerability and create a
+                // VEXStatement for it
+                JSONObject vulnerability = vulns.getJSONObject(i)
+                        .getJSONObject("cve");
+                VEXStatement statement =
+                        generateVEXStatement(vulnerability,
+                                s, componentID);
+                vexStatements.add(statement);
+            }
+        }
+        return vexStatements;
+    }
+
 
     /**
      * Build a new VEX Statement for a VEX Document
