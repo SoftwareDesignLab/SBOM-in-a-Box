9a0c4f6bf0bf02bafa408a866bcc43a5c36ccdc9
dlg1206 <dlg1206@g.rit.edu>
2023-08-01 12:14:44 -0400

    fix build errors

--- core/src/test/java/org/svip/sbomanalysis/compare/MergerTest.java
+++ core/src/test/java/org/svip/sbomanalysis/compare/MergerTest.java
@@ -19,7 +19,6 @@ import org.svip.sbom.model.shared.metadata.Organization;
 import org.svip.sbom.model.shared.util.Description;
 import org.svip.sbom.model.shared.util.ExternalReference;
 import org.svip.sbom.model.shared.util.LicenseCollection;
-import org.svip.sbomanalysis.compare.merge.*;
 
 import java.util.*;
 
