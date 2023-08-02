f6ed0686ba6266aedb573a5b17b287277c58c0c8
juanfpatino <jfp6815@rit.edu>
2023-08-02 12:40:39 -0400

    Corrected SBOMFile.id decorators

--- api/src/main/java/org/svip/api/repository/SBOMFileIdentifierGenerator.java
+++ api/src/main/java/org/svip/api/repository/SBOMFileIdentifierGenerator.java
@@ -4,6 +4,7 @@ import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
+import java.io.Serializable;
 import java.util.Random;
 
 /**
@@ -12,12 +13,15 @@ import java.util.Random;
  * @author Juan Francisco Patino
  */
 public class SBOMFileIdentifierGenerator implements IdentifierGenerator {
+
+    public final static String generatorName = "SBOMIdGenerator";
+
     @Override
-    public Long generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
+    public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
         // assign new id and name
         Random rand = new Random();
         long id = rand.nextLong();
-        id += (Math.abs(rand.nextLong())) % ((id < 0) ? id : Long.MAX_VALUE);
-        return id;
+        id += (rand.nextLong()) % ((id < 0) ? id : Long.MAX_VALUE);
+        return Math.abs(id);
     }
 }
