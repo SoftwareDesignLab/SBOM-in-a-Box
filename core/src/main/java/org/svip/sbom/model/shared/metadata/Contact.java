d615d5541628171605ab2d88dd8dd1fdb11cfe4c
Tom-Roman <tfr8811@rit.edu>
2023-08-02 13:11:35 -0400

    toString functions

--- core/src/main/java/org/svip/sbom/model/shared/metadata/Contact.java
+++ core/src/main/java/org/svip/sbom/model/shared/metadata/Contact.java
@@ -14,6 +14,7 @@ import static org.svip.compare.conflicts.MismatchType.MISC_MISMATCH;
  * Represents contact information
  *
  * @author Derek Garcia
+ * @author Thomas Roman
  */
 public class Contact implements Comparable {
     private final String name;
@@ -104,5 +105,10 @@ public class Contact implements Comparable {
 
         // no fields match if phone doesn't match
         return this.phone.equals(other.getPhone());
+    }
+
+    @Override
+    public String toString() {
+        return "Organization: " + this.name + "(" + this.email + ")";
     }
 }
