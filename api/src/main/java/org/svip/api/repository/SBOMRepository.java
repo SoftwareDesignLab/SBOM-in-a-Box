20be3c0af980b332cbe82fd750203b2bd762a8d2
Derek Garcia <dlg1206@g.rit.edu>
2023-08-02 18:11:58 -0400

    stubbed out jpa style api files

--- api/src/main/java/org/svip/api/repository/SBOMRepository.java
+++ api/src/main/java/org/svip/api/repository/SBOMRepository.java
@@ -1,13 +1,12 @@
 package org.svip.api.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.svip.api.entities.SBOM;
+import org.svip.sbom.model.interfaces.generics.SBOM;
 
 /**
- * Repository for storing SBOM Files
- * CRUD methods: https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
- * todo rename SBOMFileRepository
  * @author Derek Garcia
  **/
+
 public interface SBOMRepository extends CrudRepository<SBOM, Long> {
+
 }
