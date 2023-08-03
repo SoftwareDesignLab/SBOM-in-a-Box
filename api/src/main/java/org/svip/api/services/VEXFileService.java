7701e063ac4dcf50a63fd85ab9502fc3505c843a
Derek Garcia <dlg1206@g.rit.edu>
2023-08-03 16:47:33 -0400

    stubbed out future API changes

---
+++ api/src/main/java/org/svip/api/services/VEXFileService.java
@@ -0,0 +1,12 @@
+package org.svip.api.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Business logic for accessing the VEX File table
+ *
+ * @author Derek Garcia
+ **/
+@Service
+public class VEXFileService {
+}
