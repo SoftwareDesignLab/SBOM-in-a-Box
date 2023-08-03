30bcc841a021697b128a11de5ff2e8c1faf17212
Derek Garcia <dlg1206@g.rit.edu>
2023-08-03 16:56:43 -0400

    added missing generation endpoints

---
+++ api/src/main/java/org/svip/api/controller/ParserController.java
@@ -0,0 +1,14 @@
+package org.svip.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST API Controller for generating SBOMs using Parsers
+ *
+ * @author Derek Garcia
+ **/
+@RestController
+@RequestMapping("/svip/generators")
+public class ParserController {
+}
