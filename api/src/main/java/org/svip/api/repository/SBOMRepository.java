1d279048458c831f810737732c6225d02e728e58
Derek Garcia <dlg1206@g.rit.edu>
2023-08-03 12:11:02 -0400

    initial view sboms endpoints

--- api/src/main/java/org/svip/api/repository/SBOMRepository.java
+++ api/src/main/java/org/svip/api/repository/SBOMRepository.java
@@ -8,5 +8,5 @@ import org.svip.api.model.SBOM;
  **/
 
 public interface SBOMRepository extends CrudRepository<SBOM, Long> {
-
+    // CRUD methods: https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
 }
