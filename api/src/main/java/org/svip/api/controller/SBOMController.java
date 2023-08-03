5e3ab33939df37d5dbedfc9b9d9e74b8a1217639
Derek Garcia <dlg1206@g.rit.edu>
2023-08-03 12:22:07 -0400

    added sbom docstrings

--- api/src/main/java/org/svip/api/controller/SBOMController.java
+++ api/src/main/java/org/svip/api/controller/SBOMController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.svip.api.services.SBOMService;
 
 /**
+ * REST API Controller for Managing SBOM and SBOM operations
+ *
  * @author Derek Garcia
  **/
 @RestController
@@ -17,18 +19,33 @@ public class SBOMController {
     private final SBOMService sbomService;
 
 
+    /**
+     * Create new Controller with services
+     *
+     * @param sbomService Service for handling SBOM queries
+     */
     public SBOMController(SBOMService sbomService){
         this.sbomService = sbomService;
     }
 
+
+    /**
+     * USAGE. Send GET request to /sboms.
+     * The API will respond with an HTTP 200 and a JSON array of all IDs of currently uploaded SBOM files.
+     *
+     * @return A JSON array of IDs of all currently uploaded SBOM files.
+     */
     @GetMapping("/sboms")
     public ResponseEntity<Long[]> getAllIds(){
 
+        // Get all ids
         Long[] ids = this.sbomService.getAllIDs();
 
+        // report nothing if no SBOMs in the database
         if(ids.length == 0)
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
+        // Else return the array of stored IDs
         return new ResponseEntity<>(ids, HttpStatus.OK);
     }
 
--- api/src/main/java/org/svip/api/repository/SBOMRepository.java
+++ api/src/main/java/org/svip/api/repository/SBOMRepository.java
@@ -1,12 +1,14 @@
 package org.svip.api.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.svip.api.model.SBOM;
+import org.svip.api.entities.SBOM;
 
 /**
+ * Repository for storing SBOMs
+ * CRUD methods: https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
+ *
  * @author Derek Garcia
  **/
 
 public interface SBOMRepository extends CrudRepository<SBOM, Long> {
-    // CRUD methods: https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
 }
