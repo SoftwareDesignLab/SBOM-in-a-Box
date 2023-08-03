ea77506b4aef0ca630634974bd6ea13f95d74894
Derek Garcia <dlg1206@g.rit.edu>
2023-08-03 12:22:07 -0400

    added sbom docstrings

--- api/src/test/java/org/svip/api/controller/CompareAPITest.java
+++ api/src/test/java/org/svip/api/controller/CompareAPITest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.svip.api.model.SBOMFile;
+import org.svip.api.entities.SBOMFile;
 import org.svip.compare.DiffReport;
 
 import java.util.Map;
