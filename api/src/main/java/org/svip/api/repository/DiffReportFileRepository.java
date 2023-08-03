6d09af769e71428d040f2f3495c91d507b08a2c2
Derek Garcia <dlg1206@g.rit.edu>
2023-08-03 16:47:33 -0400

    stubbed out future API changes

---
+++ api/src/main/java/org/svip/api/repository/DiffReportFileRepository.java
@@ -0,0 +1,13 @@
+package org.svip.api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.svip.api.entities.DiffReportFile;
+
+/**
+ * Repository for storing Diff Report Files
+ * CRUD methods: https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
+ *
+ * @author Derek Garcia
+ **/
+public interface DiffReportFileRepository extends CrudRepository<DiffReportFile, Long> {
+}
