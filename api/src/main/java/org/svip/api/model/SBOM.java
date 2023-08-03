5f5f74017cee578a1520bba3de82dcef2b2f55dc
Derek Garcia <dlg1206@g.rit.edu>
2023-08-03 10:57:10 -0400

    fixed mysql keyword bug and added filetype

---
+++ api/src/main/java/org/svip/api/model/SBOM.java
@@ -0,0 +1,45 @@
+package org.svip.api.model;
+
+import jakarta.persistence.*;
+
+/**
+ * @author Derek Garcia
+ **/
+
+@Entity
+@Table(name = "SBOMS")
+public class SBOM {
+
+    // Schema of SBOM
+    public enum Schema{
+        CYCLONEDX_14,
+        SPDX_23
+    }
+
+    // File Type of SBOM
+    public enum FileType{
+        JSON,
+        TAG_VALUE
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "schema_type")
+    private Schema schema;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "file_type")
+    private FileType fileType;
+
+}
