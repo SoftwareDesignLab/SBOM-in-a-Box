7d2b2773d6a8cdfb7a1d530710eb63965065b66c
Derek Garcia <dlg1206@g.rit.edu>
2023-08-03 09:28:00 -0400

    Comment out old controller and features

--- api/src/main/java/org/svip/api/services/SBOMService.java
+++ api/src/main/java/org/svip/api/services/SBOMService.java
@@ -1,11 +1,12 @@
 package org.svip.api.services;
 
+import org.springframework.stereotype.Service;
 import org.svip.api.repository.SBOMRepository;
 
 /**
  * @author Derek Garcia
  **/
-
+@Service
 public class SBOMService {
     private final SBOMRepository sbomRepository;
 
