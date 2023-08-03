e19654143088820bc7bbcd62343d67b02cf3a0ec
Derek Garcia <dlg1206@g.rit.edu>
2023-08-03 16:47:33 -0400

    stubbed out future API changes

--- api/src/main/java/org/svip/api/controller/SBOMController.java
+++ api/src/main/java/org/svip/api/controller/SBOMController.java
@@ -7,11 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.svip.api.entities.SBOM;
-import org.svip.api.entities.SBOMFile;
 import org.svip.api.requests.UploadSBOMFileInput;
 import org.svip.api.services.SBOMFileService;
 import org.svip.api.entities.SBOMFile;
 import org.svip.api.requests.UploadSBOMFileInput;
+import org.svip.api.services.SBOMFileService;
 import org.svip.api.services.SBOMFileService;
 import org.svip.api.services.SBOMFileService;
 
