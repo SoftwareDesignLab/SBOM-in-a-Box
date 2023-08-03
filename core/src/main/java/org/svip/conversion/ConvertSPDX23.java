ace0af2b4432d8a5be4c5d944e497851790901ae
txdvse <txdvse@rit.edu>
2023-08-03 16:25:17 -0400

    Base skeleton code for Conversion. + Added Conversion driver code + Added generic + Skeleton for ConvertCDX14.java + Skeleton for ConvertSPDX23.java

---
+++ core/src/main/java/org/svip/conversion/ConvertSPDX23.java
@@ -0,0 +1,13 @@
+package org.svip.conversion;
+
+import org.svip.sbom.model.objects.SPDX23.SPDX23SBOM;
+import org.svip.sbom.model.objects.SVIPSBOM;
+
+public class ConvertSPDX23 implements Convert {
+
+    @Override
+    public SPDX23SBOM convert(SVIPSBOM sbom) {
+        return null;
+    }
+
+}
