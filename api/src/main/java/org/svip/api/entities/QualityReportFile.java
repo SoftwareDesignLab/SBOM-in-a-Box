a74ffc32bd5c4a763bd3262f115d50d19fe2afdd
Derek Garcia <dlg1206@g.rit.edu>
2023-08-04 09:30:33 -0400

    initial qa table

--- api/src/main/java/org/svip/api/entities/QualityReportFile.java
+++ api/src/main/java/org/svip/api/entities/QualityReportFile.java
@@ -3,7 +3,6 @@ package org.svip.api.entities;
 import jakarta.persistence.*;
 
 /**
- * file: QualityReportFile.java
  * Quality Report Table for the database
  *
  * @author Derek Garcia
@@ -19,57 +18,6 @@ public class QualityReportFile {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false, columnDefinition="LONGTEXT")
-    private String content;
-
-    @OneToOne(mappedBy = "qualityReportFile")   // name of field in SBOMFile NOT DB
+    @OneToOne(mappedBy = "quality_report_file")
     private SBOM sbom;
-
-
-    ///
-    /// Setters
-    ///
-
-    /**
-     * @param name Name / UID of qa
-     * @return QualityReportFile
-     */
-    public QualityReportFile setName(String name){
-        this.name = name;
-        return this;
-    }
-
-    /**
-     * todo store properly rather than massive string
-     *
-     * @param content content of qa
-     * @return QualityReportFile
-     */
-    public QualityReportFile setContent(String content){
-        this.content = content;
-        return this;
-    }
-
-    public QualityReportFile setSBOM(SBOM sbom){
-        this.sbom = sbom;
-        return this;
-    }
-
-    ///
-    /// Getters
-    ///
-
-    /**
-     * @return id of qa
-     */
-    public Long getID(){
-        return this.id;
-    }
-
-    /**
-     * @return content of qa
-     */
-    public String getContent(){
-        return this.content;
-    }
 }
