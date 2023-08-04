fdd45604b09cf60b1679e97be050c90e860f4f0f
Derek Garcia <dlg1206@g.rit.edu>
2023-08-04 09:35:51 -0400

    stubbed out qa controller/service

--- api/src/main/java/org/svip/api/services/QualityReportFileService.java
+++ api/src/main/java/org/svip/api/services/QualityReportFileService.java
@@ -1,6 +1,8 @@
 package org.svip.api.services;
 
 import org.springframework.stereotype.Service;
+import org.svip.api.repository.QualityReportFileRepository;
+import org.svip.api.repository.SBOMRepository;
 
 /**
  * Business logic for accessing the Quality Report File table
@@ -9,4 +11,15 @@ import org.springframework.stereotype.Service;
  **/
 @Service
 public class QualityReportFileService {
+
+    private final QualityReportFileRepository qualityReportFileRepository;
+
+    /**
+     * Create new Service for a target repository
+     *
+     * @param qualityReportFileRepository QA repository to access
+     */
+    public QualityReportFileService(QualityReportFileRepository qualityReportFileRepository){
+        this.qualityReportFileRepository = qualityReportFileRepository;
+    }
 }
