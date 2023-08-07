b263104d9aea131080d5f4a2c4fa4bb2b20d4a52
Derek Garcia <dlg1206@g.rit.edu>
2023-08-07 15:17:00 -0400

    initial VEX table

--- api/src/main/java/org/svip/api/entities/VEXFile.java
+++ api/src/main/java/org/svip/api/entities/VEXFile.java
@@ -1,8 +1,6 @@
 package org.svip.api.entities;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import org.svip.vex.model.VEXType;
 
 /**
  * VEX Table for the database
@@ -12,13 +10,6 @@ import org.svip.vex.model.VEXType;
 @Entity
 @Table(name = "vex_file")
 public class VEXFile {
-
-    // Database used for generation
-    public enum Database{
-        NVD,
-        OSV
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
@@ -30,59 +21,6 @@ public class VEXFile {
     @Column(nullable = false, columnDefinition="LONGTEXT")
     private String content;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, name = "schema_type")
-    private VEXType schema;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, name = "datasource")
-    private Database datasource;
-
-
-    @OneToOne(mappedBy = "vexFile")   // name of field in SBOMFile NOT DB
+    @OneToOne(mappedBy = "vex")   // name of field in SBOMFile NOT DB
     private SBOM sbom;
-
-
-    ///
-    /// Setters
-    ///
-
-    public VEXFile setName(String name){
-        this.name = name;
-        return this;
-    }
-
-    public VEXFile setContent(String content){
-        this.content = content;
-        return this;
-    }
-
-    public VEXFile setSchema(VEXType schema){
-        this.schema = schema;
-        return this;
-    }
-
-    public VEXFile setDatasource(Database database){
-        this.datasource = database;
-        return this;
-    }
-
-    public VEXFile setSBOM(SBOM sbom){
-        this.sbom = sbom;
-        return this;
-    }
-
-
-    ///
-    /// Getters
-    ///
-
-
-    public Long getID() {
-        return this.id;
-    }
-
-    public String getContent() {
-        return this.content;
-    }
 }
