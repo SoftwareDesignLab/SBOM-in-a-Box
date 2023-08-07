b442806a2fb51cf6b98f7d81e02e3fa7ed8ebd4f
Derek Garcia <dlg1206@g.rit.edu>
2023-08-07 09:48:22 -0400

    initial generate qa

--- api/src/main/java/org/svip/api/services/QualityReportFileService.java
+++ api/src/main/java/org/svip/api/services/QualityReportFileService.java
@@ -1,9 +1,17 @@
 package org.svip.api.services;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import org.svip.api.entities.QualityReportFile;
-import org.svip.api.entities.SBOM;
 import org.svip.api.repository.QualityReportFileRepository;
+import org.svip.metrics.pipelines.QualityReport;
+import org.svip.metrics.pipelines.interfaces.generics.QAPipeline;
+import org.svip.metrics.pipelines.schemas.CycloneDX14.CDX14Pipeline;
+import org.svip.metrics.pipelines.schemas.SPDX23.SPDX23Pipeline;
+import org.svip.sbom.model.interfaces.generics.SBOM;
+import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
+import org.svip.sbom.model.objects.SPDX23.SPDX23SBOM;
 
 /**
  * Business logic for accessing the Quality Report File table
@@ -40,5 +48,24 @@ public class QualityReportFileService {
             // todo custom exception instead of generic
             throw new Exception("Failed to upload to Database: " + e.getMessage());
         }
+    }
+
+
+    public QualityReport generateQualityReport(SBOM sbom) throws Exception {
+
+        // todo QAPipeline factory to handle this? similar to serializerFactory?
+        QAPipeline qaPipeline;
+        // Determine what QA Pipeline to use based on
+        if (sbom instanceof CDX14SBOM) {
+            qaPipeline = new CDX14Pipeline();
+        } else if (sbom instanceof SPDX23SBOM) {
+            qaPipeline = new SPDX23Pipeline();
+        } else {
+            // todo custom exception for QAPipeline, not the service
+            throw new Exception("Metrics not supported for " + sbom.getClass().getSimpleName() + " sboms");
+        }
+
+        // QA test SBOM and return result
+        return qaPipeline.process(sbom.getUID(), sbom);
     }
 }
