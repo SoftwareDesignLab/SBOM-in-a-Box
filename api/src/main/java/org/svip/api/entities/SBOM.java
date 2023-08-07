7f6655990dbe0e9174ed5b8e5d0981ec4470a96d
juanfpatino <jfp6815@rit.edu>
2023-08-07 14:53:23 -0400

    ConvertFromAPITest.CDXTagValueTest assertThrows SerializerException

--- api/src/main/java/org/svip/api/entities/SBOM.java
+++ api/src/main/java/org/svip/api/entities/SBOM.java
@@ -51,7 +51,7 @@ public class SBOM {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
     @JsonProperty
-    private Long id;
+    public Long id; // todo make private again
 
     @Column(nullable = false)
     @JsonProperty("fileName")
@@ -250,4 +250,5 @@ public class SBOM {
     public VEXFile getVEXFile(){
         return this.vexFile;
     }
+
 }
