63078d33b36be85a7cad98eb06e5d2738e82b6c9
Derek Garcia <dlg1206@g.rit.edu>
2023-08-07 09:28:29 -0400

    initial upload

---
+++ api/src/main/java/org/svip/api/requests/UploadQRFileInput.java
@@ -0,0 +1,32 @@
+package org.svip.api.requests;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.svip.api.entities.QualityReportFile;
+import org.svip.metrics.pipelines.QualityReport;
+
+/**
+ * @author Derek Garcia
+ **/
+public class UploadQRFileInput {
+
+    /**
+     * Create a new Quality Report File Object
+     * @return SBOM File
+     * @throws JsonProcessingException Failed to parse SBOM and is invalid
+     */
+    public QualityReportFile toQualityReportFile(QualityReport qa) throws JsonProcessingException {
+        QualityReportFile qaf = new QualityReportFile();
+
+        // Configure object mapper to remove null and empty arrays
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        // Set attributes
+        qaf.setName(qa.getUid()).setContent(mapper.writeValueAsString(qa));
+
+        return qaf;
+    }
+}
