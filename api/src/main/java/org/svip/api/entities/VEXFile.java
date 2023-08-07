b9cd0463b22a3ba3f118890011638ebf9289a34b
Derek Garcia <dlg1206@g.rit.edu>
2023-08-07 15:36:44 -0400

    added schema and data origin info to vex table

--- api/src/main/java/org/svip/api/entities/VEXFile.java
+++ api/src/main/java/org/svip/api/entities/VEXFile.java
@@ -1,5 +1,6 @@
 package org.svip.api.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 /**
@@ -10,6 +11,19 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "vex_file")
 public class VEXFile {
+
+    // Schema of VEX
+    public enum Schema{
+        CYCLONEDX_14,
+        CSAF
+    }
+
+    // Database used for generation
+    public enum Database{
+        NVD,
+        OSV
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
@@ -20,6 +34,15 @@ public class VEXFile {
 
     @Column(nullable = false, columnDefinition="LONGTEXT")
     private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "schema_type")
+    private Schema schema;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "datasource")
+    private Database database;
+
 
     @OneToOne(mappedBy = "vex")   // name of field in SBOMFile NOT DB
     private SBOM sbom;
