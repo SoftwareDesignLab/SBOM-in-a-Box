9ad196326646cea16f8ddb84190c19b9996e98bd
juanfpatino <jfp6815@rit.edu>
2023-08-07 20:57:00 -0400

    'UploadSBOMFileInput.java' generates id, better todos

--- api/src/main/java/org/svip/api/requests/UploadSBOMFileInput.java
+++ api/src/main/java/org/svip/api/requests/UploadSBOMFileInput.java
@@ -2,6 +2,7 @@ package org.svip.api.requests;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.svip.api.entities.SBOM;
+import org.svip.api.services.SBOMFileService;
 import org.svip.serializers.SerializerFactory;
 import org.svip.serializers.deserializer.Deserializer;
 
@@ -30,7 +31,7 @@ public record UploadSBOMFileInput(String fileName, String contents) {
 
         // If reach here, SBOM is valid, set additional fields
         sbom.setSchema(d)
-            .setFileType(d);
+            .setFileType(d).id = SBOMFileService.generateSBOMFileId();
 
         return sbom;
     }
