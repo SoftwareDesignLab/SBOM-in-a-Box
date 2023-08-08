0f81f9bd2a9000004f408ed6e42ebaffa33a8dcc
Derek Garcia <dlg1206@g.rit.edu>
2023-08-08 17:21:03 -0400

    refactor Comparison out to separate class

--- core/src/main/java/org/svip/compare/DiffReport.java
+++ core/src/main/java/org/svip/compare/DiffReport.java
@@ -23,48 +23,6 @@ import java.util.Map;
 public class DiffReport {
 
 
-    /**
-     * Utility class for organizing conflict data
-     */
-    private static class Comparison {
-        private static final String METADATA_TAG = "metadata";
-        @JsonProperty
-        private final Map<String, List<Conflict>> componentConflicts = new HashMap<>();
-        @JsonProperty
-        private final List<String> missingComponents = new ArrayList<>();
-
-        public Comparison(SBOM target, SBOM other) {
-            // Compare metadata
-            this.componentConflicts.put(METADATA_TAG, target.compare(other));
-
-            // Round 1: Compare target against other if equal
-            for (Component targetComponent : target.getComponents()) {
-
-                // If other doesn't have component which shares name with target component, skip
-                if (other.getComponents().stream().noneMatch(o -> o.getName() != null && o.getName().equals(targetComponent.getName()))) {
-                    this.missingComponents.add(targetComponent.getName());
-                    continue;
-                }
-
-                // Test targetValue against otherValue
-                for (Component otherComponent : other.getComponents()) {
-                    // Compare hash codes to account for different schema representations of the same component
-                    if (targetComponent.hashCode() == otherComponent.hashCode())
-                        this.componentConflicts.put(targetComponent.getName(), targetComponent.compare(otherComponent));
-                }
-
-            }
-
-            // Round 2: Check for components present in other but not in target
-            for (Component otherComponent : other.getComponents()) {
-                // If target doesn't have component which shares name with other component, skip
-                if (target.getComponents().stream().noneMatch(o -> o.getName() != null && o.getName().equals(otherComponent.getName())))
-                    this.missingComponents.add(otherComponent.getName());
-            }
-        }
-    }
-
-
     @JsonProperty("target")
     private final String targetUID;
     @JsonProperty
