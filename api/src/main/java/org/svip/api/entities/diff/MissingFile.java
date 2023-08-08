3bc02fe68a37ade817210b0a73068b5145b65435
Derek Garcia <dlg1206@g.rit.edu>
2023-08-08 17:00:25 -0400

    stubbed out new tables for db

--- api/src/main/java/org/svip/api/entities/diff/MissingFile.java
+++ api/src/main/java/org/svip/api/entities/diff/MissingFile.java
@@ -1,15 +1,16 @@
-package org.svip.api.entities;
+package org.svip.api.entities.diff;
 
 import jakarta.persistence.*;
 
 /**
- * Diff Report Table for the database
+ * File: MissingFile.java
+ * Missing Component names to be stored in the database
  *
  * @author Derek Garcia
  **/
 @Entity
-@Table(name = "diff_report_file")
-public class DiffReportFile {
+@Table(name = "missing")
+public class MissingFile {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
