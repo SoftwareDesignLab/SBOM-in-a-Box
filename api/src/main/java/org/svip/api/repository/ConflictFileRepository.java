e9770f7c8309c1ff76e06e1a7c28de094c9d31fc
Derek Garcia <dlg1206@g.rit.edu>
2023-08-08 10:49:26 -0400

    copy diff logic to service

--- api/src/main/java/org/svip/api/repository/ConflictFileRepository.java
+++ api/src/main/java/org/svip/api/repository/ConflictFileRepository.java
@@ -1,14 +1,14 @@
 package org.svip.api.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.svip.api.entities.diff.ConflictFile;
+import org.svip.api.entities.DiffReportFile;
 
 /**
- * File: ConflictFileRepository.java
- * Repository for storing Conflicts used for Comparison Files
+ * File: DiffReportFileRepository.java
+ * Repository for storing Diff Report Files
  * CRUD methods: https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
  *
  * @author Derek Garcia
  **/
-public interface ConflictFileRepository extends CrudRepository<ConflictFile, Long> {
+public interface DiffReportFileRepository extends CrudRepository<DiffReportFile, Long> {
 }
