e9a59f83bd808e76f20a8318b884f5b0e376670b
txdvse <txdvse@rit.edu>
2023-08-08 14:47:03 -0400

    MergerCrossSchema.java + Refactored to normalize both SBOMs to SVIPSBOMs + Added standardizeSBOM method.
    For rest of files, added Exception clauses.

--- core/src/main/java/org/svip/merge/MergerCrossSchema.java
+++ core/src/main/java/org/svip/merge/MergerCrossSchema.java
@@ -1,14 +1,16 @@
 package org.svip.merge;
 
+import org.svip.conversion.Conversion;
 import org.svip.sbom.builder.objects.SVIPSBOMBuilder;
 import org.svip.sbom.model.interfaces.generics.Component;
 import org.svip.sbom.model.interfaces.generics.SBOM;
 import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
 import org.svip.sbom.model.objects.SPDX23.SPDX23SBOM;
+import org.svip.sbom.model.objects.SVIPSBOM;
 
 import java.util.Set;
 
-import static org.svip.serializers.SerializerFactory.Schema.SVIP;
+import static org.svip.serializers.SerializerFactory.Schema.*;
 
 public class MergerCrossSchema extends Merger {
 
@@ -23,33 +25,40 @@ public class MergerCrossSchema extends Merger {
      * @return merged SBOM of format SVIP
      */
     @Override
-    public SBOM mergeSBOM(SBOM A, SBOM B) {
+    public SBOM mergeSBOM(SBOM A, SBOM B) throws Exception {
 
         // for simplicity
-        SPDX23SBOM SBOMA;
-        CDX14SBOM SBOMB;
+        SVIPSBOM SBOMA;
+        SVIPSBOM SBOMB;
 
         String name = (A.getName() == null || A.getName().length() == 0) ? B.getName() : A.getName();
 
-        if (A instanceof SPDX23SBOM) {
-            SBOMA = (SPDX23SBOM) A;
-            SBOMB = (CDX14SBOM) B;
-        } else {
-            SBOMA = (SPDX23SBOM) B;
-            SBOMB = (CDX14SBOM) A;
-        }
+        SBOMA = (SVIPSBOM) standardizeSBOM(A);
+        SBOMB = (SVIPSBOM) standardizeSBOM(B);
 
         Set<Component> componentsA = SBOMA.getComponents();
         Set<Component> componentsB = SBOMB.getComponents();
 
         // declare SBOM A as the main SBOM, cast it back to SPDX14SBOM
-        SPDX23SBOM mainSBOM = SBOMA;
+        SVIPSBOM mainSBOM = SBOMA;
 
         // Create a new builder for the new SBOM
         SVIPSBOMBuilder builder = new SVIPSBOMBuilder();
 
         return MergerUtils.mergeToSchema(SBOMA, SBOMB, componentsA, componentsB, mainSBOM, builder, SVIP, name);
 
+    }
+
+    public SBOM standardizeSBOM(SBOM sbom) throws Exception {
+        if(sbom instanceof CDX14SBOM) {
+            return Conversion.convertSBOM(sbom, CDX14, SVIP);
+        } else if(sbom instanceof SPDX23SBOM) {
+            return Conversion.convertSBOM(sbom, SPDX23, SVIP);
+        } else if(sbom instanceof SVIPSBOM) {
+            return sbom;
+        } else {
+            return null;
+        }
     }
 
 }
