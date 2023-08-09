d65fbf1fc035bbc763693f7afa418af3f52845d6
Derek Garcia <dlg1206@g.rit.edu>
2023-08-09 17:02:10 -0400

    new UploadConflictFileInput

--- core/src/test/java/org/svip/sbom/model/objects/CycloneDX14/CDX14SBOMConflictsTest.java
+++ core/src/test/java/org/svip/sbom/model/objects/CycloneDX14/CDX14SBOMConflictsTest.java
@@ -41,8 +41,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(1, conflictList.size());
-        assertEquals(MismatchType.ORIGIN_FORMAT_MISMATCH, conflict.GetType());
-        assertEquals("Format doesn't match", conflict.GetMessage());
+        assertEquals(MismatchType.ORIGIN_FORMAT_MISMATCH, conflict.getType());
+        assertEquals("Format doesn't match", conflict.getMessage());
     }
 
     @Test
@@ -58,8 +58,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(1, conflictList.size());
-        assertEquals(MismatchType.NAME_MISMATCH, conflict.GetType());
-        assertEquals("Name doesn't match", conflict.GetMessage());
+        assertEquals(MismatchType.NAME_MISMATCH, conflict.getType());
+        assertEquals("Name doesn't match", conflict.getMessage());
     }
 
     @Test
@@ -75,8 +75,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(1, conflictList.size());
-        assertEquals(MismatchType.MISC_MISMATCH, conflict.GetType());
-        assertEquals("UID doesn't match", conflict.GetMessage());
+        assertEquals(MismatchType.MISC_MISMATCH, conflict.getType());
+        assertEquals("UID doesn't match", conflict.getMessage());
     }
 
     @Test
@@ -92,8 +92,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(1, conflictList.size());
-        assertEquals(MismatchType.VERSION_MISMATCH, conflict.GetType());
-        assertEquals("Version doesn't match", conflict.GetMessage());
+        assertEquals(MismatchType.VERSION_MISMATCH, conflict.getType());
+        assertEquals("Version doesn't match", conflict.getMessage());
     }
 
     @Test
@@ -109,8 +109,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(1, conflictList.size());
-        assertEquals(MismatchType.SCHEMA_VERSION_MISMATCH, conflict.GetType());
-        assertEquals("Spec Version doesn't match", conflict.GetMessage());
+        assertEquals(MismatchType.SCHEMA_VERSION_MISMATCH, conflict.getType());
+        assertEquals("Spec Version doesn't match", conflict.getMessage());
     }
 
     @Test
@@ -127,8 +127,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(2, conflictList.size());
-        assertEquals(MismatchType.MISSING, conflict.GetType());
-        assertEquals("License is missing", conflict.GetMessage());
+        assertEquals(MismatchType.MISSING, conflict.getType());
+        assertEquals("License is missing", conflict.getMessage());
     }
 
     @Test
@@ -144,8 +144,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(1, conflictList.size());
-        assertEquals(MismatchType.MISC_MISMATCH, conflict.GetType());
-        assertEquals("Document Comment doesn't match", conflict.GetMessage());
+        assertEquals(MismatchType.MISC_MISMATCH, conflict.getType());
+        assertEquals("Document Comment doesn't match", conflict.getMessage());
     }
 
     @Test
@@ -165,8 +165,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(1, conflictList.size());
-        assertEquals(MismatchType.TIMESTAMP_MISMATCH, conflict.GetType());
-        assertEquals("Timestamp doesn't match", conflict.GetMessage());
+        assertEquals(MismatchType.TIMESTAMP_MISMATCH, conflict.getType());
+        assertEquals("Timestamp doesn't match", conflict.getMessage());
     }
 
     @Test
@@ -186,8 +186,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(1, conflictList.size());
-        assertEquals(MismatchType.MISC_MISMATCH, conflict.GetType());
-        assertEquals("Creator Comment doesn't match", conflict.GetMessage());
+        assertEquals(MismatchType.MISC_MISMATCH, conflict.getType());
+        assertEquals("Creator Comment doesn't match", conflict.getMessage());
     }
 
     @Test
@@ -214,9 +214,9 @@ public class CDX14SBOMConflictsTest {
 
         for(Conflict c : conflictList)
         {
-            if(c.GetType() == MismatchType.NAME_MISMATCH && Objects.equals(c.GetMessage(), "Organization: Name doesn't match"))
+            if(c.getType() == MismatchType.NAME_MISMATCH && Objects.equals(c.getMessage(), "Organization: Name doesn't match"))
                 c1 = true;
-            else if(c.GetType() == MismatchType.MISC_MISMATCH && Objects.equals(c.GetMessage(), "Organization: URL doesn't match"))
+            else if(c.getType() == MismatchType.MISC_MISMATCH && Objects.equals(c.getMessage(), "Organization: URL doesn't match"))
                 c2 = true;
         }
 
@@ -248,9 +248,9 @@ public class CDX14SBOMConflictsTest {
 
         for(Conflict c : conflictList)
         {
-            if(c.GetType() == MismatchType.NAME_MISMATCH && Objects.equals(c.GetMessage(), "Organization: Name doesn't match"))
+            if(c.getType() == MismatchType.NAME_MISMATCH && Objects.equals(c.getMessage(), "Organization: Name doesn't match"))
                 c1 = true;
-            else if(c.GetType() == MismatchType.MISC_MISMATCH && Objects.equals(c.GetMessage(), "Organization: URL doesn't match"))
+            else if(c.getType() == MismatchType.MISC_MISMATCH && Objects.equals(c.getMessage(), "Organization: URL doesn't match"))
                 c2 = true;
         }
 
@@ -277,8 +277,8 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(2, conflictList.size());
-        assertEquals(MismatchType.MISSING, conflict.GetType());
-        assertEquals("Creation Data: Author is missing", conflict.GetMessage());
+        assertEquals(MismatchType.MISSING, conflict.getType());
+        assertEquals("Creation Data: Author is missing", conflict.getMessage());
     }
 
     @Test
@@ -308,7 +308,7 @@ public class CDX14SBOMConflictsTest {
         Conflict conflict = conflictList.get(0);
 
         assertEquals(2, conflictList.size());
-        assertEquals(MismatchType.MISSING, conflict.GetType());
-        assertEquals("Creation Data: Tool is missing", conflict.GetMessage());
+        assertEquals(MismatchType.MISSING, conflict.getType());
+        assertEquals("Creation Data: Tool is missing", conflict.getMessage());
     }
 }
