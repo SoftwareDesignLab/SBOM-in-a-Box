743a51770ebfb48660666c088d6b3ecc0b3f2cdd
Derek Garcia <dlg1206@g.rit.edu>
2023-08-09 14:14:35 -0400

    added missing VEXFile Docstrings

--- api/src/main/java/org/svip/api/entities/VEXFile.java
+++ api/src/main/java/org/svip/api/entities/VEXFile.java
@@ -38,7 +38,9 @@ public class VEXFile {
     @Column(nullable = false, name = "datasource")
     private Database datasource;
 
-
+    ///
+    /// Relationships
+    ///
     @OneToOne(mappedBy = "vexFile")   // name of field in SBOMFile NOT DB
     private SBOM sbom;
 
@@ -47,26 +49,51 @@ public class VEXFile {
     /// Setters
     ///
 
+    /**
+     * Set Vex File Name
+     * @param name name
+     * @return VEXFile
+     */
     public VEXFile setName(String name){
         this.name = name;
         return this;
     }
 
+    /**
+     * Set Vex File Content
+     * @param content vex json contents
+     * @return VEXFile
+     */
     public VEXFile setContent(String content){
         this.content = content;
         return this;
     }
 
+    /**
+     * Set VEX Schema
+     * @param schema schema
+     * @return VEXFile
+     */
     public VEXFile setSchema(VEXType schema){
         this.schema = schema;
         return this;
     }
 
+    /**
+     * Set VEX Datasource
+     * @param database origin database
+     * @return VEXFile
+     */
     public VEXFile setDatasource(Database database){
         this.datasource = database;
         return this;
     }
 
+    /**
+     * Set the parent SBOM
+     * @param sbom sbom vex was generated for
+     * @return VEXFile
+     */
     public VEXFile setSBOM(SBOM sbom){
         this.sbom = sbom;
         return this;
@@ -77,11 +104,16 @@ public class VEXFile {
     /// Getters
     ///
 
-
+    /**
+     * @return VEXFile ID
+     */
     public Long getID() {
         return this.id;
     }
 
+    /**
+     * @return VEXFile JSON Contents
+     */
     public String getContent() {
         return this.content;
     }
