b247e567880d61c335f4bd39409288043453c7f8
Derek Garcia <dlg1206@g.rit.edu>
2023-08-09 12:19:33 -0400

    initial find target and other id

--- api/src/main/java/org/svip/api/repository/ComparisonFileRepository.java
+++ api/src/main/java/org/svip/api/repository/ComparisonFileRepository.java
@@ -3,6 +3,8 @@ package org.svip.api.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.svip.api.entities.diff.ComparisonFile;
 
+import java.util.List;
+
 /**
  * File: DiffReportFileRepository.java
  * Repository for storing Diff Report Files
@@ -11,4 +13,15 @@ import org.svip.api.entities.diff.ComparisonFile;
  * @author Derek Garcia
  **/
 public interface ComparisonFileRepository extends CrudRepository<ComparisonFile, Long> {
+
+    /**
+     * Select Conflict files for a target and other sbom
+     *
+     * SQL: SELECT * FROM comparison WHERE target_id = targetID AND other_id = otherID;
+     * @param targetID Target SBOM ID
+     * @param otherID  Other SBOM ID
+     *
+     * @return ComparisonFile for the target and other sbom
+     */
+    ComparisonFile findByTargetIDAndOtherID(Long targetID, Long otherID);
 }
