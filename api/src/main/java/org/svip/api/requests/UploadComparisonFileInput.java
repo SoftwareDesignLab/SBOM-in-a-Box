38ab3319b2f10b307e65af06141298f27722e11d
Derek Garcia <dlg1206@g.rit.edu>
2023-08-09 16:48:32 -0400

    added initial ComparisonFile setters and relationship

--- api/src/main/java/org/svip/api/requests/UploadComparisonFileInput.java
+++ api/src/main/java/org/svip/api/requests/UploadComparisonFileInput.java
@@ -1,13 +1,8 @@
 package org.svip.api.requests;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.svip.api.entities.QualityReportFile;
 import org.svip.api.entities.SBOM;
 import org.svip.api.entities.diff.ComparisonFile;
 import org.svip.compare.Comparison;
-import org.svip.metrics.pipelines.QualityReport;
 
 /**
  * file: UploadComparisonFileInput.java
@@ -21,6 +16,7 @@ public record UploadComparisonFileInput(Comparison comparison) {
      * Create a new Comparison File Object
      *
      * @param targetSBOM Target SBOM for comparison
+     * @param otherSBOM otherSBOM for comparison
      * @return ComparisonFile
      */
     public ComparisonFile toQualityReportFile(SBOM targetSBOM, SBOM otherSBOM) {
