d03dd03bb3dbe34f7dee5d8f24c3acb61988e62b
Derek Garcia <dlg1206@g.rit.edu>
2023-08-09 17:53:22 -0400

    comparison saves but conflict fails to upload

--- core/src/main/java/org/svip/compare/Comparison.java
+++ core/src/main/java/org/svip/compare/Comparison.java
@@ -123,4 +123,15 @@ public class Comparison {
     public Map<String, List<Conflict>> getComponentConflicts() {
         return this.componentConflicts;
     }
+
+    ///
+    /// Getters
+    ///
+
+    /**
+     * @return List of component conflicts
+     */
+    public Map<String, List<Conflict>> getComponentConflicts() {
+        return this.componentConflicts;
+    }
 }
