50173f4579e9ccc4cf301e72615445db10dfb51c
Ian Dunn <itd3516@rit.edu>
2023-08-10 19:37:07 -0400

    Move directory management to enum

--- core/src/main/java/org/svip/generation/osi/OSI.java
+++ core/src/main/java/org/svip/generation/osi/OSI.java
@@ -26,10 +26,60 @@ import java.util.Map;
  * @author Ian Dunn
  **/
 public class OSI {
+
     /**
-     * The location of the bound directory relative to the build path (core).
+     * Private enumeration to store, validate, and retrieve full file paths for subdirectories of /bound_dir
      */
-    private static final String BOUND_DIR = "/src/main/java/org/svip/generation/osi/bound_dir";
+    private enum BOUND_DIRS {
+        CODE("code"),
+        SBOMS("sboms");
+
+        /**
+         * The location of the bound directory relative to the build path (core).
+         */
+        private static final String BOUND_DIR = "/src/main/java/org/svip/generation/osi/bound_dir";
+
+        /**
+         * The directory name of the /bound_dir subdirectory
+         */
+        private final String dirName;
+
+        BOUND_DIRS(String dirName) {
+            this.dirName = dirName;
+        }
+
+        /**
+         * Gets the path to the OSI bound_dir folder from anywhere in the system.
+         *
+         * @return A File containing a reference to that folder, which is guaranteed to exist.
+         */
+        public File getPath() {
+            String path = System.getProperty("user.dir") + BOUND_DIR + "/" + dirName;
+            // Replace api from core if running in api working directory
+            File file = new File(path.replaceFirst("api", "core"));
+
+            // Make directories
+            if (!file.exists()) file.mkdirs();
+
+            return file;
+        }
+
+        /**
+         * Cleans the subdirectory in /bound_dir to remove all files and re-replace the .gitignore.
+         *
+         * @throws IOException If a file cannot be removed from the directory or if the .gitignore could not be written.
+         */
+        public void cleanPath() throws IOException {
+            File dir = this.getPath();
+
+            FileUtils.cleanDirectory(dir);
+
+            // Add gitignore
+            try (PrintWriter w = new PrintWriter(dir + "/.gitignore")) {
+                w.print("*\n" + "!.gitignore");
+            }
+        }
+    }
 
     /**
      * The OSIClient that manages the container.
@@ -45,8 +95,8 @@ public class OSI {
      */
     public OSI() throws DockerNotAvailableException, IOException {
         // Remove all SBOMs and source files in the bound_dir folder
-        cleanBoundDirectory("code");
-        cleanBoundDirectory("sboms");
+        BOUND_DIRS.CODE.cleanPath();
+        BOUND_DIRS.SBOMS.cleanPath();
 
         // Run Docker check and throw if we can't validate an install
         int dockerStatus = dockerCheck();
@@ -118,7 +168,7 @@ public class OSI {
      * @throws IOException If the file could not be written to the code bind directory.
      */
     public void addSourceFile(String fileName, String fileContents) throws IOException {
-        File project = getBoundDirPath("code");
+        File project = BOUND_DIRS.CODE.getPath();
 
         // Constructing the printwriter with a file means that it takes care of all system-specific path problems
         try (PrintWriter writer = new PrintWriter(new File(project.getAbsolutePath() + "/" + fileName))) {
@@ -135,7 +185,7 @@ public class OSI {
      * @throws IOException If one of the files in the copied directory could not be written to the code bind directory.
      */
     public void addSourceDirectory(File dirPath) throws IOException {
-        File project = getBoundDirPath("code");
+        File project = BOUND_DIRS.CODE.getPath();
 
         FileUtils.copyDirectory(dirPath, project);
     }
@@ -154,11 +204,11 @@ public class OSI {
     public Map<String, String> generateSBOMs(List<String> toolNames) throws IOException {
         boolean status = this.client.generateSBOMs(toolNames);
 
-        cleanBoundDirectory("code");
+        BOUND_DIRS.CODE.cleanPath();
 
         Map<String, String> sboms = new HashMap<>();
 
-        File sbomDir = getBoundDirPath("sboms");
+        File sbomDir = BOUND_DIRS.SBOMS.getPath();
         // If container failed or files are null, return empty map.
         if (status || !sbomDir.exists() || sbomDir.listFiles() == null) return sboms;
 
@@ -166,48 +216,13 @@ public class OSI {
             if (!file.getName().equalsIgnoreCase(".gitignore"))
                 sboms.put(file.getName(), Files.readString(file.toPath()));
 
-        cleanBoundDirectory("sboms");
+        BOUND_DIRS.SBOMS.cleanPath();
 
         return sboms;
     }
 
     /**
-     * Gets the path to the OSI bound_dir folder from anywhere in the system.
-     *
-     * @param directoryName The subdirectory of the bound_dir to access (code or sboms).
-     * @return A File containing a reference to that folder, which is guaranteed to exist.
-     */
-    protected File getBoundDirPath(String directoryName) {
-        String path = System.getProperty("user.dir") + BOUND_DIR + "/" + directoryName;
-        // Replace api from core if running in api working directory
-        File file = new File(path.replaceFirst("api", "core"));
-
-        // Make directories
-        if (!file.exists()) file.mkdirs();
-
-        return file;
-    }
-
-    /**
-     * Cleans a subdirectory in the bound directory (sboms, code) to remove all files and then re-replace the
-     * .gitignore.
-     *
-     * @param directoryName The directory name to clean.
-     * @throws IOException If a file cannot be removed from the directory or if the .gitignore could not be written.
-     */
-    protected void cleanBoundDirectory(String directoryName) throws IOException {
-        File dir = getBoundDirPath(directoryName);
-
-        FileUtils.cleanDirectory(dir);
-
-        // Add gitignore
-        try (PrintWriter w = new PrintWriter(dir + "/.gitignore")) {
-            w.print("*\n" + "!.gitignore");
-        }
-    }
-
-    /**
-     * OSI Client to interact with Docker.
+     * OSI Client to interact with the Docker container's Flask API.
      */
     private static class OSIClient {
 
