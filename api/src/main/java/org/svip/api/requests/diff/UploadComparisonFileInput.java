d464a139e826a6d294b2346ca6bbe31d72ec117c
Derek Garcia <dlg1206@g.rit.edu>
2023-08-10 14:29:26 -0400

    hot fix prevent null field components

--- api/src/main/java/org/svip/api/requests/diff/UploadComparisonFileInput.java
+++ api/src/main/java/org/svip/api/requests/diff/UploadComparisonFileInput.java
@@ -32,11 +32,31 @@ public record UploadComparisonFileInput(Comparison comparison) {
         }
 
         // Add missing conflicts
-        for(String component : this.comparison().getMissingFromTarget())
+        for(String component : this.comparison().getMissingFromTarget()){
+            /* TODO HOTFIX
+                Handles edge case where an SBOM object has only null values, guess is regex failure and not enough
+                info to build in the deserialization stage. UPLOAD SHOULD FAIL IF NAME IS NULL, this just prevents
+                that from happening
+            */
+            if(component == null)
+                continue;
+            // end hotfix
             cf.addConflictFile(new UploadConflictFileInput(component).toMissingConflictFile(cf, false));
+        }
 
-        for(String component : this.comparison().getMissingFromOther())
+
+        for(String component : this.comparison().getMissingFromOther()){
+            /* TODO HOTFIX
+                Handles edge case where an SBOM object has only null values, guess is regex failure and not enough
+                info to build in the deserialization stage. UPLOAD SHOULD FAIL IF NAME IS NULL, this just prevents
+                that from happening
+            */
+            if(component == null)
+                continue;
+            // end hotfix
             cf.addConflictFile(new UploadConflictFileInput(component).toMissingConflictFile(cf, true));
+        }
+
 
         // set parent relationships
         cf.setTargetSBOM(targetSBOM)
