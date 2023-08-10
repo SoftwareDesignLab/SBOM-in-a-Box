8d21f2c6671f56d647b162a663f598977ae14a08
Derek Garcia <dlg1206@g.rit.edu>
2023-08-10 10:18:24 -0400

    Successful upload of missing components

--- api/src/main/java/org/svip/api/requests/UploadConflictFileInput.java
+++ api/src/main/java/org/svip/api/requests/UploadConflictFileInput.java
@@ -3,6 +3,7 @@ package org.svip.api.requests;
 import org.svip.api.entities.diff.ComparisonFile;
 import org.svip.api.entities.diff.ConflictFile;
 import org.svip.compare.conflicts.Conflict;
+import org.svip.compare.conflicts.MismatchType;
 
 /**
  * file: UploadConflictFileInput.java
@@ -10,15 +11,16 @@ import org.svip.compare.conflicts.Conflict;
  *
  * @author Derek Garcia
  **/
-public record UploadConflictFileInput(String name, Conflict conflict) {
+public record UploadConflictFileInput(String name) {
 
     /**
-     * Create a new Conflict File Object
+     * Create a new Conflict File Object using a Conflict
      *
      * @param cf Comparison File the conflict belongs to
+     * @param conflict Conflict to get details from
      * @return ComparisonFile
      */
-    public ConflictFile toConflictFile(ComparisonFile cf) {
+    public ConflictFile toConflictFile(ComparisonFile cf, Conflict conflict) {
         ConflictFile conflictFile = new ConflictFile();
 
         // Set content
@@ -28,6 +30,35 @@ public record UploadConflictFileInput(String name, Conflict conflict) {
                     .setTargetValue(conflict.getTarget())
                     .setOtherValue(conflict.getOther())
                     .setComparison(cf);     // set parent relationship
+
+        // set relationship to comparison
+        cf.addConflictFile(conflictFile);
+
+        return conflictFile;
+    }
+
+    /**
+     * Special Case to add a missing component
+     *
+     * @param cf Comparison File the conflict belongs to
+     * @param inTarget is the missing component in the target
+     * @return ComparisonFile
+     */
+    public ConflictFile toMissingConflictFile(ComparisonFile cf, boolean inTarget) {
+        ConflictFile conflictFile = new ConflictFile();
+
+        // Set content
+        conflictFile.setName(this.name)
+                .setMessage("Component is missing")
+                .setMismatchType(MismatchType.MISSING)
+                .setComparison(cf);     // set parent relationship
+
+        // Set the name in the value that it's present in
+        if(inTarget){
+            conflictFile.setTargetValue(this.name);
+        } else {
+            conflictFile.setOtherValue(this.name);
+        }
 
         // set relationship to comparison
         cf.addConflictFile(conflictFile);
