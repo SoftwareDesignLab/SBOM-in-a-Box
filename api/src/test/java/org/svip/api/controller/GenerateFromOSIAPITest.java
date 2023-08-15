5820bdd0ac124d27bef17719cdf2ba8a524552a0
Tom-Roman <tfr8811@rit.edu>
2023-08-15 16:10:57 -0400

    fixed tests

--- api/src/test/java/org/svip/api/controller/GenerateFromOSIAPITest.java
+++ api/src/test/java/org/svip/api/controller/GenerateFromOSIAPITest.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.svip.api.entities.MockMultipartFile;
 import org.svip.api.entities.SBOMFile;
 import org.svip.api.entities.SBOMFile;
 import org.svip.generation.osi.OSI;
