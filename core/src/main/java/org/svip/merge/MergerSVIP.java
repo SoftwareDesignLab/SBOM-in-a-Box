c26672c70b57078403cb55b7d7cbe7b1b9bb479c
juanfpatino <jfp6815@rit.edu>
2023-08-15 15:17:16 -0400

    MergerSVIP stubbed out untested

--- core/src/main/java/org/svip/merge/MergerSVIP.java
+++ core/src/main/java/org/svip/merge/MergerSVIP.java
@@ -1,4 +1,38 @@
 package org.svip.merge;
 
-public class MergerSVIP {
+import org.svip.sbom.builder.objects.SVIPSBOMBuilder;
+import org.svip.sbom.model.interfaces.generics.Component;
+import org.svip.sbom.model.interfaces.generics.SBOM;
+import org.svip.sbom.model.objects.SVIPSBOM;
+
+import java.util.Set;
+
+import static org.svip.serializers.SerializerFactory.Schema.SVIP;
+
+/**
+ * File: MergerSPDX.java
+ * <p>
+ * Merges two SVIP SBOMs together.
+ *
+ * @author Juan Francisco Patino
+ */
+public class MergerSVIP extends Merger  {
+    public MergerSVIP() {
+        super();
+    }
+    @Override
+    public SBOM mergeSBOM(SBOM A, SBOM B) {
+
+        Set<Component> componentsA = A.getComponents();
+        Set<Component> componentsB = B.getComponents();
+
+        // declare SBOM A as the main SBOM, cast it back to SPDX14SBOM
+        SVIPSBOM mainSBOM = (SVIPSBOM) A;
+
+        // Create a new builder for the new SBOM
+        SVIPSBOMBuilder builder = new SVIPSBOMBuilder();
+
+        return MergerUtils.mergeToSchema(A, B, componentsA, componentsB, mainSBOM, builder, SVIP, "");
+
+    }
 }
