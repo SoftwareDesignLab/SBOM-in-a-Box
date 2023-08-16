d404e2ff96aad82e39db66678d4465132153b246
Ian Dunn <itd3516@rit.edu>
2023-08-16 11:52:38 -0400

    Revert "ParserController and ParserService"
    This reverts commit cc051e5d6d9bcc7ae78c1299b4596efaf72ec014.

--- api/src/main/java/org/svip/api/controller/ParserController.java
+++ api/src/main/java/org/svip/api/controller/ParserController.java
@@ -1,15 +1,7 @@
 package org.svip.api.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.svip.api.services.ParserService;
-import org.svip.serializers.SerializerFactory;
-import java.io.IOException;
 
 /**
  * REST API Controller for generating SBOMs using Parsers
@@ -19,41 +11,4 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/svip/generators")
 public class ParserController {
-    private final ParserService parserService;
-
-
-    /**
-     * Create new Controller with services
-     *
-     * @param parserService Service for handling SBOM queries
-     */
-    public ParserController(ParserService parserService){
-        this.parserService = parserService;
-    }
-
-
-
-    /**
-     * USAGE. Send GENERATE request to /generate an SBOM from source file(s)
-     *
-     * @param projectName of project to be converted to SBOM
-     * @param zipFile     path to zip file
-     * @param schema      to convert to
-     * @param format      to convert to
-     * @return generated SBOM
-     */
-    @PostMapping("/generators/parsers")
-    public ResponseEntity<?> generateParsers(@RequestParam("zipFile") MultipartFile zipFile,
-                                             @RequestParam("projectName") String projectName,
-                                             @RequestParam("schema") SerializerFactory.Schema schema,
-                                             @RequestParam("format") SerializerFactory.Format format) throws IOException {
-
-        Long parseId = this.parserService.parseSBOM(zipFile, projectName, schema, format);
-        // todo convert should probably throw errors instead of returning null if error occurs
-        if (parseId == null)
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-
-        // Return final merged ID
-        return new ResponseEntity<>(parseId, HttpStatus.OK);
-    }
 }
