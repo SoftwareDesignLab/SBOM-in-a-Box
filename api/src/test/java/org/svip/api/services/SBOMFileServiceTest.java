342ecc5560c233726b309e627d8b15d27ef5bfdb
dlg1206 <dlg1206@g.rit.edu>
2023-08-16 12:23:28 -0400

    use UploadSBOMFileInput instead of raw constructor

--- api/src/test/java/org/svip/api/services/SBOMFileServiceTest.java
+++ api/src/test/java/org/svip/api/services/SBOMFileServiceTest.java
@@ -8,6 +8,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.svip.api.entities.SBOM;
 import org.svip.api.repository.SBOMRepository;
+import org.svip.api.requests.UploadSBOMFileInput;
+import org.svip.serializers.SerializerFactory;
+import org.svip.serializers.deserializer.Deserializer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.never;
@@ -29,67 +36,86 @@ public class SBOMFileServiceTest {
     @InjectMocks
     private SBOMFileService sbomFileService;    // Instance of service for testing
 
+    // Test SBOMs
+    private static final String CDX_JSON_SBOM_FILE = "./src/test/resources/sample_sboms/cdx-gomod-1.4.0-bin.json";
+    private static final String SPDX_JSON_SBOM_FILE = "./src/test/resources/sample_sboms/syft-0.80.0-source-spdx-json.json";
+    private static final String SPDX_TAG_VALUE_SBOM_FILE = "./src/test/resources/sample_sboms/sbom.alpine-compare.2-3.spdx";
+
     ///
     /// Upload
     ///
     @Test
     @DisplayName("Upload CDX14 JSON SBOM")
     void upload_cdx14_json_sbom() {
-        // Given
-        SBOM sbom = buildMockSBOMFile(SBOM.Schema.CYCLONEDX_14, SBOM.FileType.JSON);
-        // When
         try{
+            // Given
+            SBOM sbom = buildMockSBOMFile(CDX_JSON_SBOM_FILE);
+            // When
             this.sbomFileService.upload(sbom);
+            // Then
+            verify(this.sbomRepository).save(sbom);
+        } catch (IOException e){
+            fail("Failed to parse file: " + CDX_JSON_SBOM_FILE);
         } catch (Exception e){
             fail("Valid CDX14 SBOM");
         }
-        // Then
-        verify(this.sbomRepository).save(sbom);
     }
 
     @Test
     @DisplayName("Upload SPDX23 JSON SBOM")
     void upload_spdx23_json_sbom() {
-        // Given
-        SBOM sbom = buildMockSBOMFile(SBOM.Schema.SPDX_23, SBOM.FileType.JSON);
-        // When
         try{
+            // Given
+            SBOM sbom = buildMockSBOMFile(SPDX_JSON_SBOM_FILE);
+            // When
             this.sbomFileService.upload(sbom);
+            // Then
+            verify(this.sbomRepository).save(sbom);
+        } catch (IOException e){
+            fail("Failed to parse file: " + SPDX_JSON_SBOM_FILE);
         } catch (Exception e){
             fail("Valid SPDX23 JSON SBOM");
         }
-        // Then
-        verify(this.sbomRepository).save(sbom);
+
     }
 
     @Test
     @DisplayName("Upload SPDX23 Tag Value SBOM")
     void upload_spdx23_tag_value_sbom() {
-        // Given
-        SBOM sbom = buildMockSBOMFile(SBOM.Schema.SPDX_23, SBOM.FileType.TAG_VALUE);
-        // When
-        try{
+        try {
+            // Given
+            SBOM sbom = buildMockSBOMFile(SPDX_TAG_VALUE_SBOM_FILE);
+            // When
             this.sbomFileService.upload(sbom);
+            // Then
+            verify(this.sbomRepository).save(sbom);
+        } catch (IOException e){
+            fail("Failed to parse file: " + SPDX_TAG_VALUE_SBOM_FILE);
         } catch (Exception e){
             fail("Valid SPDX23 Tag Value SBOM");
         }
-        // Then
-        verify(this.sbomRepository).save(sbom);
     }
+
+    ///
+    /// Convert
+    ///
 
     ///
     /// Helper methods
     ///
 
     /**
+     * Generate SBOM File from a filepath
+     *
+     * @param filepath path of the sbom
      * @return Valid Mock SBOM file
+     * @throws IOException failed to open file
      */
-    private SBOM buildMockSBOMFile(SBOM.Schema schema, SBOM.FileType fileType){
-        // Skip Input record
-        return new SBOM().setName("SBOM_NAME")
-                .setContent("SBOM_CONTENT")
-                .setSchema(schema)
-                .setFileType(fileType);
+    private SBOM buildMockSBOMFile(String filepath) throws IOException {
+        // Get file contents
+        String content = new String(Files.readAllBytes(Paths.get(filepath)));
+        // Create SBOM
+        return new UploadSBOMFileInput(filepath, content).toSBOMFile();
     }
 
 
