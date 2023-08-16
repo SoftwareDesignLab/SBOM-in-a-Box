1db20b8ad84ed103ca3cb665f69d05a778e55e39
txdvse <txdvse@rit.edu>
2023-08-16 15:56:32 -0400

    Skeleton for entire repair package

--- core/src/main/java/org/svip/repair/repair/RepairCDX14.java
+++ core/src/main/java/org/svip/repair/repair/RepairCDX14.java
@@ -1,15 +1,13 @@
 package org.svip.repair.repair;
 
-import org.svip.repair.fix.Fix;
 import org.svip.sbom.model.interfaces.generics.SBOM;
 
-import java.util.List;
 import java.util.Map;
 
-public class RepairCDX14 implements Repair { // todo depreciate class
+public class RepairCDX14 implements Repair {
 
     @Override
-    public SBOM repairSBOM(String uid, SBOM sbom, Map<String, Map<String, List<Fix<?>>>> repairs) {
+    public SBOM repairSBOM(String uid, SBOM sbom, Map<String, Map<String, String>> repairs) {
         return null;
     }
 
