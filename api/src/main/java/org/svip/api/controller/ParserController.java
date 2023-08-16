13e2432d4f1c7c4906fcc07d36cf3b0c6ce2555e
Ian Dunn <itd3516@rit.edu>
2023-08-16 12:00:13 -0400

    Fix endpoints

--- api/src/main/java/org/svip/api/controller/ParserController.java
+++ api/src/main/java/org/svip/api/controller/ParserController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Derek Garcia
  **/
 @RestController
-@RequestMapping("/svip/generators")
+@RequestMapping("/svip/generators/parsers")
 public class ParserController {
 }
