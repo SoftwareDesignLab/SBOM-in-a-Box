0fd074af57b411b07d36f5faae50134bdcced2f9
txdvse <txdvse@rit.edu>
2023-09-13 17:24:13 -0400

    RepairStatementSPDX23CDX14.java + Updated docstrings

--- core/src/main/java/org/svip/repair/statements/RepairStatementSPDX23CDX14.java
+++ core/src/main/java/org/svip/repair/statements/RepairStatementSPDX23CDX14.java
@@ -1,12 +1,14 @@
 package org.svip.repair.statements;
 
 import org.svip.metrics.pipelines.QualityReport;
+import org.svip.metrics.pipelines.SVIPPipeline;
 import org.svip.metrics.pipelines.schemas.CycloneDX14.CDX14Pipeline;
 import org.svip.metrics.pipelines.schemas.SPDX23.SPDX23Pipeline;
 import org.svip.metrics.resultfactory.Result;
 import org.svip.metrics.resultfactory.enumerations.STATUS;
 import org.svip.repair.fix.*;
 import org.svip.sbom.model.interfaces.generics.SBOM;
+import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
 import org.svip.sbom.model.objects.SPDX23.SPDX23SBOM;
 
 import java.util.ArrayList;
@@ -23,28 +25,47 @@ public class RepairStatementSPDX23CDX14 implements RepairStatement {
     public Map<String, Map<String, List<Fix<?>>>> generateRepairStatement(String uid, SBOM sbom) {
 
         QualityReport report; // get quality report
+
+        // If the SBOM is an SPDX 2.3 SBOM
         if (sbom instanceof SPDX23SBOM) {
+
+            // Generate the quality report
             SPDX23Pipeline pipeline = new SPDX23Pipeline();
             report = pipeline.process(uid, sbom);
-        } else {
+
+        }
+        // If the SBOM is a CycloneDX 1.4 SBOM
+        else if (sbom instanceof CDX14SBOM) {
+
+            // Generate the quality report
             CDX14Pipeline pipeline = new CDX14Pipeline();
             report = pipeline.process(uid, sbom);
+
+        }
+        // Otherwise, try to generate an SVIP Quality Report
+        else {
+
+            // Generate the quality report
+            SVIPPipeline pipeline = new SVIPPipeline();
+            report = pipeline.process(uid, sbom);
+
         }
 
+        // get the Quality Report's results
         Map<String, Map<String, List<Result>>> results = report.getResults();
 
-        for (String repairType : results.keySet()
-        ) {
+        // For each result
+        for (String repairType : results.keySet()) {
 
+            // Make a new repair type list
             Map<String, List<Fix<?>>> repairsForThisRepairType = new HashMap<>();
 
-            for (String repairSubType : results.get(repairType).keySet()
-            ) {
+            // Iterate through those sub results as repair subtypes
+            for (String repairSubType : results.get(repairType).keySet()) {
 
                 ArrayList<Fix<?>> fixArrayList = new ArrayList<>();
 
-                for (Result toFix : results.get(repairType).get(repairSubType)
-                ) {
+                for (Result toFix : results.get(repairType).get(repairSubType)) {
 
                     // if a result fails, it needs to be fixed
                     if (toFix.getStatus().equals(STATUS.FAIL)) {
@@ -52,24 +73,34 @@ public class RepairStatementSPDX23CDX14 implements RepairStatement {
                         // fix
                         Fixes fixes = getFixes(toFix);
                         List<Fix<?>> fixList = null;
+
+                        // If the fixes is not null
                         if (fixes != null)
+                            // Set the fix list
                             fixList = fixes.fix(toFix, sbom, repairSubType);
+
+                        // If the fix list is not null
                         if (fixList != null)
+                            // Add all the fixes to the fix array list
                             fixArrayList.addAll(fixList);
 
                     }
 
                 }
 
+                // Add the repairs for this specific repair type
                 repairsForThisRepairType.put(repairSubType, fixArrayList);
 
             }
 
+            // Add the repairs to the main repair list
             repairs.put(repairType, repairsForThisRepairType);
 
         }
 
+        // Return the repairs
         return repairs;
+
     }
 
     /**
@@ -79,9 +110,13 @@ public class RepairStatementSPDX23CDX14 implements RepairStatement {
      * @return appropriate fixes class
      */
     private static Fixes getFixes(Result result) {
+
+        // Fixes
         Fixes fixes = null;
 
+        // Depending on the failing test, get the correct "fix" class
         switch (result.getTest()) {
+
             case "Matching CPE" -> fixes = new CPEFixes();
 
             case "EmptyOrNullTest", "Has Creation Info", "HasSPDXID" -> fixes = new EmptyOrNullFixes(null);
@@ -94,7 +129,10 @@ public class RepairStatementSPDX23CDX14 implements RepairStatement {
 
 
         }
+
+        // Return the fixes
         return fixes;
+
     }
 
 
