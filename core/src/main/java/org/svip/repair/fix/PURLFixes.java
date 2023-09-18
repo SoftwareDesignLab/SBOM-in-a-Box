06d5be763b07361a276fccaf8553f81d422154dd
txdvse <122822903+txdvse@users.noreply.github.com>
2023-09-18 14:39:37 -0400

    RepairStatements (#248)
    * Initial Repair file commit
    
    * Skeleton for entire repair package
    
    * RepairStatementSPDX23.generateRepairStatement stubbed out
    
    * 'Fixes' architecture
    
    * 'Fixes' architecture
    
    * Cleaner
    
    * EmptyOrNullFixes.bomVersionFix
    
    * EmptyOrNullFixes.creationDataFix
    
    * Some cleanup
    
    * EmptyOrNullFixes.SPDXIDFix and began PURLFixes.purlMatchFix
    
    * Fixes.fix signature fix, PURLFixes.purlMatchFix
    
    * PURLFixes.purlMatchFix null fixes
    
    * EmptyOrNullFixes.commentNullFix
    
    * EmptyOrNullFixes.attributionTextNullFix
    
    * Comments and Fix.java format fix
    
    * EmptyOrNullFixes.bomVersionFix
    
    * PURLFixes fix, and we will probably depreciate RepairCDX14 and RepairStatementCDX14
    
    * Fixes.fix signature change to take repairSubType key for more accurate potential fixes
    
    * EmptyOrNullFixes.nullAuthorFix and EmptyOrNullFixes.googlePotentialAuthors untested
    
    * CPEFixes.CPEFix
    
    * CPEFixes.CPEFix fixed
    
    * PURL and CPE fixes classes simplified and work. todos in RepairTest.repairTest
    
    * RepairController in API created
    
    * `/sboms/repair/statment` endpoint untested
    
    * `/sboms/repair` endpoint untested and Repair.repairSBOM signature change
    
    * repair/statement should be GET
    
    * Forgot spring request params
    
    * RepairController with appropriate decorators
    
    * /sboms/repair/statement tested and works with SPDX sboms
    
    * RepairStatementSPDX23.java replaced with RepairStatementSPDX23CDX14, code cleanup + docstrings
    
    * fixed cdx14pipeline bug in RepairStatementSPDX23CDX14
    
    * changelog.md
    
    * RepairTest.CDXRepairTest
    
    * Got rid of todos
    
    * CPEFixes fix and EmptyOrNullFixes.bomRefFix
    
    * RepairController.java
    + Updated docstrings
    
    * CPEFixes.java
    + Updated docstrings
    
    * EmptyOrNullFixes.java
    + Updated docstrings
    
    * PURLFixes.java
    + Updated docstrings
    
    * RepairStatementSPDX23CDX14.java
    + Updated docstrings
    
    * Dockerfile
    + Removed -x test clause
    
    EmptyOrNullFixes.java
    + Changed collections for PURL handling
    + Now adds comment to author fixes that the new-found author is unconfirmed/a potential author.
    
    HashFixes.java
    + TODO
    
    LicenseFixes.java
    + TODO
    
    Fixes.java
    + Updated exceptions
    
    * updated exception throws
    
    * updated exception throws
    
    * updated exception throws
    
    ---------
    
    Co-authored-by: juanfpatino <jfp6815@rit.edu>
    Co-authored-by: Juan Patino <juan9patino9@gmail.com>

---
+++ core/src/main/java/org/svip/repair/fix/PURLFixes.java
@@ -0,0 +1,153 @@
+package org.svip.repair.fix;
+
+import org.svip.metrics.resultfactory.Result;
+import org.svip.sbom.model.interfaces.generics.Component;
+import org.svip.sbom.model.interfaces.generics.SBOM;
+import org.svip.sbom.model.interfaces.schemas.CycloneDX14.CDX14Package;
+import org.svip.sbom.model.interfaces.schemas.SPDX23.SPDX23Package;
+import org.svip.sbom.model.uids.PURL;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Fixes class to generate suggested component PURL repairs
+ */
+public class PURLFixes implements Fixes {
+    @Override
+    public List<Fix<?>> fix(Result result, SBOM sbom, String repairSubType) {
+
+        // Create a new purl
+        PURL newPurl = null;
+
+        for (Component component : sbom.getComponents() // for each component in the SBOM
+        ) {
+
+            String[] split = result.getDetails().split(" "); // split up result message
+
+            boolean fixThisPurl = false;
+
+            // are we fixing this PURL, if so is it because something doesn't match, or is inaccurate?
+            if (result.getMessage().toLowerCase().contains("does not match"))
+                fixThisPurl = split[split.length - 1].contains(component.getName());
+
+            // Otherwise, if the result was accurate
+            else if (result.getTest().contains("Accurate")) {
+
+                // If the component is an SPDX 2.3 Package
+                if (component instanceof SPDX23Package spdx23Package) {
+
+                    // For each PURL
+                    for (String purl : spdx23Package.getPURLs()) {
+                        // If the PURL is invalid, set fixThisPurl to true and break
+                        if (purl.equals(split[0])) {
+
+                            fixThisPurl = true;
+                            break;
+
+                        }
+
+                    }
+                }
+                // Otherwise, if the component is a CycloneDX 1.4 Package
+                else if (component instanceof CDX14Package cdx14Package)
+
+                    // For each PURL
+                    for (String purl : cdx14Package.getPURLs()) {
+
+                        // If the PURL is invalid, set fixThisPurl to true and break
+                        if (purl.equals(split[0])) {
+
+                            fixThisPurl = true;
+                            break;
+
+                        }
+
+                    }
+
+            }
+
+            // If This PURL needs to be fixes
+            if (fixThisPurl)
+                try {
+
+                    // Set the standard identifiers/fields
+                    String type = "pkg"; // todo is this true for all components?
+                    String name = repairSubType;
+                    if (component.getName() != null)
+                        name = component.getName();
+                    String nameSpace = null;
+                    String version = null;
+
+                    // If the component is an SPDX 2.3 Package
+                    if (component instanceof SPDX23Package spdx23Package) {
+
+                        // And if the type is null, set the type
+                        if (spdx23Package.getType() != null)
+                            type = spdx23Package.getType();
+
+                        // And if the Supplier is null, set the Supplier
+                        if (spdx23Package.getSupplier() != null)
+                            nameSpace = spdx23Package.getSupplier().getName();
+
+                        // And if the Version is null, set the Version
+                        if (spdx23Package.getVersion() != null)
+                            version = spdx23Package.getVersion();
+
+                    }
+                    // Otherwise, if the component is a CycloneDX 1.4 Package
+                    else if (component instanceof CDX14Package cdx14Package) {
+
+                        // If the type is null, set the Type
+                        if (cdx14Package.getType() != null)
+                            type = cdx14Package.getType();
+
+                        // If the Supplier is null, set the Supplier
+                        if (cdx14Package.getSupplier() != null)
+                            nameSpace = cdx14Package.getSupplier().getName();
+
+                        // If the Version is null, set the Version
+                        if (cdx14Package.getVersion() != null)
+                            version = cdx14Package.getVersion();
+                    }
+
+                    // If the namespace is null, set the namespace equal to the component author
+                    if (nameSpace == null)
+                        nameSpace = component.getAuthor();
+                    // And if that doesn't work, try setting the namespace to the repairSubType
+                    if (nameSpace == null)
+                        nameSpace = repairSubType;
+
+                    // Create the new PURL
+                    newPurl = new PURL(type + ":" + nameSpace + "/" + name +
+                            ((version != null) ? ("@" + version) : "")); // todo qualifiers?
+
+                    // break
+                    break;
+
+                } catch (Exception ignored) {
+                    // If all goes wrong, return null
+                    return null;
+                }
+
+        }
+
+        // If the newPURL is null, return null
+        if (newPurl == null)
+            return null;
+
+        try {
+
+            // Then, try to return the fixes
+            return Collections.singletonList(new Fix<>("", newPurl));
+
+        } catch (Exception e) {
+
+            // But if that goes wrong return null
+            return null;
+
+        }
+
+    }
+
+}
