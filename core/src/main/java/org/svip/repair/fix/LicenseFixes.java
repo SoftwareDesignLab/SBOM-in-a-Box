701b07f2dbf9040330302bce41eba54f6fe7c67c
Hooobot <hoobot0@gmail.com>
2023-09-25 15:01:50 -1000

    Update switch to add LGPL shells

--- core/src/main/java/org/svip/repair/fix/LicenseFixes.java
+++ core/src/main/java/org/svip/repair/fix/LicenseFixes.java
@@ -112,6 +112,18 @@ public class LicenseFixes implements Fixes<License> {
                 return null;
             case "GPL-3.0-with-GCC-exception":
                 return null;
+            case "LGPL-2.0":
+                return null;
+            case "LGPL-2.0+":
+                return null;
+            case "LGPL-2.1":
+                return null;
+            case "LGPL-2.1+":
+                return null;
+            case "LGPL-3.0":
+                return null;
+            case "LGPL-3.0+":
+                return null;
             // ... Adding other deprecated Ids...
             default:
                 return Collections.emptyList();
