d42999f499b1e3da21699524439d140bb882280c
txdvse <txdvse@rit.edu>
2023-09-28 22:41:11 -0400

    ManipulateSVIP.java + Initial skeleton commit

---
+++ core/src/main/java/org/svip/manipulation/manipulate/ManipulateSVIP.java
@@ -0,0 +1,11 @@
+package org.svip.manipulation.manipulate;
+
+/**
+ * Name: ManipulateSVIP.java
+ * Description: Creates a new SVIP SBOM and 'manipulates'
+ * the fields based on the desired schema and format.
+ *
+ * @author Tyler Drake
+ */
+public class ManipulateSVIP {
+}
