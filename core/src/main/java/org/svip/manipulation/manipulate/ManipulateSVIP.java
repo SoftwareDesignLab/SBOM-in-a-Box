f52b9fb0174014ba39546b25016c9922f7aabab9
txdvse <txdvse@rit.edu>
2023-09-29 00:43:48 -0400

    ManipulateSVIP.java + Added implementation for modify + Added skeleton for modifyComponent

--- core/src/main/java/org/svip/manipulation/manipulate/ManipulateSVIP.java
+++ core/src/main/java/org/svip/manipulation/manipulate/ManipulateSVIP.java
@@ -1,5 +1,11 @@
 package org.svip.manipulation.manipulate;
 
+import org.svip.sbom.builder.objects.SVIPComponentBuilder;
+import org.svip.sbom.builder.objects.SVIPSBOMBuilder;
+import org.svip.sbom.model.interfaces.generics.Component;
+import org.svip.sbom.model.objects.SVIPComponentObject;
+import org.svip.sbom.model.objects.SVIPSBOM;
+
 /**
  * Name: ManipulateSVIP.java
  * Description: Creates a new SVIP SBOM and 'manipulates'
@@ -8,4 +14,78 @@ package org.svip.manipulation.manipulate;
  * @author Tyler Drake
  */
 public class ManipulateSVIP {
+
+    public static SVIPSBOM modify(SVIPSBOM sbom, SchemaManipulationMap manipulationMap) {
+
+        // Create new SVIP SBOM Builder
+        SVIPSBOMBuilder builder = new SVIPSBOMBuilder();
+
+        // Format
+        builder.setFormat(sbom.getFormat());
+
+        // Name
+        builder.setName(sbom.getName());
+
+        // UID
+        builder.setUID(sbom.getUID());
+
+        // Version
+        builder.setVersion(sbom.getVersion());
+
+        // Spec Version
+        builder.setSpecVersion(sbom.getSpecVersion());
+
+        // Stream licenses into new SBOM
+        if(sbom.getLicenses() != null)
+            sbom.getLicenses().stream().forEach(x -> builder.addLicense(x));
+
+        // Creation Data
+        builder.setCreationData(sbom.getCreationData());
+
+        // Document Comment
+        builder.setDocumentComment(sbom.getDocumentComment());
+
+        // Document Comment
+        builder.setDocumentComment(sbom.getDocumentComment());
+
+        // Root Component
+        builder.setRootComponent(modifyComponent(sbom.getRootComponent()));
+
+        // Stream components from SVIP SBOM, convert them, then put into CDX SBOM
+        if(sbom.getComponents() != null)
+            sbom.getComponents().stream().forEach(x -> builder.addComponent(modifyComponent(x)));
+
+        // Stream Relationship data into new SBOM
+        if(sbom.getRelationships() != null) {
+            sbom.getRelationships().keySet().forEach(
+                    x -> sbom.getRelationships().get(x).stream().forEach(
+                            y -> builder.addRelationship(x, y)
+                    )
+            );
+        }
+
+        // Stream External References into new SBOM
+        if(sbom.getExternalReferences() != null) {
+            sbom.getExternalReferences().stream().forEach(
+                    x -> builder.addExternalReference(x)
+            );
+        }
+
+        // Set SPDX License List Version
+        builder.setSPDXLicenseListVersion(sbom.getSPDXLicenseListVersion());
+
+
+        // Build the SBOM and return it
+        return builder.Build();
+
+    }
+
+    public static SVIPComponentObject modifyComponent(Component originalComponent) {
+
+        SVIPComponentBuilder builder = new SVIPComponentBuilder();
+
+        return builder.buildAndFlush();
+
+    }
+
 }
