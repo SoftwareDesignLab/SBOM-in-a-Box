31ab5a42238e03e748ce57471a2537c758cb6224
txdvse <txdvse@rit.edu>
2023-10-02 18:52:12 -0400

    ManipulateSVIP.java + Finished modifyComponent functionality

--- core/src/main/java/org/svip/manipulation/manipulate/ManipulateSVIP.java
+++ core/src/main/java/org/svip/manipulation/manipulate/ManipulateSVIP.java
@@ -6,6 +6,8 @@ import org.svip.sbom.model.interfaces.generics.Component;
 import org.svip.sbom.model.objects.SVIPComponentObject;
 import org.svip.sbom.model.objects.SVIPSBOM;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -18,6 +20,8 @@ import java.util.UUID;
 public class ManipulateSVIP {
 
     public static SVIPSBOM modify(SVIPSBOM sbom, SchemaManipulationMap manipulationMap) {
+
+        HashMap<String, String> relationshipMap = new HashMap<>();
 
         // Create new SVIP SBOM Builder
         SVIPSBOMBuilder builder = new SVIPSBOMBuilder();
@@ -51,11 +55,11 @@ public class ManipulateSVIP {
         builder.setDocumentComment(sbom.getDocumentComment());
 
         // Root Component
-        builder.setRootComponent(modifyComponent(sbom.getRootComponent()));
+        builder.setRootComponent(modifyComponent(sbom.getRootComponent(), manipulationMap, relationshipMap));
 
         // Stream components from SVIP SBOM, convert them, then put into CDX SBOM
         if(sbom.getComponents() != null)
-            sbom.getComponents().stream().forEach(x -> builder.addComponent(modifyComponent(x)));
+            sbom.getComponents().stream().forEach(x -> builder.addComponent(modifyComponent(x, manipulationMap, relationshipMap)));
 
         // Stream Relationship data into new SBOM
         if(sbom.getRelationships() != null) {
@@ -82,15 +86,123 @@ public class ManipulateSVIP {
 
     }
 
-    public static SVIPComponentObject modifyComponent(Component originalComponent) {
+    public static SVIPComponentObject modifyComponent(
+            Component originalComponent, SchemaManipulationMap manipulationMap, HashMap relationshipMap
+    ) {
 
+        // Cast component
+        SVIPComponentObject component = (SVIPComponentObject) originalComponent;
+
+        //
         SVIPComponentBuilder builder = new SVIPComponentBuilder();
 
+        // Type
+        builder.setType(component.getType());
+
+        // UID
+        String oldId = component.getUID();
+        String newId = resolveID(component, manipulationMap);
+        relationshipMap.put(oldId, newId);
+        builder.setUID(newId);
+
+        // Author
+        builder.setAuthor(component.getAuthor());
+
+        // Name
+        builder.setName(component.getName());
+
+        // Licenses
+        builder.setLicenses(component.getLicenses());
+
+        // Copyright
+        builder.setCopyright(component.getCopyright());
+
+        // Hashes
+        Map<String, String> hashes = component.getHashes();
+        if (hashes != null) hashes.keySet().forEach(x -> builder.addHash(x, hashes.get(x)));
+
+        // Comment
+        builder.setComment(component.getComment());
+
+        // Attribution Text
+        builder.setAttributionText(component.getAttributionText());
+
+        // File Notice
+        builder.setFileNotice(component.getFileNotice());
+
+        // Download Location
+        builder.setDownloadLocation(component.getDownloadLocation());
+
+        // File Name
+        builder.setFileName(component.getFileName());
+
+        // Files Analyzed
+        builder.setFilesAnalyzed(component.getFilesAnalyzed());
+
+        // Verification Code
+        builder.setVerificationCode(component.getVerificationCode());
+
+        // Home Page
+        builder.setHomePage(component.getHomePage());
+
+        // Source Info
+        builder.setSourceInfo(component.getSourceInfo());
+
+        // Release Date
+        builder.setReleaseDate(component.getReleaseDate());
+
+        // Built Date
+        builder.setBuildDate(component.getBuiltDate());
+
+        // Valid Until Date
+        builder.setValidUntilDate(component.getValidUntilDate());
+
+        // Supplier
+        builder.setSupplier(component.getSupplier());
+
+        // Version
+        builder.setVersion(component.getVersion());
+
+        // Description
+        builder.setDescription(component.getDescription());
+
+        // CPEs
+        if(component.getCPEs() != null) component.getCPEs().forEach(x -> builder.addCPE(x));
+
+        // PURLs
+        if(component.getPURLs() != null) component.getPURLs().forEach(x -> builder.addPURL(x));
+
+        // External References
+        if(component.getExternalReferences() != null)
+            component.getExternalReferences().forEach(x -> builder.addExternalReference(x));
+
+        // Mime Type
+        builder.setMimeType(component.getMimeType());
+
+        // Publisher
+        builder.setPublisher(component.getPublisher());
+
+        // Scope
+        builder.setScope(component.getScope());
+
+        // Group
+        builder.setGroup(component.getGroup());
+
+        // Properties
+        if(component.getProperties() != null) {
+            component.getProperties().keySet().forEach(
+                    x -> component.getProperties().get(x).stream().forEach(
+                            y -> builder.addProperty(x, y)
+                    )
+            );
+        }
+
+        // Build the component and return it
         return builder.buildAndFlush();
 
     }
 
-    public static String generateID(String oldID, SVIPComponentObject component, SchemaManipulationMap manipulationMap) {
+    public static String resolveID(SVIPComponentObject component, SchemaManipulationMap manipulationMap) {
         switch (manipulationMap) {
             case SPDX23 -> {
                 return manipulationMap.getComponentIDType() + component.getName() + "-" + component.getVersion();
@@ -100,5 +212,4 @@ public class ManipulateSVIP {
             }
         }
     }
-
 }
