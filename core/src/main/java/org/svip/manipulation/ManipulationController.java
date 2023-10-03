f9555c99a9a78023d25b9df9f3d50dd642ccd612
txdvse <txdvse@rit.edu>
2023-10-02 20:21:51 -0400

    ManipulationController.java + New manipulation package controller class along with skeleton

--- core/src/main/java/org/svip/manipulation/ManipulationController.java
+++ core/src/main/java/org/svip/manipulation/ManipulationController.java
@@ -1,4 +1,48 @@
 package org.svip.manipulation;
 
+import org.svip.sbom.model.interfaces.generics.SBOM;
+import org.svip.sbom.model.objects.SVIPSBOM;
+
 public class ManipulationController {
+
+    public static SVIPSBOM toSVIP() {
+
+        SVIPSBOM sbom = null;
+
+        return sbom;
+
+    }
+
+    public static SVIPSBOM manipulate() {
+
+        SVIPSBOM sbom = null;
+
+        return sbom;
+
+    }
+
+    public static SBOM toSchema() {
+
+        SBOM sbom = null;
+
+        return sbom;
+
+    }
+
+    public static SBOM convertStandard() {
+
+        SVIPSBOM sbom = null;
+
+        return sbom;
+
+    }
+
+    public static SBOM fullConvert() {
+
+        SBOM sbom = null;
+
+        return sbom;
+
+    }
+
 }
