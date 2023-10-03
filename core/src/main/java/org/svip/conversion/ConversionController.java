4ba8fa159fc7c466cbb572206c2371657d7f898f
txdvse <txdvse@rit.edu>
2023-10-03 09:44:12 -0400

    ConversionController.java + Added toSchema implementation

--- core/src/main/java/org/svip/conversion/ConversionController.java
+++ core/src/main/java/org/svip/conversion/ConversionController.java
@@ -4,6 +4,8 @@ import org.svip.conversion.manipulate.ManipulateController;
 import org.svip.conversion.manipulate.SchemaManipulationMap;
 import org.svip.conversion.toSVIP.ToSVIP;
 import org.svip.conversion.toSVIP.ToSVIPController;
+import org.svip.conversion.toSchema.ToSchema;
+import org.svip.conversion.toSchema.ToSchemaController;
 import org.svip.sbom.model.interfaces.generics.SBOM;
 import org.svip.sbom.model.objects.SVIPSBOM;
 import org.svip.serializers.SerializerFactory;
@@ -30,15 +32,17 @@ public class ConversionController {
 
     }
 
-    public static SBOM toSchema() {
+    public static SBOM toSchema(SVIPSBOM sbom, SerializerFactory.Schema desiredSchema) {
 
-        SBOM sbom = null;
+        ToSchema toSchema = ToSchemaController.getToSchema(desiredSchema);
 
-        return sbom;
+        SBOM convertedSBOM = toSchema.convert(sbom);
+
+        return convertedSBOM;
 
     }
 
-    public static SBOM convertToStandard() {
+    public static SBOM convertToSVIPManipulate() {
 
         SVIPSBOM sbom = null;
 
