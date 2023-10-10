103d9e37ad4cd822633f1334bfceba630b639b64
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-10-10 15:11:26 -0400

    Created abstract Extraction class

---
+++ core/src/main/java/org/svip/repair/extraction/Extraction.java
@@ -0,0 +1,27 @@
+package org.svip.repair.extraction;
+
+import org.svip.generation.parsers.utils.QueryWorker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+/**
+ * <b>File</b>: Extraction.java<br>
+ * <b>Description</b>: Abstract core Class for extracting information
+ * from package manager metadata
+ * @author Justin Jantzi
+ */
+public abstract class Extraction {
+
+    protected final ArrayList<QueryWorker> queryWorkers;
+    private HashMap<String, String> purl;
+
+    protected Extraction(HashMap<String, String> purl) {
+        this.purl = purl;
+        this.queryWorkers = new ArrayList<>();
+    }
+
+    public abstract String extractCopyright();
+}
