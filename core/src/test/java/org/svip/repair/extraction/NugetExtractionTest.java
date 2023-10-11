b6a5936c261d36d4316412ba543a726da9191b56
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-10-11 15:40:00 -0400

    Nuget Extraction complete with 3 passing tests

--- core/src/test/java/org/svip/repair/extraction/NugetExtractionTest.java
+++ core/src/test/java/org/svip/repair/extraction/NugetExtractionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * Tests for the NugetExtraction class
@@ -15,16 +16,31 @@ public class NugetExtractionTest {
 
     private NugetExtraction ext;
     private final String MICROSOFT_COPYRIGHT = "© Microsoft Corporation. All rights reserved.";
+    private final HashMap<String, String> PURL = new HashMap<String, String>() {{
+        put("name", "System.Text.Json");
+        put("version", "8.0.0-rc.2.23479.6");
+    }};
+    private final String MICROSOFT_LICENSE = "MIT";
 
     public NugetExtractionTest() {
-        HashMap<String, String> examplePurl = new HashMap<String, String>();
-
-        ext = new NugetExtraction(examplePurl);
+        ext = new NugetExtraction(PURL);
+        ext.extract();
     }
 
     @Test
-    public void extractCopyright() {
-        String copyright = ext.extractCopyright();
+    public void getValidCopyright() {
+        String copyright = ext.getCopyright();
         assertEquals(MICROSOFT_COPYRIGHT, copyright);
+    }
+
+    @Test
+    public void getValidLicense() {
+        String license = ext.getLicense();
+        assertEquals(MICROSOFT_LICENSE, license);
+    }
+
+    @Test public void getInvalidPURL() {
+        NugetExtraction ne = new NugetExtraction(null);
+        assertNull(ne.getCopyright());
     }
 }
