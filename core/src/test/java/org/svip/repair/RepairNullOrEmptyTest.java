dede1046ddf44394917a9fce61814da137c04933
jwj7297 <91503444+jwj7297@users.noreply.github.com>
2023-10-13 23:03:53 -0400

    Added test and support for CDX 14

--- core/src/test/java/org/svip/repair/RepairNullOrEmptyTest.java
+++ core/src/test/java/org/svip/repair/RepairNullOrEmptyTest.java
@@ -2,7 +2,9 @@ package org.svip.repair;
 
 import org.junit.jupiter.api.Test;
 import org.svip.repair.fix.Fix;
+import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
 import org.svip.sbom.model.objects.SPDX23.SPDX23SBOM;
+import org.svip.serializers.deserializer.CDX14JSONDeserializer;
 import org.svip.serializers.deserializer.SPDX23JSONDeserializer;
 import org.svip.serializers.deserializer.SPDX23TagValueDeserializer;
 import org.svip.utils.Debug;
@@ -14,9 +16,17 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Tests for EmptyOrNullFixes class
+ *
+ * @author Justin Jantzi
+ */
 public class RepairNullOrEmptyTest {
-    private final String NULL_COPYRIGHT_SBOM = System.getProperty("user.dir") +
+    private final String NULL_COPYRIGHT_SBOM_SPDX = System.getProperty("user.dir") +
             "/src/test/resources/repair/null-copyright.spdx";
+
+    private final String NULL_COPYRIGHT_SBOM_CDX = System.getProperty("user.dir") +
+            "/src/test/resources/repair/null-copyright-cdx.json";
 
     private final String MICROSOFT_COPYRIGHT_FIX = "© Microsoft Corporation. All rights reserved.";
 
@@ -25,10 +35,18 @@ public class RepairNullOrEmptyTest {
     private final RepairController r = new RepairController();
 
     @Test
-    public void NullCopyright() throws Exception {
+    public void NullCopyrightSPDX23() throws Exception {
         SPDX23TagValueDeserializer spdx23JSONDeserializer = new SPDX23TagValueDeserializer();
-        SPDX23SBOM sbom = spdx23JSONDeserializer.readFromString(Files.readString(Path.of(NULL_COPYRIGHT_SBOM)));
+        SPDX23SBOM sbom = spdx23JSONDeserializer.readFromString(Files.readString(Path.of(NULL_COPYRIGHT_SBOM_SPDX)));
         Map<String, Map<String, List<Fix<?>>>> statement = r.generateStatement(sbom, sbom.getUID());
         assertEquals(MICROSOFT_COPYRIGHT_FIX, statement.get(PACKAGE_NAME).get(PACKAGE_NAME).get(0).fixed());
+    }
+
+    @Test
+    public void NullCopyrightCDX() throws Exception {
+        CDX14JSONDeserializer  cdx14JSONDeserializer = new CDX14JSONDeserializer();
+        CDX14SBOM sbom = cdx14JSONDeserializer.readFromString(Files.readString(Path.of(NULL_COPYRIGHT_SBOM_CDX)));
+        Map<String, Map<String, List<Fix<?>>>> statement = r.generateStatement(sbom, sbom.getUID());
+        assertEquals(MICROSOFT_COPYRIGHT_FIX, statement.get(PACKAGE_NAME).get(PACKAGE_NAME).get(3).fixed());
     }
 }
