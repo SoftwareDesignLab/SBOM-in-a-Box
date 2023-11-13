967e533bc3e33b3e78d7db74713328a9ac832e97
Derek Garcia <dlg1206@g.rit.edu>
2023-11-13 05:20:01 -0500

    update initial config files

--- core/src/main/java/org/svip/generation/osi/docker/server/constants/Format.py
+++ core/src/main/java/org/svip/generation/osi/docker/server/constants/Format.py
@@ -6,7 +6,8 @@ class Format(Enum):
     Enum for current SBOM Formats
     """
     JSON = 1,
-    SPDX = 2
+    SPDX = 2,
+    XML = 3
 
 
 def to_format(format_str: str):
@@ -15,5 +16,7 @@ def to_format(format_str: str):
             return Format.JSON
         case "spdx":
             return Format.SPDX
+        case "xml":
+            return Format.XML
         case _:
             raise Exception(f"'{format_str}' is an unsupported format")
