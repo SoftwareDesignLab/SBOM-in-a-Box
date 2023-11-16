2cfad495dde43317b20a6674120b6ed7acea90c8
txdvse <txdvse@rit.edu>
2023-11-16 14:48:52 -0500

    CDX14XMLDeserializer.java + implemented skeleton

--- core/src/main/java/org/svip/serializers/deserializer/CDX14XMLDeserializer.java
+++ core/src/main/java/org/svip/serializers/deserializer/CDX14XMLDeserializer.java
@@ -1,4 +1,33 @@
 package org.svip.serializers.deserializer;
 
-public class CDX14XMLDeserializer {
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.fasterxml.jackson.dataformat.xml.deser.FromXmlParser;
+import com.fasterxml.jackson.dataformat.xml.deser.XmlDeserializationContext;
+import com.fasterxml.jackson.dataformat.xml.deser.XmlTextDeserializer;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+import org.svip.sbom.model.interfaces.generics.SBOM;
+import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
+
+import java.io.IOException;
+
+public class CDX14XMLDeserializer implements Deserializer {
+
+    public void deserialize(CDX14SBOM value, FromXmlParser parser, SerializerProvider provider) throws IOException {
+
+    }
+
+    @Override
+    public SBOM readFromString(String fileContents) {
+        return null;
+    }
+
+    @Override
+    public ObjectMapper getObjectMapper() {
+        return null;
+    }
 }
