79207a95b3f7c6fa952e2d51ad2c15e2dccd4a1b
txdvse <txdvse@rit.edu>
2023-11-18 22:08:38 -0500

    CDX14XMLSerializer.java + implemented skeleton

--- core/src/main/java/org/svip/serializers/serializer/CDX14XMLSerializer.java
+++ core/src/main/java/org/svip/serializers/serializer/CDX14XMLSerializer.java
@@ -1,4 +1,22 @@
 package org.svip.serializers.serializer;
 
-public class CDX14XMLSerializer {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.svip.sbom.model.objects.SVIPSBOM;
+
+public class CDX14XMLSerializer implements Serializer{
+    @Override
+    public String writeToString(SVIPSBOM sbom) {
+        return null;
+    }
+
+    @Override
+    public ObjectMapper getObjectMapper() {
+        return null;
+    }
+
+    @Override
+    public void setPrettyPrinting(boolean prettyPrint) {
+
+    }
 }
