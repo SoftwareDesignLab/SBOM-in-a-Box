282f8b53a1242bdca19e2085d43c4c415981a442
txdvse <txdvse@rit.edu>
2023-11-19 23:37:11 -0500

    CDX14XMLSerializerTest.java + Added writeToStringTest

--- core/src/test/java/org/svip/generation/serializers/serializer/CDX14XMLSerializerTest.java
+++ core/src/test/java/org/svip/generation/serializers/serializer/CDX14XMLSerializerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.svip.sbom.model.objects.CycloneDX14.CDX14SBOM;
 import org.svip.serializers.SerializerFactory;
 import org.svip.serializers.serializer.CDX14JSONSerializer;
+import org.svip.serializers.serializer.CDX14XMLSerializer;
 import org.svip.serializers.serializer.Serializer;
 import org.svip.utils.Debug;
 
@@ -12,7 +13,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CDX14XMLSerializerTest extends SerializerTest {
 
-    public CDX14XMLSerializerTest(Serializer serializer) {
-        super(serializer);
+    public CDX14XMLSerializerTest() {
+        super(new CDX14XMLSerializer());
     }
+
+    @Test
+    public void writeToStringTest() throws JsonProcessingException {
+        Debug.logBlockTitle("CDX 1.4 XML");
+        String serialized = getSerializer().writeToString(getTestSBOM());
+        Debug.log(Debug.LOG_TYPE.DEBUG, "\n" + serialized);
+        Debug.logBlock();
+        Debug.log(Debug.LOG_TYPE.SUMMARY, "Successfully serialized SBOM.");
+
+        Debug.log(Debug.LOG_TYPE.SUMMARY, "Deserializing SBOM back to object.");
+        CDX14SBOM sbom = (CDX14SBOM) SerializerFactory.createDeserializer(serialized).readFromString(serialized);
+
+        // TODO Compare getTestSBOM() and sbom with Comparison when it's finished
+        assertEquals(3, sbom.getComponents().size());
+    }
+
 }
