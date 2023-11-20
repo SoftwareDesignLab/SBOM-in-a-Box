e3559fed7611e4bcf5d296ad46b3e40fef3134ab
txdvse <txdvse@rit.edu>
2023-11-20 04:56:32 -0500

    CDX14XMLSerializer.java + Restructured to extend off of StdSerializer and use ToXmlGenerator

--- core/src/main/java/org/svip/serializers/serializer/CDX14XMLSerializer.java
+++ core/src/main/java/org/svip/serializers/serializer/CDX14XMLSerializer.java
@@ -1,21 +1,56 @@
 package org.svip.serializers.serializer;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import org.svip.sbom.model.objects.SVIPSBOM;
 
 import java.io.IOException;
 
-public class CDX14XMLSerializer implements Serializer {
+public class CDX14XMLSerializer extends StdSerializer<SVIPSBOM> implements Serializer {
 
     private boolean prettyPrint = false;
 
-    public void serialize(SVIPSBOM sbom, ToXmlGenerator xmlGenerator, SerializerProvider provider) throws IOException {
+    public CDX14XMLSerializer() {
+        super(SVIPSBOM.class);
+    }
+
+    protected CDX14XMLSerializer(Class<SVIPSBOM> t) {
+        super(t);
+    }
+
+    @Override
+    public String writeToString(SVIPSBOM sbom) throws JsonProcessingException {
+        if(prettyPrint)
+            return getObjectMapper().writer().with(SerializationFeature.INDENT_OUTPUT).writeValueAsString(sbom);
+        else return getObjectMapper().writer().writeValueAsString(sbom);
+    }
+
+    @Override
+    public ObjectMapper getObjectMapper() {
+        XmlMapper mapper = new XmlMapper();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(SVIPSBOM.class, this);
+        mapper.registerModule(module);
+        return mapper;
+    }
+
+    @Override
+    public void setPrettyPrinting(boolean prettyPrint) {
+        this.prettyPrint = prettyPrint;
+    }
+
+    @Override
+    public void serialize(SVIPSBOM sbom, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+
+        ToXmlGenerator xmlGenerator = (ToXmlGenerator) gen;
 
         xmlGenerator.writeStartObject();
 
@@ -29,23 +64,5 @@ public class CDX14XMLSerializer implements Serializer {
         xmlGenerator.writeStringField("serialNumber", sbom.getUID());
 
         xmlGenerator.writeEndArray();
-    }
-
-    @Override
-    public String writeToString(SVIPSBOM sbom) throws JsonProcessingException {
-        if(prettyPrint)
-            return getObjectMapper().writer().with(SerializationFeature.INDENT_OUTPUT).writeValueAsString(sbom);
-        else return getObjectMapper().writer().writeValueAsString(sbom);
-    }
-
-    @Override
-    public ObjectMapper getObjectMapper() {
-        XmlMapper mapper = new XmlMapper();
-        return mapper;
-    }
-
-    @Override
-    public void setPrettyPrinting(boolean prettyPrint) {
-        this.prettyPrint = prettyPrint;
     }
 }
