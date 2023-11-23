270ba48bd287cdd4abcffd6f82ee69bf4fb5d748
txdvse <txdvse@rit.edu>
2023-11-23 07:49:48 -0500

    CDX14XMLSerializer.java + import cleanup

--- core/src/main/java/org/svip/serializers/serializer/CDX14XMLSerializer.java
+++ core/src/main/java/org/svip/serializers/serializer/CDX14XMLSerializer.java
@@ -2,7 +2,6 @@ package org.svip.serializers.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.SerializableString;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.SerializerProvider;
