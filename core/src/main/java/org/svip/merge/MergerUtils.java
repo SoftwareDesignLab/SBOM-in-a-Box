5b18794ea38a76f433fcd63f72b4d26a53e4b147
txdvse <txdvse@rit.edu>
2023-11-30 00:39:23 -0500

    MergerUtils.java + unused import

--- core/src/main/java/org/svip/merge/MergerUtils.java
+++ core/src/main/java/org/svip/merge/MergerUtils.java
@@ -1,6 +1,5 @@
 package org.svip.merge;
 
-import org.svip.conversion.toSVIP.CDX14;
 import org.svip.merge.utils.Utils;
 import org.svip.sbom.builder.interfaces.generics.SBOMBuilder;
 import org.svip.sbom.builder.objects.SVIPComponentBuilder;
