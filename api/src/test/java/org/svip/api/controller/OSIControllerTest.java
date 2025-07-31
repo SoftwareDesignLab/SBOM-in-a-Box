fcbaf22dfaf14996556f4d699311ce39c6fdf915
Derek Garcia <dgarcia2@hawaii.edu>
2025-07-30 15:56:17 -1000

    chore: update deprecated features and mark unchecked casts with todo

--- api/src/test/java/org/svip/api/controller/OSIControllerTest.java
+++ api/src/test/java/org/svip/api/controller/OSIControllerTest.java
@@ -52,6 +52,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
